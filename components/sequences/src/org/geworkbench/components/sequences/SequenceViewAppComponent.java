9664312e9078ee45fad3f673336014a8d0e064d0
Xiaoqing Zhang <xiaoqing@columbia.edu>
2006-01-05 22:14:14 +0000

    Fix bugs for sequenceRetriever.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1081 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/sequences/src/org/geworkbench/components/sequences/SequenceViewAppComponent.java
+++ components/sequences/src/org/geworkbench/components/sequences/SequenceViewAppComponent.java
@@ -22,6 +22,11 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.HashMap;
+import org.geworkbench.bison.datastructure.complex.pattern.sequence.DSSeqRegistration;
+import org.geworkbench.bison.datastructure.complex.pattern.DSMatchedPattern;
+import org.geworkbench.bison.datastructure.biocollections.DSCollection;
+import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
+import org.geworkbench.components.discovery.ParmsDataSet;
 
 
 /**
@@ -33,7 +38,7 @@ import java.util.HashMap;
  * @author
  * @version 1.0
  */
-@AcceptTypes({CSSequenceSet.class}) public class SequenceViewAppComponent implements VisualPlugin, org.geworkbench.engine.config.MenuListener, PropertyChangeListener {
+@AcceptTypes({CSSequenceSet.class, ParmsDataSet.class}) public class SequenceViewAppComponent implements VisualPlugin, org.geworkbench.engine.config.MenuListener, PropertyChangeListener {
     org.geworkbench.util.sequences.SequenceViewWidget sViewWidget;
     EventListenerList listenerList = new EventListenerList();
     JMenuItem jOpenFASTAItem = new JMenuItem();
