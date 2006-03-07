83e702d2fdf8f0ae92bdbe4235cfa47694b87071
Ken Smith <kcs3@columbia.edu>
2006-03-07 19:48:50 +0000

    "Delete selected group" changed to "Delete selected set"
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1329 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/selectors/src/org/geworkbench/components/selectors/SelectorPanel.java
+++ components/selectors/src/org/geworkbench/components/selectors/SelectorPanel.java
@@ -474,7 +474,7 @@ public abstract class SelectorPanel<T extends DSSequential> implements VisualPlu
     protected void deletePanelPressed() {
         String[] labels = getSelectedTreesFromTree();
         if (labels.length > 0) {
-            int confirm = JOptionPane.showConfirmDialog(getComponent(), "Delete selected group" + (labels.length > 1 ? "s" : "") + "?");
+            int confirm = JOptionPane.showConfirmDialog(getComponent(), "Delete selected set" + (labels.length > 1 ? "s" : "") + "?");
             if (confirm == JOptionPane.YES_OPTION) {
                 panelTree.clearSelection();
                 for (int i = 0; i < labels.length; i++) {
