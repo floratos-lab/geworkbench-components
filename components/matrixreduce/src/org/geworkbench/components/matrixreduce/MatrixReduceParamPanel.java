f1ba941be8e4d6b64460b08b213c2c65e3ea31ca
John Watkinson <watkinson@columbia.edu>
2006-06-28 14:35:15 +0000

    Added basics for Matrix Reduce plugin.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1589 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/matrixreduce/src/org/geworkbench/components/matrixreduce/MatrixReduceParamPanel.java
@@ -0,0 +1,12 @@
+package org.geworkbench.components.matrixreduce;
+
+import org.geworkbench.analysis.AbstractSaveableParameterPanel;
+
+import java.io.Serializable;
+
+/**
+ * @author John Watkinson
+ */
+public class MatrixReduceParamPanel extends AbstractSaveableParameterPanel implements Serializable {
+
+}
