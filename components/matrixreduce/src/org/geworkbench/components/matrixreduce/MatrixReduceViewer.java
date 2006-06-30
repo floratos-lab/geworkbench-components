fa6eb2a0b6d7c5784904bad82a61e7859527b2d3
John Watkinson <watkinson@columbia.edu>
2006-06-30 22:14:42 +0000

    Matrix reduce implementation continued.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1597 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/matrixreduce/src/org/geworkbench/components/matrixreduce/MatrixReduceViewer.java
+++ components/matrixreduce/src/org/geworkbench/components/matrixreduce/MatrixReduceViewer.java
@@ -1,27 +1,32 @@
 package org.geworkbench.components.matrixreduce;
 
-import org.geworkbench.engine.config.VisualPlugin;
-import org.geworkbench.engine.management.Subscribe;
-import org.geworkbench.engine.management.AcceptTypes;
-import org.geworkbench.bison.datastructure.complex.pattern.matrix.CSMatrixReduceSet;
-import org.geworkbench.bison.datastructure.complex.pattern.matrix.DSPositionSpecificAffintyMatrix;
-import org.geworkbench.bison.datastructure.complex.pattern.matrix.DSMatrixReduceSet;
+import org.apache.commons.collections15.map.ListOrderedMap;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.complex.pattern.matrix.DSMatrixReduceSet;
+import org.geworkbench.bison.datastructure.complex.pattern.matrix.DSPositionSpecificAffintyMatrix;
+import org.geworkbench.engine.config.VisualPlugin;
+import org.geworkbench.engine.management.AcceptTypes;
+import org.geworkbench.engine.management.Subscribe;
 import org.geworkbench.events.ProjectEvent;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Collection;
 
 /**
  * @author John Watkinson
  */
 @AcceptTypes(DSMatrixReduceSet.class)
 public class MatrixReduceViewer implements VisualPlugin {
+
+    private static final int TAB_PSAM = 0;
+    private static final int TAB_SEQUENCE = 1;
 
     private JTabbedPane tabPane;
     private JPanel psamPanel, sequencePanel;
@@ -31,6 +36,33 @@ public class MatrixReduceViewer implements VisualPlugin {
     private static final int IMAGE_HEIGHT = 284;
     private JTable table;
     private int defaultTableRowHeight;
+    private int selectedPSAM = 0;
+    private ListOrderedMap<String, String> sequences;
+    private ListModel sequenceModel;
+    private int maxSequenceLength = 1;
+    private SequenceGraph[] graphs = null;
+
+    private class ListModel extends AbstractListModel {
+        public int getSize() {
+            if (sequences == null) {
+                return 0;
+            } else {
+                return sequences.size();
+            }
+        }
+
+        public String getElementAt(int index) {
+            return sequences.get(sequences.get(index));
+        }
+
+        public void fireContentsChanged() {
+            if (sequences != null) {
+                super.fireContentsChanged(this, 0, sequences.size() - 1);
+            } else {
+                super.fireContentsChanged(this, 0, 0);
+            }
+        }
+    }
 
     private class TableModel extends AbstractTableModel {
 
@@ -94,7 +126,7 @@ public class MatrixReduceViewer implements VisualPlugin {
                 default:
                     return String.class;
             }
-                    }
+        }
     }
 
     public Component getComponent() {
@@ -143,8 +175,21 @@ public class MatrixReduceViewer implements VisualPlugin {
         table.setDefaultRenderer(ImageIcon.class, new DefaultTableCellRenderer() {
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 // JLabel label = (JLabel)super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                imageLabel.setIcon((Icon)value);
+                imageLabel.setIcon((Icon) value);
                 return imageLabel;
+            }
+        });
+        table.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    int row = table.rowAtPoint(e.getPoint());
+                    if (row != -1) {
+                        selectedPSAM = row;
+                        updateGraphs();
+                        sequenceModel.fireContentsChanged();
+                        tabPane.setSelectedIndex(TAB_SEQUENCE);
+                    }
+                }
             }
         });
         defaultTableRowHeight = table.getRowHeight();
@@ -152,13 +197,61 @@ public class MatrixReduceViewer implements VisualPlugin {
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setColumnHeaderView(table.getTableHeader());
         psamPanel.add(scrollPane, BorderLayout.CENTER);
+        //// Sequence Tab
+        sequenceModel = new ListModel();
+        JList sequenceList = new JList(sequenceModel);
+        sequenceList.getInsets().set(4, 4, 4, 4);
+        sequenceList.setCellRenderer(new DefaultListCellRenderer() {
+            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+//                JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+//                label.setText(value.toString());
+//                return label;
+                return graphs[index];
+            }
+        });
+        sequencePanel.add(new JScrollPane(sequenceList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+    }
+
+    private void updateGraphs() {
+        if ((dataSet != null) && (selectedPSAM < dataSet.size())) {
+            for (SequenceGraph graph : graphs) {
+                graph.createScores(dataSet.get(selectedPSAM), true);
+                graph.createScores(dataSet.get(selectedPSAM), false);
+            }
+        }
     }
 
     @Subscribe public void receive(ProjectEvent projectEvent, Object source) {
         DSDataSet data = projectEvent.getDataSet();
         if ((data != null) && (data instanceof DSMatrixReduceSet)) {
-            dataSet = ((DSMatrixReduceSet)data);
+            dataSet = ((DSMatrixReduceSet) data);
             model.fireTableStructureChanged();
+            sequences = dataSet.getSequences();
+            int n = sequences.size();
+            graphs = new SequenceGraph[n];
+            maxSequenceLength = 1;
+            for (String s : ((Collection<String>) sequences.values())) {
+                if (s.length() > maxSequenceLength) {
+                    maxSequenceLength = s.length();
+                }
+            }
+            // Unregister tooltips
+            if (graphs != null) {
+                for (SequenceGraph graph : graphs) {
+                    if (graph != null) {
+                        ToolTipManager.sharedInstance().unregisterComponent(graph);
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                String label = sequences.get(i);
+                String sequence = sequences.get(label);
+                graphs[i] = new SequenceGraph(sequence, label, maxSequenceLength);
+                ToolTipManager.sharedInstance().registerComponent(graphs[i]);
+            }
+            selectedPSAM = 0;
+            updateGraphs();
+            sequenceModel.fireContentsChanged();
         }
     }
 
