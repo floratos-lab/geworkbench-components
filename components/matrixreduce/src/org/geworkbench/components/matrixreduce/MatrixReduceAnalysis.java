5b9f0be207f0952532f1e179459da79fe9691334
Kiran Keshav <keshav@columbia.edu>
2006-07-10 20:50:27 +0000

    minor import update
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1641 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/matrixreduce/src/org/geworkbench/components/matrixreduce/MatrixReduceAnalysis.java
+++ components/matrixreduce/src/org/geworkbench/components/matrixreduce/MatrixReduceAnalysis.java
@@ -5,14 +5,12 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
