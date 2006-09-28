9ebe18ef85e3c9b5de07bf55eadf51a8b148f694
Xiaoqing Zhang <xiaoqing@columbia.edu>
2006-09-28 20:25:17 +0000

    FIX BUGs
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1908 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/promoter/src/org/geworkbench/components/promoter/MatrixScrollPane.java
+++ components/promoter/src/org/geworkbench/components/promoter/MatrixScrollPane.java
@@ -2,16 +2,20 @@ package org.geworkbench.components.promoter;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableModel;
 import java.awt.BorderLayout;
 import java.text.DecimalFormat;
 
 /**
  * <p>Title: </p>
- *
+ * <p/>
  * <p>Description: </p>
- *
+ * <p/>
  * <p>Copyright: Copyright (c) 2005</p>
- *
+ * <p/>
  * <p>Company: </p>
  *
  * @author not attributable
@@ -20,21 +24,49 @@ import java.text.DecimalFormat;
 public class MatrixScrollPane extends JScrollPane {
     private JTable matrixTable;
     private static final DecimalFormat formatPV = new DecimalFormat("0.00");
+
     public MatrixScrollPane() {
         super();
         matrixTable = new JTable();
 
     }
-    public MatrixScrollPane(JTable table){
+
+    public MatrixScrollPane(JTable table) {
         super(table);
     }
 
     public JTable createMatrixTable(Matrix matrix) {
 
-        String[] cols = createColumnsName(matrix);
-        Object[][] rowsData = createRowsData(matrix);
-        matrixTable = new JTable(rowsData, cols);
-         return matrixTable;
+        final String[] cols = createColumnsName(matrix);
+        final Object[][] rowData = createRowsData(matrix);
+        if(cols==null || rowData==null){
+            return null;
+        }
+
+        TableModel tableModel = new AbstractTableModel() {
+            public String getColumnName(int column) {
+                return cols[column].toString();
+            }
+
+            public int getRowCount() {
+                return rowData.length;
+            }
+
+            public int getColumnCount() {
+                return cols.length;
+            }
+
+            public Object getValueAt(int row, int col) {
+                return rowData[row][col];
+            }
+
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+        };
+
+        matrixTable = new JTable(tableModel);
+        return matrixTable;
     }
 
     /**
@@ -48,12 +80,12 @@ public class MatrixScrollPane extends JScrollPane {
             char[] symbols = matrix.getSymbols();
             int rowLength = symbols.length;
             int columnlength = matrix.getLength();
-            Object[][] cols = new Object[rowLength][columnlength+1];
+            Object[][] cols = new Object[rowLength][columnlength + 1];
             Distribution[] dis = matrix.getRawCountTable();
             for (int i = 0; i < rowLength; i++) {
                 cols[i][0] = new Character(symbols[i]);
                 for (int j = 0; j < columnlength; j++) {
-                    cols[i][j+1] = formatPV.format(dis[j].get(symbols[i]));//)matrix.getMatch(j, symbols[i]);
+                    cols[i][j + 1] = formatPV.format(dis[j].get(symbols[i]));//)matrix.getMatch(j, symbols[i]);
                 }
             }
             return cols;
@@ -74,7 +106,7 @@ public class MatrixScrollPane extends JScrollPane {
             String[] cols = new String[length];
             cols[0] = "    ";
             for (int i = 1; i < length; i++) {
-                cols[i] =  i+ "  ";
+                cols[i] = i + "  ";
             }
             return cols;
         }
