fc8c7792df8bb8cc7fd6fc8bd450ff16c11d1854
Kiran Keshav <keshav@columbia.edu>
2007-01-10 21:50:34 +0000

    minor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2195 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/cagrid/src/org/geworkbench/components/cagrid/CaGridPanel.java
+++ components/cagrid/src/org/geworkbench/components/cagrid/CaGridPanel.java
@@ -58,7 +58,7 @@ import gov.nih.nci.cagrid.metadata.ServiceMetadata;
 /**
  * @author watkinson
  * @author keshav
- * @version $Id: CaGridPanel.java,v 1.8 2007-01-10 21:49:45 keshav Exp $
+ * @version $Id: CaGridPanel.java,v 1.9 2007-01-10 21:50:34 keshav Exp $
  */
 public class CaGridPanel extends JPanel implements VisualPlugin {
 
@@ -384,7 +384,7 @@ public class CaGridPanel extends JPanel implements VisualPlugin {
 							if (somCluster != null) {
 								// convert grid to bison hierarchical cluster
 
-								createBisonSomClustering(somCluster, view);
+								// createBisonSomClustering(somCluster, view);
 								// TODO implement me - for now, doing:
 								FormLayout layout = new FormLayout(
 										"right:max(40dlu;pref), 3dlu, 100dlu, 7dlu",
