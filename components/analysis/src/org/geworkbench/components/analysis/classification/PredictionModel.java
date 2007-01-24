e0cdeb19e103f378e8a6e212be742a39b47d11ab
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-01-24 16:31:17 +0000

    GenePattern prediction model
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2321 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/analysis/src/org/geworkbench/components/analysis/classification/PredictionModel.java
@@ -0,0 +1,84 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2007) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+package org.geworkbench.components.analysis.classification;
+
+import org.genepattern.io.OdfWriter;
+import org.genepattern.io.odf.OdfObject;
+
+import java.io.File;
+
+/**
+ * @author Marc-Danie Nazaire
+ */
+public class PredictionModel
+{
+    OdfObject model;
+    private File predFile;
+
+    public PredictionModel(File fileName)
+    {
+        try
+        {
+            OdfObject odf = new OdfObject(fileName.getAbsolutePath());
+            model = odf;
+
+           fileName.delete();
+        }
+       catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public File getPredModelFile()
+    {
+        try
+        {
+            if(predFile!= null && predFile.exists())
+                return predFile;
+
+            predFile = File.createTempFile("predModel", ".odf", new File(System.getProperty("temporary.files.directory")));
+            predFile.deleteOnExit();
+
+            OdfWriter odfWriter = new OdfWriter(predFile.getAbsolutePath(), null,
+                                    model.getHeader("Model"), model.getIntHeader("DataLines"), false);
+
+            String[] columnTypes = new String[model.getColumnCount()];
+            for(int i=0; i < model.getColumnCount(); i++)
+                columnTypes[i]= model.getColumnClass(i).getSimpleName();
+
+            odfWriter.setColumnTypes(columnTypes);
+
+            String[] assignments = model.getArrayHeader("ASSIGNMENTS");
+            odfWriter.addHeader("ASSIGNMENTS", assignments);
+
+            odfWriter.printHeader();
+            for(int r = 0; r < model.getRowCount(); r++)
+            {
+                for(int c = 0; c < model.getColumnCount(); c++ )
+                {
+                     odfWriter.write(model.getValueAt(r, c).toString());
+                     odfWriter.write("\t");
+                }
+                odfWriter.write(System.getProperty("line.separator"));
+            }
+
+            odfWriter.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return predFile;
+    }
+}
