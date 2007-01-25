556738f247b84ac20b82f753dd02f900173c5f68
Kiran Keshav <keshav@columbia.edu>
2007-01-25 21:33:19 +0000

    removed client-config.wsdd, added an xml encoder
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2338 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/cagrid/src/edu/columbia/geworkbench/cagrid/utils/GridUtils.java
+++ components/cagrid/src/edu/columbia/geworkbench/cagrid/utils/GridUtils.java
@@ -1,11 +1,15 @@
 package edu.columbia.geworkbench.cagrid.utils;
 
+import java.beans.XMLEncoder;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Random;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,79 +18,109 @@ import edu.columbia.geworkbench.cagrid.cluster.parser.TabDelimParser;
 
 /**
  * @author keshav
- * @version $Id: GridUtils.java,v 1.1 2007-01-10 17:14:28 keshav Exp $
+ * @version $Id: GridUtils.java,v 1.2 2007-01-25 21:33:19 keshav Exp $
  */
 public class GridUtils {
-    private static Log log = LogFactory.getLog( GridUtils.class );
+	private static Log log = LogFactory.getLog(GridUtils.class);
 
-    private static TabDelimParser parser = null;
+	private static TabDelimParser parser = null;
 
-    /**
-     * @param is
-     * @return
-     */
-    public static float[][] readTabFile( InputStream is ) {
+	/**
+	 * @param is
+	 * @return
+	 */
+	public static float[][] readTabFile(InputStream is) {
 
-        parser = new TabDelimParser();
-        Collection results = new LinkedHashSet();
-        try {
-            parser.parse( is );
-            results = parser.getResults();
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
+		parser = new TabDelimParser();
+		Collection results = new LinkedHashSet();
+		try {
+			parser.parse(is);
+			results = parser.getResults();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        int i = 0;
-        float[][] values = new float[results.size()][];
+		int i = 0;
+		float[][] values = new float[results.size()][];
 
-        Iterator iter = results.iterator();
-        while ( iter.hasNext() ) {
-            String[] array = ( String[] ) iter.next();
-            values[i] = new float[array.length];
-            for ( int j = 0; j < values[i].length; j++ ) {
-                /*
-                 * FIXME exp files acually contain val | pval, but this is a performance test, so I don't care about the
-                 * actual values. Inspects the data, row by row. If it starts with a character, then this is the header,
-                 * if it ends with _at, then this is a probe name. FIXME I also don't like the _at check here as not all
-                 * probes will end with this. You need something more generic.
-                 */
-                if ( array[j].equalsIgnoreCase( "probe" ) ) break;
-                if ( Character.isLetter( array[j].charAt( 0 ) ) || array[j].endsWith( "_at" ) ) continue;
-                values[i][j] = Float.parseFloat( array[j] );
-            }
-            i++;
-        }
+		Iterator iter = results.iterator();
+		while (iter.hasNext()) {
+			String[] array = (String[]) iter.next();
+			values[i] = new float[array.length];
+			for (int j = 0; j < values[i].length; j++) {
+				/*
+				 * FIXME exp files acually contain val | pval, but this is a
+				 * performance test, so I don't care about the actual values.
+				 * Inspects the data, row by row. If it starts with a character,
+				 * then this is the header, if it ends with _at, then this is a
+				 * probe name. FIXME I also don't like the _at check here as not
+				 * all probes will end with this. You need something more
+				 * generic.
+				 */
+				if (array[j].equalsIgnoreCase("probe"))
+					break;
+				if (Character.isLetter(array[j].charAt(0))
+						|| array[j].endsWith("_at"))
+					continue;
+				values[i][j] = Float.parseFloat(array[j]);
+			}
+			i++;
+		}
 
-        return values;
-    }
+		return values;
+	}
 
-    /**
-     * @param filename
-     * @return float[][]
-     */
-    public static float[][] readTabFile( String filename ) {
+	/**
+	 * @param filename
+	 * @return float[][]
+	 */
+	public static float[][] readTabFile(String filename) {
 
-        InputStream is;
+		InputStream is;
 
-        float[][] values = null;
+		float[][] values = null;
 
-        try {
-            is = new FileInputStream( new File( filename ) );
-            values = readTabFile( is );
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
-        return values;
-    }
+		try {
+			is = new FileInputStream(new File(filename));
+			values = readTabFile(is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return values;
+	}
 
-    /**
-     * @return
-     */
-    public static Object getHeader() {
+	/**
+	 * @return Object
+	 */
+	public static Object getHeader() {
 
-        if ( parser == null ) throw new RuntimeException( "Null header.  Try calling readTabFile first." );
+		if (parser == null)
+			throw new RuntimeException(
+					"Null header.  Try calling readTabFile first.");
 
-        return parser.getHeader();
+		return parser.getHeader();
 
-    }
+	}
+
+	/**
+	 * 
+	 * @param obj
+	 */
+	public static void serializeToXml(Object obj) {
+		log.info("Writing object " + obj + " to xml");
+
+		FileOutputStream fos = null;
+		Random r = new Random();
+		try {
+			fos = new FileOutputStream(obj.getClass().getSimpleName()
+					+ r.nextInt() + ".xml");
+			// Create XML encoder.
+			XMLEncoder xenc = new XMLEncoder(fos);
+			// Write object.
+			xenc.writeObject(obj);
+		} catch (FileNotFoundException e) {
+			log.error("Could not create the FileOutputStream.  Exception is: ");
+			throw new RuntimeException(e);
+		}
+	}
 }
