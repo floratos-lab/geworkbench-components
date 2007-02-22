e70bf77582ddf6a88ae92906f11e20383f38c581
Kiran Keshav <keshav@columbia.edu>
2007-02-22 18:44:31 +0000

    changed HierarchicalClusteringParameter to use enumerations
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2439 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/cagrid/src/edu/columbia/geworkbench/cagrid/cluster/hierarchical/Distance.java
@@ -0,0 +1,72 @@
+/**
+ * Distance.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Mar 03, 2006 (12:17:06 EST) WSDL2Java emitter.
+ */
+
+package edu.columbia.geworkbench.cagrid.cluster.hierarchical;
+
+public class Distance implements java.io.Serializable {
+    private java.lang.String _value_;
+    private static java.util.HashMap _table_ = new java.util.HashMap();
+
+    // Constructor
+    protected Distance(java.lang.String value) {
+        _value_ = value;
+        _table_.put(_value_,this);
+    }
+
+    public static final java.lang.String _euclidean = "euclidean";
+    public static final java.lang.String _spearman = "spearman";
+    public static final java.lang.String _pearson = "pearson";
+    public static final Distance euclidean = new Distance(_euclidean);
+    public static final Distance spearman = new Distance(_spearman);
+    public static final Distance pearson = new Distance(_pearson);
+    public java.lang.String getValue() { return _value_;}
+    public static Distance fromValue(java.lang.String value)
+          throws java.lang.IllegalArgumentException {
+        Distance enumeration = (Distance)
+            _table_.get(value);
+        if (enumeration==null) throw new java.lang.IllegalArgumentException();
+        return enumeration;
+    }
+    public static Distance fromString(java.lang.String value)
+          throws java.lang.IllegalArgumentException {
+        return fromValue(value);
+    }
+    public boolean equals(java.lang.Object obj) {return (obj == this);}
+    public int hashCode() { return toString().hashCode();}
+    public java.lang.String toString() { return _value_;}
+    public java.lang.Object readResolve() throws java.io.ObjectStreamException { return fromValue(_value_);}
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new org.apache.axis.encoding.ser.EnumSerializer(
+            _javaType, _xmlType);
+    }
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new org.apache.axis.encoding.ser.EnumDeserializer(
+            _javaType, _xmlType);
+    }
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(Distance.class);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("gme://cagrid.geworkbench.columbia.edu/1/edu.columbia.geworkbench.cagrid.cluster.hierarchical", "distance"));
+    }
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+}
