9f896413e3926bf50b850fc665c4dcbcdb452ed1
Kiran Keshav <keshav@columbia.edu>
2007-02-22 22:11:05 +0000

    updated classpath, cvsignore, and CaGridConverterTest
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2445 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/cagrid/test/edu/columbia/geworkbench/cagrid/converter/CaGridConverterTest.java
+++ components/cagrid/test/edu/columbia/geworkbench/cagrid/converter/CaGridConverterTest.java
@@ -57,48 +57,6 @@ public class CaGridConverterTest extends TestCase {
 
 	/**
 	 * 
-	 * 
-	 */
-	public void testConvertToCagridBioAssayArray() {
-		float[][] fdata = TabFileReader.readTabFile(is);
-
-		MageBioAssayGenerator mageBioAssayGenerator = new MageBioAssayGeneratorImpl();
-
-		String[] rowNames = new String[fdata.length];
-		for (int i = 0; i < rowNames.length; i++) {
-			rowNames[i] = String.valueOf(i);
-		}
-
-		String[] colNames = new String[fdata[0].length];
-		for (int i = 0; i < colNames.length; i++) {
-			rowNames[i] = String.valueOf(i);
-		}
-		BioAssay[] bioAssays = mageBioAssayGenerator.float2DToBioAssayArray(
-				fdata, rowNames, colNames);
-
-		String cube = null;
-		for (BioAssay bioAssay : bioAssays) {
-			DerivedBioAssay derivedBioAssay = (DerivedBioAssay) bioAssay;
-			DerivedBioAssayData[] derivedBioAssayDatas = derivedBioAssay
-					.getDerivedBioAssayData();
-			for (DerivedBioAssayData derivedBioAssayData : derivedBioAssayDatas) {
-				BioDataValues bioDataValues = derivedBioAssayData
-						.getBioDataValues();
-				cube = ((BioDataCube) bioDataValues).getCube();
-				// byte[] bdata = Base64.decode(cube);
-				// float[] returnedFdata =
-				// BasicConverter.byteArrayToFloats(bdata);
-				// log.debug("float data: " + returnedFdata);
-			}
-
-		}
-
-		// parseBase64Encoding(cube);
-
-	}
-
-	/**
-	 * 
 	 * @param cube
 	 */
 	private void parseBase64Encoding(String cube) {
