be256040b5dabb0acf75743854c377953182b663
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-03-06 15:50:48 +0000

    Added copyright information
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2476 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/analysis/src/org/geworkbench/components/analysis/classification/GPTrainingPanel.java
+++ components/analysis/src/org/geworkbench/components/analysis/classification/GPTrainingPanel.java
@@ -1,3 +1,14 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2007) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
 package org.geworkbench.components.analysis.classification;
 
 import org.geworkbench.algorithms.AbstractTrainingPanel;
