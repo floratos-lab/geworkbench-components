e17f0d6a5b770879e1807151f33abb9a06af4394
Kiran Keshav <keshav@columbia.edu>
2007-03-14 20:32:08 +0000

    added a grid service panel and a listener
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2524 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/analysis/src/org/geworkbench/components/cagrid/gui/GridServicePanel.java
@@ -0,0 +1,209 @@
+package org.geworkbench.components.cagrid.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+
+import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+import edu.columbia.geworkbench.cagrid.discovery.client.DiscoveryServiceUtil;
+import gov.nih.nci.cagrid.discovery.MetadataUtils;
+import gov.nih.nci.cagrid.metadata.ServiceMetadata;
+
+/**
+ * 
+ * @author keshav
+ * @version $Id: GridServicePanel.java,v 1.1 2007-03-14 20:32:08 keshav Exp $
+ */
+public class GridServicePanel extends JPanel {
+	private Log log = LogFactory.getLog(this.getClass());
+
+	JPanel innerPanel = null;
+
+	JPanel outerPanel = null;
+
+	JScrollPane serviceDetailsScrollPane = null;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GridServicePanel(String name) {
+		super();
+		super.setName(name);
+		super.setLayout(new BorderLayout());
+
+		/* part A */
+		DefaultFormBuilder indexServiceBuilder = new DefaultFormBuilder(
+				new FormLayout(""));
+		indexServiceBuilder.appendColumn("right:pref");
+		indexServiceBuilder.appendColumn("10dlu");
+		indexServiceBuilder.appendColumn("right:pref");
+		indexServiceBuilder.appendColumn("10dlu");
+		indexServiceBuilder.appendColumn("right:pref");
+		indexServiceBuilder.appendColumn("10dlu");
+		indexServiceBuilder.appendColumn("right:pref");
+		indexServiceBuilder.appendColumn("10dlu");
+
+		// TODO move this
+		String localButtonString = "Local";
+		JRadioButton localButton = new JRadioButton(localButtonString);
+		localButton.setSelected(true);
+		localButton.setActionCommand(localButtonString);
+		localButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				log.debug("local radio button");
+			}
+		});
+		// TODO move this
+		String localGridString = "Grid";
+		JRadioButton gridButton = new JRadioButton(localGridString);
+		gridButton.setSelected(false);
+		gridButton.setActionCommand(localGridString);
+		gridButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				log.debug("grid radio button");
+			}
+		});
+
+		ButtonGroup buttonGroup = new ButtonGroup();
+		buttonGroup.add(localButton);
+		buttonGroup.add(gridButton);
+
+		indexServiceBuilder.append(localButton);
+		indexServiceBuilder.append(gridButton);
+
+		JLabel indexServiceLabel = new JLabel("Change Index Service");
+
+		indexServiceLabel.setForeground(Color.BLUE);
+
+		final IndexServiceLabelListener indexServiceLabelListener = new IndexServiceLabelListener(
+				indexServiceLabel);
+		indexServiceLabel.addMouseListener(indexServiceLabelListener);
+		indexServiceBuilder.append(indexServiceLabel);
+
+		JButton getServicesButton = indexServiceLabelListener
+				.getIndexServiceButton();
+		indexServiceBuilder.append(getServicesButton);
+
+		/* part B */
+		final DefaultFormBuilder urlServiceBuilder = new DefaultFormBuilder(
+				new FormLayout(""));
+		urlServiceBuilder.setBorder(BorderFactory
+				.createBevelBorder(BevelBorder.LOWERED));
+		urlServiceBuilder.appendColumn("center:pref");
+		urlServiceBuilder.appendColumn("10dlu");
+		urlServiceBuilder.appendColumn("center:pref");
+		urlServiceBuilder.appendColumn("10dlu");
+		urlServiceBuilder.appendColumn("center:pref");
+		urlServiceBuilder.appendColumn("10dlu");
+		urlServiceBuilder.appendColumn("center:pref");
+		urlServiceBuilder.appendColumn("10dlu");
+
+		urlServiceBuilder.append("");
+		urlServiceBuilder.append("Grid Service URL");
+		urlServiceBuilder.append("Research Center Name");
+		urlServiceBuilder.append("Description");
+
+		JScrollPane UrlServiceBuilderScrollPane = new JScrollPane(
+				urlServiceBuilder.getPanel(),
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		final Map<String, EndpointReferenceType> seenServices = new HashMap<String, EndpointReferenceType>(); // TODO
+
+		/* part C */
+		final DefaultFormBuilder serviceDetailsBuilder = new DefaultFormBuilder(
+				new FormLayout(
+						"right:max(60dlu;pref), 3dlu, max(150dlu;pref), 7dlu",
+						""));
+		serviceDetailsBuilder.setBorder(BorderFactory
+				.createBevelBorder(BevelBorder.LOWERED));
+		// serviceDetailsBuilder.appendColumn("left:pref");
+		serviceDetailsBuilder.appendSeparator("Service Details");
+		serviceDetailsBuilder.nextLine();
+		// end C
+
+		// TODO refactor me into a separate listener
+		final ButtonGroup servicesButtonGroup = new ButtonGroup();
+		getServicesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				EndpointReferenceType[] services = DiscoveryServiceUtil
+						.getServices(indexServiceLabelListener.getHost(),
+								indexServiceLabelListener.getPort(),
+								indexServiceLabelListener.getFilter());
+
+				for (EndpointReferenceType service : services) {
+
+					ServiceMetadata commonMetadata;
+					try {
+						commonMetadata = MetadataUtils
+								.getServiceMetadata(service);
+					} catch (Exception e1) {
+						throw new RuntimeException(e1);
+					}
+
+					JRadioButton button = new JRadioButton();
+					servicesButtonGroup.add(button);
+
+					String url = DiscoveryServiceUtil
+							.getUrlFromMetadata(service);
+					String description = DiscoveryServiceUtil
+							.getDescriptionFromMetadata(commonMetadata);
+
+					/* check if we've already seen this service */
+					if (!seenServices.containsKey(url)) {
+						seenServices.put(url, service);
+						urlServiceBuilder.append(button);
+						urlServiceBuilder.append(new JLabel(url));
+						urlServiceBuilder.append(new JLabel("put center here"));
+						urlServiceBuilder.append(new JLabel(description));
+						urlServiceBuilder.nextLine();
+
+						serviceDetailsBuilder.append("Research Center Name: ",
+								new JLabel("put center name"));
+						serviceDetailsBuilder.append("Type: ", new JLabel(
+								"put type here"));
+						serviceDetailsBuilder.append("Description: ",
+								new JLabel(description));
+						serviceDetailsBuilder.append("Contact: ", new JLabel(
+								"put contact here"));
+						serviceDetailsBuilder.append("Contact Number: ",
+								new JLabel("put number here"));
+						serviceDetailsBuilder.append("Address: ", new JLabel(
+								"put address here"));
+
+					}
+				}
+				urlServiceBuilder.getPanel().revalidate();
+			}
+		});
+
+		/* add A, B, and C to the main */
+		this.add(indexServiceBuilder.getPanel(), BorderLayout.NORTH);
+		this.add(UrlServiceBuilderScrollPane);
+		this.add(serviceDetailsBuilder.getPanel(), BorderLayout.SOUTH);
+	}
+
+}
---
+++ components/analysis/src/org/geworkbench/components/cagrid/gui/IndexServiceLabelListener.java
@@ -0,0 +1,201 @@
+package org.geworkbench.components.cagrid.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.util.Util;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+import edu.columbia.geworkbench.cagrid.discovery.client.DiscoveryServiceUtil;
+
+/**
+ * 
+ * @author keshav
+ * @version $Id: IndexServiceLabelListener.java,v 1.1 2007-03-14 20:32:08 keshav Exp $
+ */
+public class IndexServiceLabelListener implements MouseListener {
+	private Log log = LogFactory.getLog(this.getClass());
+
+	private int DEFAULT_PORT = 8080;
+
+	private String DEFAULT_FILTER = "hierarchical";
+
+	private String DEFAULT_HOST = "localhost";
+
+	private JDialog indexServiceDialog = null;
+
+	private String host = DEFAULT_HOST;
+
+	private int port = DEFAULT_PORT;
+
+	private String filter = DEFAULT_FILTER;
+
+	JLabel indexServiceLabel = null;
+
+	JButton indexServiceButton = null;
+
+	public IndexServiceLabelListener(JLabel indexServiceLabel) {
+		super();
+		this.indexServiceLabel = indexServiceLabel;
+
+		indexServiceButton = new JButton("Get Services");
+		indexServiceButton.setVisible(false);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
+	public void mouseClicked(MouseEvent e) {
+		log.debug("changing url");
+
+		// TODO can you refactor a lot of this into a pattern? See GuiTest.
+		indexServiceDialog = new JDialog();
+		DefaultFormBuilder indexServicePanelBuilder = new DefaultFormBuilder(
+				new FormLayout("right:20dlu"));
+		final JTextField hostField = new JTextField(host);
+		final JTextField portField = new JTextField("" + port);
+		final JTextField filterField = new JTextField("" + filter);
+
+		JPanel buttonPanel = new JPanel(new FlowLayout());
+		JButton okButton = new JButton("Ok");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				port = Integer.parseInt(portField.getText());
+				host = hostField.getText();
+				filter = filterField.getText();
+
+				log.info("host: " + host);
+				log.info("port: " + port);
+				log.info("filter: " + filter);
+
+				/* change color for clickable text */
+				// indexServiceLabel.setForeground(Color.magenta);
+				indexServiceButton.setVisible(true);
+
+				indexServiceDialog.dispose();
+
+			}
+		});
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				indexServiceDialog.dispose();
+			}
+		});
+
+		/* add to button panel */
+		buttonPanel.add(okButton);
+		buttonPanel.add(cancelButton);
+
+		/* the builder */
+		indexServicePanelBuilder.appendColumn("5dlu");
+		indexServicePanelBuilder.appendColumn("45dlu");
+
+		indexServicePanelBuilder.append("host", hostField);
+		indexServicePanelBuilder.append("port", portField);
+		indexServicePanelBuilder.append("filter", filterField);
+
+		if (!StringUtils.isEmpty(host))
+			hostField.setText(host);
+
+		JPanel indexServicePanel = new JPanel(new BorderLayout());
+		indexServicePanel.add(indexServicePanelBuilder.getPanel());
+		indexServicePanel.add(buttonPanel, BorderLayout.SOUTH);
+		indexServiceDialog.add(indexServicePanel);
+		indexServiceDialog.setModal(true);
+		indexServiceDialog.pack();
+		Util.centerWindow(indexServiceDialog);
+		indexServiceDialog.setVisible(true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 * @return String
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getFilter() {
+		return filter;
+	}
+
+	/**
+	 * @return JButton
+	 */
+	public JButton getIndexServiceButton() {
+		return indexServiceButton;
+	}
+
+}
