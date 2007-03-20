a2ad108b3a1cab3591119d89ae4242798a55db5c
Kiran Keshav <keshav@columbia.edu>
2007-03-20 19:31:55 +0000

    added a listener for the index service buttons
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2594 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/analysis/src/org/geworkbench/components/cagrid/gui/IndexServiceSelectionButtonListener.java
+++ components/analysis/src/org/geworkbench/components/cagrid/gui/IndexServiceSelectionButtonListener.java
@@ -1,15 +1,63 @@
 package org.geworkbench.components.cagrid.gui;
 
+import edu.columbia.geworkbench.cagrid.discovery.client.DiscoveryServiceUtil;
+import gov.nih.nci.cagrid.discovery.MetadataUtils;
+import gov.nih.nci.cagrid.metadata.ServiceMetadata;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+
+import org.apache.axis.message.addressing.EndpointReferenceType;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
 
 /**
  * 
  * @author keshav
- * @version $Id: IndexServiceSelectionButtonListener.java,v 1.1 2007-03-15 15:41:35 keshav Exp $
+ * @version $Id: IndexServiceSelectionButtonListener.java,v 1.1 2007/03/15
+ *          15:41:35 keshav Exp $
  */
 public class IndexServiceSelectionButtonListener implements ActionListener {
-	String url = null;
+
+	private DefaultFormBuilder serviceDetailsBuilder = null;
+
+	private JScrollPane serviceDetailsBuilderScrollPane = null;
+
+	private String url = null;
+
+	private Map<String, EndpointReferenceType> seenServices = null;
+
+	private ButtonGroup servicesButtonGroup = null;
+
+	/**
+	 * 
+	 * 
+	 */
+	public IndexServiceSelectionButtonListener() {
+
+		seenServices = new HashMap<String, EndpointReferenceType>();
+
+		serviceDetailsBuilder = new DefaultFormBuilder(new FormLayout(
+				"right:max(60dlu;pref), 3dlu, max(150dlu;pref), 7dlu", ""));
+		serviceDetailsBuilder.setBorder(BorderFactory
+				.createBevelBorder(BevelBorder.LOWERED));
+		serviceDetailsBuilder.appendSeparator("Service Details");
+		serviceDetailsBuilder.nextLine();
+
+		serviceDetailsBuilderScrollPane = new JScrollPane(serviceDetailsBuilder
+				.getPanel(), ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -18,7 +66,49 @@ public class IndexServiceSelectionButtonListener implements ActionListener {
 	 */
 	public void actionPerformed(ActionEvent e) {
 		url = e.getActionCommand();
-		System.out.println("action command: " + url);
+
+		for (String serviceUrl : seenServices.keySet()) {
+			if (serviceUrl.equalsIgnoreCase(url)) {
+
+				// serviceDetailsBuilder.getPanel().removeAll();
+
+				EndpointReferenceType service = seenServices.get(serviceUrl);
+
+				ServiceMetadata commonMetadata;
+				try {
+					commonMetadata = MetadataUtils.getServiceMetadata(service);
+				} catch (Exception e1) {
+					throw new RuntimeException(e1);
+				}
+
+				String researchCenter = DiscoveryServiceUtil
+						.getResearchCenterName(commonMetadata);
+				String description = DiscoveryServiceUtil
+						.getDescription(commonMetadata);
+				String type = DiscoveryServiceUtil.getType(commonMetadata);
+				String contact = DiscoveryServiceUtil
+						.getContactName(commonMetadata);
+				String contactNumber = DiscoveryServiceUtil
+						.getContactNumber(commonMetadata);
+				String address = DiscoveryServiceUtil
+						.getAddress(commonMetadata);
+
+				serviceDetailsBuilder.append("Research Center Name: ",
+						new JLabel(researchCenter));
+				serviceDetailsBuilder.append("Type: ", new JLabel(type));
+				serviceDetailsBuilder.append("Description: ", new JLabel(
+						description));
+				serviceDetailsBuilder.append("Contact: ", new JLabel(contact));
+				serviceDetailsBuilder.append("Contact Number: ", new JLabel(
+						contactNumber));
+				serviceDetailsBuilder.append("Address: ", new JLabel(address));
+
+				serviceDetailsBuilder.getPanel().revalidate();
+
+				break;
+			}
+		}
+
 	}
 
 	/**
@@ -27,6 +117,42 @@ public class IndexServiceSelectionButtonListener implements ActionListener {
 	 */
 	public String getUrl() {
 		return url;
+	}
+
+	public Map<String, EndpointReferenceType> getSeenServices() {
+		return seenServices;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public ButtonGroup getServicesButtonGroup() {
+		return servicesButtonGroup;
+	}
+
+	/**
+	 * 
+	 * @param servicesButtonGroup
+	 */
+	public void setServicesButtonGroup(ButtonGroup servicesButtonGroup) {
+		this.servicesButtonGroup = servicesButtonGroup;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public DefaultFormBuilder getServiceDetailsBuilder() {
+		return serviceDetailsBuilder;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public JScrollPane getServiceDetailsBuilderScrollPane() {
+		return serviceDetailsBuilderScrollPane;
 	}
 
 }
