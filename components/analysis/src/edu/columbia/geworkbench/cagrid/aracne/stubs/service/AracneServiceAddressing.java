985281e1ae2da3859f1d3633fcbb79e3e0e6c914
Kiran Keshav <keshav@columbia.edu>
2007-04-02 17:16:50 +0000

    minor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2643 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/analysis/src/edu/columbia/geworkbench/cagrid/aracne/stubs/service/AracneServiceAddressing.java
@@ -0,0 +1,14 @@
+/**
+ * AracneServiceAddressing.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Mar 03, 2006 (12:17:06 EST) WSDL2Java emitter.
+ */
+
+package edu.columbia.geworkbench.cagrid.aracne.stubs.service;
+
+public interface AracneServiceAddressing extends edu.columbia.geworkbench.cagrid.aracne.stubs.service.AracneService {
+    public edu.columbia.geworkbench.cagrid.aracne.stubs.AracnePortType getAracnePortTypePort(org.apache.axis.message.addressing.EndpointReferenceType reference) throws javax.xml.rpc.ServiceException;
+
+
+}
