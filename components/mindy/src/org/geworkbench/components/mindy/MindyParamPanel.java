ffc8cc207e43dbbc6903eac33e6c56dc0219c2de
Matt Hall <mhall@columbia.edu>
2007-04-17 21:52:11 +0000

    Analysis panel parameter changes, error checking.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2694 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/mindy/src/org/geworkbench/components/mindy/MindyParamPanel.java
+++ components/mindy/src/org/geworkbench/components/mindy/MindyParamPanel.java
@@ -1,13 +1,15 @@
 package org.geworkbench.components.mindy;
 
 import org.geworkbench.analysis.AbstractSaveableParameterPanel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.swing.*;
-import java.io.Serializable;
-import java.io.File;
+import java.io.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.builder.DefaultFormBuilder;
@@ -17,16 +19,24 @@ import com.jgoodies.forms.builder.DefaultFormBuilder;
  */
 public class MindyParamPanel extends AbstractSaveableParameterPanel implements Serializable {
 
+    static Log log = LogFactory.getLog(MindyParamPanel.class);
+
     private JButton loadResultsButton = new JButton("Load...");
+    private JButton loadModulatorsFile = new JButton("Load");
+    private JButton loadDPIAnnotationFile = new JButton("Load");
     private String candidateModulatorsFile = new String("data/mindy/candidateModulators.txt");
 
+    private JTextField modulatorList = new JTextField("");
+    private JTextField dpiAnnotationList = new JTextField("");
     private JTextField setFraction = new JTextField("35");
-    private JTextField subsetKernelWidth = new JTextField("-1");
     private JTextField subsetMIThreshold = new JTextField("-1");
+    private JTextField subsetPValue = new JTextField("-1");
     private JTextField dpiTolerance = new JTextField("0.1");
-    private JTextField fullsetKernelWidth = new JTextField("-1");
     private JTextField fullsetMIThreshold = new JTextField("-1");
+    private JTextField fullsetPValue = new JTextField("-1");
     private JTextField transcriptionFactor = new JTextField("1973_s_at");
+    private String modulatorFile = "data/mindy/candidate_modulator.lst";
+    private String dpiAnnotationFile = "data/mindy/transcription_factor.lst";
 
     public MindyParamPanel() {
         this.setLayout(new BorderLayout());
@@ -36,16 +46,83 @@ public class MindyParamPanel extends AbstractSaveableParameterPanel implements S
         DefaultFormBuilder builder = new DefaultFormBuilder(layout);
         builder.setDefaultDialogBorder();
         builder.appendSeparator("MINDY Paramaters");
-        builder.append("Set Fraction", setFraction);
-        builder.append("Subset Kernel Width", subsetKernelWidth);
-        builder.append("Subset MI Threshold", subsetMIThreshold);
+        builder.append("Conditional Gene List", modulatorList, 3);
+        builder.append(loadModulatorsFile);
+
+        builder.append("DPI Annotation List", dpiAnnotationList, 3);
+        builder.append(loadDPIAnnotationFile);
+
+        builder.append("Hub Gene", transcriptionFactor);
+        builder.append("Conditional Mutual Info.", subsetMIThreshold);
+        builder.append("Sample per Condition (%)", setFraction);
+        builder.append("Conditional P-Value", subsetPValue);
         builder.append("DPI Tolerance", dpiTolerance);
-        builder.append("Full Set Kernel Width", fullsetKernelWidth);
-        builder.append("Full Set Mi Threshold", fullsetMIThreshold);
-        builder.append("Candidate Modulators", loadResultsButton);
-        builder.append("Transcription Factor", transcriptionFactor);
+        builder.append("Unconditional Mutual Info.", fullsetMIThreshold);
+        builder.append("Unconditional P-Value", fullsetPValue);
         builder.nextRow();
         this.add(builder.getPanel());
+
+        loadModulatorsFile.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+//                hubGenes = new ArrayList<String>();
+                StringBuilder geneListBuilder = new StringBuilder();
+                try {
+                    File hubFile = new File(modulatorFile);
+                    JFileChooser chooser = new JFileChooser(hubFile.getParent());
+                    chooser.showOpenDialog(MindyParamPanel.this);
+                    if (chooser.getSelectedFile().getPath() != null) {
+                        modulatorFile = chooser.getSelectedFile().getPath();
+
+                        BufferedReader reader = new BufferedReader(new FileReader(modulatorFile));
+                        String hub = reader.readLine();
+                        while (hub != null && !"".equals(hub)) {
+                        //                        hubGenes.add(hub);
+                            geneListBuilder.append(hub + ", ");
+                            hub = reader.readLine();
+                        }
+
+                        String geneString = geneListBuilder.toString();
+                        modulatorList.setText(geneString.substring(0, geneString.length() - 2));
+                    }
+
+                } catch (IOException e) {
+                    log.error(e);
+                }
+
+            }
+        });
+
+        loadDPIAnnotationFile.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+//                hubGenes = new ArrayList<String>();
+                StringBuilder geneListBuilder = new StringBuilder();
+                try {
+                    File hubFile = new File(dpiAnnotationFile);
+                    JFileChooser chooser = new JFileChooser(hubFile.getParent());
+                    chooser.showOpenDialog(MindyParamPanel.this);
+                    if (chooser.getSelectedFile().getPath() != null) {
+                        dpiAnnotationFile = chooser.getSelectedFile().getPath();
+
+                        BufferedReader reader = new BufferedReader(new FileReader(dpiAnnotationFile));
+                        String hub = reader.readLine();
+                        while (hub != null && !"".equals(hub)) {
+                        //                        hubGenes.add(hub);
+                            geneListBuilder.append(hub + ", ");
+                            hub = reader.readLine();
+                        }
+
+                        String geneString = geneListBuilder.toString();
+                        dpiAnnotationList.setText(geneString.substring(0, geneString.length() - 2));
+                    }
+
+                } catch (IOException e) {
+                    log.error(e);
+                }
+
+            }
+        });
+
+
         loadResultsButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 File file = new File(candidateModulatorsFile);
@@ -64,27 +141,51 @@ public class MindyParamPanel extends AbstractSaveableParameterPanel implements S
         return Integer.valueOf(setFraction.getText());
     }
 
-    public float getSubsetKernelWidth() {
-        return Float.valueOf(subsetKernelWidth.getText());
-    }
-
     public float getSubsetMIThreshold() {
         return Float.valueOf(subsetMIThreshold.getText());
+    }
+
+    public float getSubsetPValueThreshold() {
+        return Float.valueOf(subsetPValue.getText());
     }
 
     public float getDPITolerance() {
         return Float.valueOf(dpiTolerance.getText());
     }
 
-    public float getFullSetKernelWidth() {
-        return Float.valueOf(fullsetKernelWidth.getText());
-    }
-
     public float getFullSetMIThreshold() {
         return Float.valueOf(fullsetMIThreshold.getText());
+    }
+
+    public float getFullsetPValueThreshold() {
+        return Float.valueOf(fullsetPValue.getText());
     }
 
     public String getTranscriptionFactor() {
         return transcriptionFactor.getText();
     }
+
+    public ArrayList<String> getModulatorGeneList() {
+        String geneString = modulatorList.getText();
+        ArrayList<String> geneList = breakStringIntoGenes(geneString);
+        return geneList;
+    }
+
+    public ArrayList<String> getDPIAnnotatedGeneList() {
+        String geneString = dpiAnnotationList.getText();
+        ArrayList<String> geneList = breakStringIntoGenes(geneString);
+        return geneList;
+    }
+
+    private ArrayList<String> breakStringIntoGenes(String geneString) {
+        String[] genes = geneString.split(",");
+        ArrayList<String> geneList = new ArrayList<String>();
+        for (String gene : genes) {
+            if (gene != null && !"".equals(gene)) {
+                geneList.add(gene.trim());
+            }
+        }
+        return geneList;
+    }
+
 }
