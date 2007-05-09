171db26ea7e77fcbf3fc44f1c78efa8dedb24ce2
Kiran Keshav <keshav@columbia.edu>
2007-05-09 19:40:31 +0000

    added test to test using the config file
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2798 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/medusa/test/org/geworkbench/components/medusa/MedusaAnalysisTest.java
+++ components/medusa/test/org/geworkbench/components/medusa/MedusaAnalysisTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
 /**
  * 
  * @author keshav
- * @version $Id: MedusaAnalysisTest.java,v 1.5 2007-05-09 16:31:57 keshav Exp $
+ * @version $Id: MedusaAnalysisTest.java,v 1.6 2007-05-09 19:40:31 keshav Exp $
  */
 public class MedusaAnalysisTest extends TestCase {
 	MedusaParamPanel panel = null;
@@ -22,54 +22,60 @@ public class MedusaAnalysisTest extends TestCase {
 		analysis.setLabel("MEDUSA");
 	}
 
-	/**
-	 * 
-	 * 
-	 */
-	public void testExecute() {
-		panel.setFeaturesFile("data/test/dataset/small_yeast/yeast_test.fasta");
-
-		JTextField minKmer = new JTextField();
-		minKmer.setText(String.valueOf(3));
-		panel.setMinKmerTextField(minKmer);
-
-		JTextField maxKmer = new JTextField();
-		maxKmer.setText(String.valueOf(5));
-		panel.setMaxKmerTextField(maxKmer);
-
-		JComboBox dimersCombo = new JComboBox(new String[] { "No", "Yes" });
-		// dimersCombo.setSelectedIndex(0);
-		panel.setDimersCombo(dimersCombo);
-
-		JTextField minGap = new JTextField();
-		minGap.setText(String.valueOf(0));
-		panel.setDimerMinGapTextField(minGap);
-
-		JTextField maxGap = new JTextField();
-		maxGap.setText(String.valueOf(3));
-		panel.setDimerMaxGapTextField(maxGap);
-
-		JTextField iter = new JTextField();
-		iter.setText(String.valueOf(10));
-		panel.setBoostingIterationsTextField(iter);
-
-		JTextField pssm = new JTextField();
-		pssm.setText(String.valueOf(18));
-		panel.setPssmLengthTextField(pssm);
-
-		JTextField aggTextField = new JTextField();
-		aggTextField.setText(String.valueOf(5));
-		panel.setAggTextField(aggTextField);
-
-		JComboBox reverseComplementCombo = new JComboBox(new String[] { "True",
-				"False" });
-		// reverseComplementCombo.setSelectedIndex(0);
-		panel.setReverseComplementCombo(reverseComplementCombo);
-
+	public void testExecuteUsingConfigFile() {
+		panel.setConfigFilePath("data/test/dataset/config.xml");
 		analysis.setDefaultPanel(panel);
-		analysis.setIFile("data/test/dataset/config.xml");
 		analysis.execute(null);
-		// analysis.printhelp();
 	}
+
+	// /**
+	// *
+	// *
+	// */
+	// public void testExecute() {
+	//
+	// panel.setFeaturesFile("data/test/dataset/small_yeast/yeast_test.fasta");
+	//
+	// JTextField minKmer = new JTextField();
+	// minKmer.setText(String.valueOf(3));
+	// panel.setMinKmerTextField(minKmer);
+	//
+	// JTextField maxKmer = new JTextField();
+	// maxKmer.setText(String.valueOf(5));
+	// panel.setMaxKmerTextField(maxKmer);
+	//
+	// JComboBox dimersCombo = new JComboBox(new String[] { "No", "Yes" });
+	// // dimersCombo.setSelectedIndex(0);
+	// panel.setDimersCombo(dimersCombo);
+	//
+	// JTextField minGap = new JTextField();
+	// minGap.setText(String.valueOf(0));
+	// panel.setDimerMinGapTextField(minGap);
+	//
+	// JTextField maxGap = new JTextField();
+	// maxGap.setText(String.valueOf(3));
+	// panel.setDimerMaxGapTextField(maxGap);
+	//
+	// JTextField iter = new JTextField();
+	// iter.setText(String.valueOf(10));
+	// panel.setBoostingIterationsTextField(iter);
+	//
+	// JTextField pssm = new JTextField();
+	// pssm.setText(String.valueOf(18));
+	// panel.setPssmLengthTextField(pssm);
+	//
+	// JTextField aggTextField = new JTextField();
+	// aggTextField.setText(String.valueOf(5));
+	// panel.setAggTextField(aggTextField);
+	//
+	// JComboBox reverseComplementCombo = new JComboBox(new String[] { "True",
+	// "False" });
+	// // reverseComplementCombo.setSelectedIndex(0);
+	// panel.setReverseComplementCombo(reverseComplementCombo);
+	//
+	// analysis.setDefaultPanel(panel);
+	// analysis.execute(null);
+	// // analysis.printhelp();
+	// }
 
 }
