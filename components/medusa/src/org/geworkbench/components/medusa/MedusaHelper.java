77ef81dcdee5055f5a677cbeac756955e1335b3c
Kiran Keshav <keshav@columbia.edu>
2007-05-14 14:56:45 +0000

    modified to correctly obtain the markers from the view.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2834 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/medusa/src/org/geworkbench/components/medusa/MedusaHelper.java
+++ components/medusa/src/org/geworkbench/components/medusa/MedusaHelper.java
@@ -8,13 +8,13 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
+import org.geworkbench.bison.datastructure.bioobjects.markers.CSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
-import org.geworkbench.bison.datastructure.complex.panels.DSItemList;
 
 /**
  * 
  * @author keshav
- * @version $Id: MedusaHelper.java,v 1.1 2007-05-11 17:00:16 keshav Exp $
+ * @version $Id: MedusaHelper.java,v 1.2 2007-05-14 14:56:45 keshav Exp $
  */
 public class MedusaHelper {
 
@@ -30,29 +30,34 @@ public class MedusaHelper {
 		boolean pass = true;
 		try {
 
-			DSItemList<DSGeneMarker> markers = (DSItemList<DSGeneMarker>) microarraySetView
-					.markers();
-
 			out = new BufferedWriter(new FileWriter(filename));
 
-			for (DSGeneMarker marker : markers) {
-				double[] data = microarraySetView.getRow(marker);
+			int markerSize = microarraySetView.getMarkerPanel().size();
+
+			for (int i = 0; i < markerSize; i++) {
+
+				DSGeneMarker marker = (CSGeneMarker) microarraySetView
+						.getMarkerPanel().get(i);
+				double[] data = microarraySetView.getRow(i);
+
+				if (data == null)
+					continue;
 
 				if (regulatorNames.contains(marker.getLabel())) {
 					out.write('R');
 				} else if (targetNames.contains(marker.getLabel())) {
 					out.write('T');
 				} else {
-					log.info("marker " + marker.getLabel()
-							+ " neither regulator nor target.");
+					log.info("Marker " + marker.getLabel()
+							+ " neither regulator nor target ... skipping.");
 					continue;
 				}
 				out.write(TAB_SEPARATOR);
 				out.write(marker.getLabel());
 				out.write(TAB_SEPARATOR);
-				for (int i = 0; i < data.length; i++) {
-					out.write(String.valueOf(data[i]));
-					if (i < data.length - 1)
+				for (int j = 0; j < data.length; j++) {
+					out.write(String.valueOf(data[j]));
+					if (j < data.length - 1)
 						out.write(TAB_SEPARATOR);
 					else
 						out.write("\n");
