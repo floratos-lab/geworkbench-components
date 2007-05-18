8b15e562f00b9f4154065476d6099beb04fa2257
Kiran Keshav <keshav@columbia.edu>
2007-05-18 21:34:56 +0000

    minor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2870 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/medusa/src/org/geworkbench/components/medusa/MedusaPlugin.java
+++ components/medusa/src/org/geworkbench/components/medusa/MedusaPlugin.java
@@ -1,14 +1,24 @@
 package org.geworkbench.components.medusa;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.components.medusa.heatmap.MedusaHeatMap;
+
+import ubic.basecode.dataStructure.matrix.DenseDoubleMatrix2DNamed;
+import ubic.basecode.dataStructure.matrix.DoubleMatrixNamed;
+import ubic.basecode.gui.ColorMatrix;
+import ubic.basecode.gui.JMatrixDisplay;
+
 /**
  * 
  * @author keshav
- * @version $Id: MedusaPlugin.java,v 1.3 2007-05-15 20:39:27 keshav Exp $
+ * @version $Id: MedusaPlugin.java,v 1.4 2007-05-18 21:34:56 keshav Exp $
  */
 public class MedusaPlugin extends JPanel {
 
@@ -26,6 +36,35 @@ public class MedusaPlugin extends JPanel {
 		JTabbedPane tabbedPane = new JTabbedPane();
 
 		JPanel motifPanel = new JPanel();
+
+		//MedusaHeatMap heatMap = new MedusaHeatMap(Color.gray, Color.gray,
+		//		medusaData);
+		// motifPanel.add(heatMap);
+		// int i = 0;
+		// List<DSGeneMarker> targets = medusaData.getTargets();
+		// double[][] rawMatrix = new double[targets.size()][];
+		// for (DSGeneMarker target : targets) {
+		// double[] data = medusaData.getArraySet().getRow(target);
+		// rawMatrix[i] = data;
+		// i++;
+		// }
+		//
+		// int j = 0;
+		// DoubleMatrixNamed namedMatrix = new
+		// DenseDoubleMatrix2DNamed(rawMatrix);
+		// for (DSGeneMarker target : targets) {
+		// namedMatrix.addRowName(target.getLabel(), j);
+		// j++;
+		// }
+		//
+		// for (int k = 0; i < rawMatrix[0].length; k++) {
+		// namedMatrix.addColumnName(String.valueOf(k), k);
+		// }
+		//
+		// ColorMatrix colorMatrix = new ColorMatrix(namedMatrix);
+		// JMatrixDisplay matrixDisplay = new JMatrixDisplay(colorMatrix);
+		//		motifPanel.add(matrixDisplay);
+
 		tabbedPane.add("Motif", motifPanel);
 
 		JPanel pssmPanel = new JPanel();
@@ -33,9 +72,6 @@ public class MedusaPlugin extends JPanel {
 
 		setLayout(new BorderLayout());
 		add(tabbedPane, BorderLayout.CENTER);
-		
-		//ColorMosaicImage image = new ColorMosaicImage();
-		
-	}
 
+	}
 }
