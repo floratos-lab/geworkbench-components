bd64a5b7a6e7e2f0aa0eb5a7074e46a366648822
Kiran Keshav <keshav@columbia.edu>
2007-05-22 04:42:49 +0000

    tests the rules beans
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2890 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/medusa/test/org/geworkbench/components/medusa/pssm/RulesBeanTest.java
@@ -0,0 +1,75 @@
+package org.geworkbench.components.medusa.pssm;
+
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.components.medusa.MedusaHelper;
+
+import edu.columbia.ccls.medusa.io.RuleParser;
+import edu.columbia.ccls.medusa.io.SerializedRule;
+
+/**
+ * Tests for pssm related "stuff", such as reading (previously generated) MEDUSA
+ * rule files, generating consensue sequences, etc.
+ * 
+ * @author keshav
+ * @version $Id: RulesBeanTest.java,v 1.1 2007-05-22 04:42:49 keshav Exp $
+ */
+public class RulesBeanTest extends TestCase {
+	private Log log = LogFactory.getLog(this.getClass());
+
+	RuleParser parser = null;
+
+	String rulesPath = "data/test/dataset/pssm/rules/";
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Read 1 rule.
+	 * 
+	 */
+	public void testRead() {
+		boolean fail = false;
+		SerializedRule srule = null;
+		try {
+			srule = RuleParser.read(rulesPath + "rule_0.xml");
+		} catch (IOException e) {
+			fail = true;
+			e.printStackTrace();
+		} finally {
+			assertFalse(fail);
+		}
+
+		double data[][] = srule.getPssm();
+
+		// MedusaHelper.printData(data);
+
+		MedusaHelper.generateConsensusSequence(data);
+	}
+
+	/**
+	 * Read multiple rules files.
+	 * 
+	 */
+	public void testReadAll() {
+		boolean fail = false;
+		List<SerializedRule> srules = null;
+		try {
+			srules = RuleParser.readAll(rulesPath);
+		} catch (IOException e) {
+			fail = true;
+			e.printStackTrace();
+		} finally {
+			assertFalse(fail);
+		}
+
+		assertEquals(2, srules.size());
+	}
+}
