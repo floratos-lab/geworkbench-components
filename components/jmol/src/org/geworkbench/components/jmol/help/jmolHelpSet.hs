19790b331f82fd99e5d3268a9454d19e71331ca2
edaly <edaly@columbia.edu>
2007-06-08 17:28:46 +0000

    updated toc
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2974 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/jmol/src/org/geworkbench/components/jmol/help/jmolHelpSet.hs
+++ components/jmol/src/org/geworkbench/components/jmol/help/jmolHelpSet.hs
@@ -6,18 +6,18 @@
 <?TestTarget The help set file for the project tree component.?>
 <helpset version="1.0">
 	<!-- title -->
-	<title>Cellular NetWork</title>
+	<title>JMOL</title>
 	<!-- maps -->
 	<maps>
 		<homeID>overview</homeID>
-		<mapref location="cellular.jhm"/>
+		<mapref location="jmol.jhm"/>
 	</maps>
 	<!-- views -->
 	<view>
 		<name>TOC</name>
-		<label>Cellular NetWork</label>
+		<label>JMOL</label>
 		<type>javax.help.TOCView</type>
-		<data>cellulartoc.xml</data>
+		<data>jmoltoc.xml</data>
 	</view>
 </helpset>
 
