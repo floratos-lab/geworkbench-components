e8596ac4a83aa2b7e172da8862e668aae93e3485
Christine Hung <hungc@columbia.edu>
2007-07-10 17:54:22 +0000

    Added javadoc comments.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3089 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/mindy/src/org/geworkbench/components/mindy/GeneMarkerListComparator.java
+++ components/mindy/src/org/geworkbench/components/mindy/GeneMarkerListComparator.java
@@ -4,14 +4,39 @@ import java.util.Comparator;
 import org.geworkbench.bison.datastructure.bioobjects.markers.*;
 import org.geworkbench.util.pathwaydecoder.mutualinformation.MindyData;
 
+/**
+ * Compare two gene markers (for sorting).
+ * @author ch2514
+ * @version $Id: GeneMarkerListComparator.java,v 1.2 2007-07-10 17:54:02 hungc Exp $
+ */
 public class GeneMarkerListComparator implements Comparator<DSGeneMarker> {
-	// modes
+	/**
+	 * Sorting mode - compare two gene markers based on their short names.
+	 */
     public static final int SHORT_NAME = 1;
+    /**
+     * Sorting mode - compare two gene markers based on their M# stat.
+     */
     public static final int M_POUND = 2;
+    /**
+     * Sorting mode - compare two gene markers based on their M+ stat.
+     */
     public static final int M_PLUS = 3;
+    /**
+     * Sorting mode - compare two gene markers based on their M- stat.
+     */
     public static final int M_MINUS = 4;
+    /**
+     * Sorting mode - compare two gene markers based on their descriptions.
+     */
     public static final int DESCRIPTION = 5;
+    /**
+     * Sorting mode - compare two gene markers based on their scores.
+     */
     public static final int SCORE = 6;
+    /**
+     * Sorting mode - compare two gene markers based on their modes.
+     */
     public static final int MODE = 7;
     
     // variables
@@ -20,17 +45,40 @@ public class GeneMarkerListComparator implements Comparator<DSGeneMarker> {
     private boolean ascending;
     private DSGeneMarker modulator;
 	
+    /** 
+     * @param md - MINDY data
+     * @param mode - sorting mode
+     * @param ascending - (for sorting).  If true, the gene markers are sorted in
+	 * ascending order based on the sorting mode.
+     */
 	public GeneMarkerListComparator(MindyData md, int mode, boolean ascending){
 		this.md = md;
 		this.mode = mode;
 		this.ascending = ascending;
 	}
 	
+	/** 
+	 * @param md - MINDY data
+	 * @param modulator - modulator with which to calculate the score of a marker
+	 * @param mode - sorting mode
+	 * @param ascending - (for sorting).  If true, the gene markers are sorted in
+	 * ascending order based on the sorting mode.
+	 */
 	public GeneMarkerListComparator(MindyData md, DSGeneMarker modulator, int mode, boolean ascending){
 		this(md, mode, ascending);
 		this.modulator = modulator;
 	}
 
+	/**
+	 * Compares two gene markers based on the mode specified in the constructor.
+	 * This method is for Collections sorting to call.
+	 * 
+	 * @param x - the first gene marker to be compared
+     * @param y - the second gene marker to be compared
+     * @return A negative integer if the first gene marker precedes the second.
+     * Zero if the two markers are the same.  
+     * A positive integer if the second marker precedes the first.
+	 */
 	public int compare(DSGeneMarker x, DSGeneMarker y) {
         switch(mode){
         case SHORT_NAME:
--- components/mindy/src/org/geworkbench/components/mindy/MindyRowComparator.java
+++ components/mindy/src/org/geworkbench/components/mindy/MindyRowComparator.java
@@ -4,19 +4,41 @@ import java.util.Comparator;
 
 import org.geworkbench.util.pathwaydecoder.mutualinformation.MindyData;
 
+/**
+ * Compare two MINDY result rows (for sorting).
+ * @author ch2514
+ * @version $Id: MindyRowComparator.java,v 1.3 2007-07-10 17:54:22 hungc Exp $
+ */
 public class MindyRowComparator implements Comparator<MindyData.MindyResultRow> {
-	// mode
+	/**
+	 * Sorting mode - compare MINDY result rows based on their scores. 
+	 */
 	public static final int SCORE = 1;
     
 	// variable
     private int mode;
     private boolean ascending;
     
+    /**
+     * @param mode - how to compare two rows
+     * @param ascending (for sorting) - If true, the rows are sorted in ascending order
+     * as per sorting mode.
+     */
     public MindyRowComparator(int mode, boolean ascending){
     	this.mode = mode;
     	this.ascending = ascending;
     }
 
+    /**
+     * Compares two MINDY result rows based on the mode specified in the constructor.
+     * This method is for Collections sorting to call.
+     * 
+     * @param x - the first MINDY result row to be compared
+     * @param y - the second MINDY result row to be compared
+     * @return A negative integer if the first MINDY result row precedes the second.
+     * Zero if the two rows are the same.  
+     * A positive integer if the second row precedes the first.
+     */
 	public int compare(MindyData.MindyResultRow x, MindyData.MindyResultRow y) {
         switch(mode){
         case SCORE:
