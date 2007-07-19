67d4909ebbcfa17487c2a86b29ff6b44f3eaf042
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-07-19 15:41:39 +0000

    Analysis panel for GenePattern modules
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3118 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/GPAnalysisPanel.java
@@ -0,0 +1,59 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2007) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+package org.geworkbench.components.gpmodule;
+
+import org.geworkbench.bison.model.analysis.ParameterPanel;
+import org.geworkbench.analysis.AbstractSaveableParameterPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author: Marc-Danie Nazaire
+ */
+public abstract class GPAnalysisPanel extends AbstractSaveableParameterPanel {
+    private JTabbedPane gpTabbedPane;
+    private GPConfigPanel gpConfigPanel;
+    private GPHelpPanel gpHelpPanel;
+    protected ParameterPanel parameterPanel;
+
+    public GPAnalysisPanel(ParameterPanel panel, String label)
+    {
+       gpTabbedPane = new JTabbedPane();
+       gpConfigPanel = new GPConfigPanel();
+       gpHelpPanel = new GPHelpPanel(label, getParamDescriptionFile(), getDescriptionFile()); 
+       parameterPanel = panel;
+    }
+
+    protected abstract void initParameterPanel();
+    protected abstract String getDescriptionFile();
+    protected abstract String getParamDescriptionFile();
+
+
+    protected void init()
+    {
+        setLayout(new BorderLayout());
+
+        initParameterPanel();
+
+        gpTabbedPane.addTab("Parameters", parameterPanel);
+        gpTabbedPane.addTab("GenePattern Server Settings", gpConfigPanel);
+        gpTabbedPane.addTab("Help", gpHelpPanel);
+
+        add(gpTabbedPane, BorderLayout.PAGE_START);
+    }
+
+    public String getPassword()
+    {
+        return gpConfigPanel.getPassword();
+    }    
+}
