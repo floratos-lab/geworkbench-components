ed49894147a8e81c176147bf30fbb212a37b494d
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-07-20 17:02:43 +0000

    analysis panel for Principal Components Analysis
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3123 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/pca/PCAAnalysisPanel.java
@@ -0,0 +1,76 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2007) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+package org.geworkbench.components.gpmodule.pca;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+import org.geworkbench.analysis.AbstractSaveableParameterPanel;
+import org.geworkbench.components.gpmodule.GPAnalysisPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author: Marc-Danie Nazaire
+ */
+public class PCAAnalysisPanel extends GPAnalysisPanel
+{
+    private JComboBox clusterBy;
+    
+    public PCAAnalysisPanel()
+     {
+        super(new AbstractSaveableParameterPanel(), "PCA");
+         
+        try
+        {
+            init();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void initParameterPanel()
+    {
+        clusterBy = new JComboBox();
+        clusterBy.addItem("rows");
+        clusterBy.addItem("columns");
+
+        FormLayout formLayout = new FormLayout("right:max(80dlu;pref), 7dlu,  max(70dlu;pref), 7dlu, max(70dlu;pref)");
+
+        DefaultFormBuilder builder = new DefaultFormBuilder(formLayout);
+        builder.setDefaultDialogBorder();
+
+        builder.appendSeparator("Principal Components Analysis parameters");
+        builder.nextLine();
+
+        builder.append("cluster by", clusterBy);
+
+        parameterPanel.add(builder.getPanel(), BorderLayout.LINE_START);
+    }
+
+    public String getClusterBy()
+    {
+        return (String)clusterBy.getSelectedItem();
+    }
+
+    protected String getParamDescriptionFile()
+    {
+        return PCAAnalysisPanel.class.getResource("paramDesc.html").getPath();
+    }
+
+    protected String getDescriptionFile()
+    {
+        return PCAAnalysisPanel.class.getResource("paramDesc.html").getPath();
+    }
+}
