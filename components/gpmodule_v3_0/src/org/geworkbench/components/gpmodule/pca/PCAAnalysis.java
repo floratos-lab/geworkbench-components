eb7b4b949706bbdf33bd9d5dbd1baa91f6467aae
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-08-15 19:36:43 +0000

    PCA Analysis
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3149 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/pca/PCAAnalysis.java
@@ -0,0 +1,83 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2007) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+package org.geworkbench.components.gpmodule.pca;
+
+import org.geworkbench.bison.model.analysis.AlgorithmExecutionResults;
+import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+import org.geworkbench.bison.annotation.DSAnnotationContext;
+import org.geworkbench.bison.annotation.CSAnnotationContextManager;
+import org.geworkbench.engine.management.Publish;
+import org.geworkbench.events.ProjectNodeAddedEvent;
+import org.geworkbench.components.gpmodule.GPAnalysis;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.genepattern.webservice.Parameter;
+import org.tigr.util.FloatMatrix;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author: Marc-Danie Nazaire
+ */
+public class PCAAnalysis extends GPAnalysis
+{
+    static Log log = LogFactory.getLog(PCAAnalysis.class);
+
+    public PCAAnalysis()
+    {
+        setLabel("PCA Analysis");
+
+        panel = new PCAAnalysisPanel();
+        setDefaultPanel(panel);
+    }
+
+    public AlgorithmExecutionResults execute(Object input)
+    {
+        assert (input instanceof DSMicroarraySetView);
+        DSMicroarraySetView<DSGeneMarker, DSMicroarray> view = (DSMicroarraySetView<DSGeneMarker, DSMicroarray>) input;
+
+        DSMicroarraySet maSet = view.getMicroarraySet();
+        
+        String gctFileName = createGCTFile("pcaDataset", view.markers(), view.items()).getAbsolutePath();
+
+        DSAnnotationContext<DSMicroarray> context = CSAnnotationContextManager.getInstance().getCurrentContext(maSet);
+
+        List parameters = new ArrayList();
+
+        parameters.add(new Parameter("input.filename", gctFileName));
+        parameters.add(new Parameter("cluster.by", ((PCAAnalysisPanel)panel).getClusterBy()));
+
+        List results = runAnalysis("PCA", (Parameter[])parameters.toArray(new Parameter[0]), panel.getPassword());        
+
+        PCAData pcaData = new PCAData(results);
+        CSAncillaryDataSet pcaDataSet = new PCADataSet(view.getDataSet(), "PCA", pcaData);
+       
+        return new AlgorithmExecutionResults(true, "PCA", pcaDataSet);
+    }
+
+    public List<FloatMatrix> extractMatrices(List files)
+    {
+
+        return null;
+    }
+
+    @Publish
+    public ProjectNodeAddedEvent publishProjectNodeAddedEvent(ProjectNodeAddedEvent event) 
+    {
+        return event;
+    }
+}
