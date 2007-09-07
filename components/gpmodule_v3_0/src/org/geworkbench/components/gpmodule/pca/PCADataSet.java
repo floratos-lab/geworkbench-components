a2981aaae3b927ff9252c43e5f3e66e1c183d2c6
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2007-09-07 16:13:19 +0000

    overridden writeToFile() method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3199 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/pca/PCADataSet.java
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/pca/PCADataSet.java
@@ -14,14 +14,18 @@ package org.geworkbench.components.gpmodule.pca;
 import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import java.io.File;
+import javax.swing.*;
+import java.io.*;
 
 /**
  * @author: Marc-Danie Nazaire
  */
 public class PCADataSet extends CSAncillaryDataSet implements DSAncillaryDataSet
 {
+    static Log log = LogFactory.getLog(PCADataSet.class);
     private PCAData pcaData;
 
     public PCADataSet(DSDataSet parent, String label, PCAData pcaData)
@@ -44,5 +48,41 @@ public class PCADataSet extends CSAncillaryDataSet implements DSAncillaryDataSet
     public PCAData getData()
     {
         return pcaData;
+    }
+
+    public void writeToFile(String fileName)
+    {
+        File file = new File(fileName);
+
+        try
+        {
+            file.createNewFile();
+            if (!file.canWrite()) {
+                JOptionPane.showMessageDialog(null, "Cannot write to specified file.");
+                return;
+            }
+            
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            String fileSeparator = System.getProperty("line.separator");
+            for(int i=1; i <= pcaData.getNumPCs(); i++)
+            {
+                writer.write("Principal Component:\t" + i);
+                writer.write(fileSeparator) ;
+                writer.write("Eigenvalue:\t" + pcaData.getEigenValues().get(new Integer(i)));
+                writer.write(fileSeparator) ;
+                writer.write("Percentage Variation:\t" + pcaData.getPercentVars().get(new Integer(i)));
+                writer.write(fileSeparator) ;
+                writer.write("Eigenvector:\t" + pcaData.getEigenVectors().get(new Integer(i)));
+                writer.write(fileSeparator);
+                writer.write(fileSeparator) ;
+            }
+
+            writer.flush();
+            writer.close();
+        }
+        catch(IOException io)
+        {
+           log.error(io);
+        }
     }
 }
