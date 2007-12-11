3931d66d645b43c6fa2ba6963b65d741d919c4c7
Kiran Keshav <keshav@columbia.edu>
2007-12-11 19:51:10 +0000

    minor: using log4j
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3392 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/genspace/src/org/geworkbench/components/genspace/ObjectHandler.java
+++ components/genspace/src/org/geworkbench/components/genspace/ObjectHandler.java
@@ -3,34 +3,43 @@ package org.geworkbench.components.genspace;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A handler used to log events.
+ * 
+ * @author seth
+ * @version $Id: ObjectHandler.java,v 1.2 2007-12-11 19:51:10 keshav Exp $
+ */
 public class ObjectHandler {
-	
+
+	private Log log = LogFactory.getLog(this.getClass());
+
 	public ObjectHandler(Object event, Object source) {
-		System.out.println("\n***GenSpace Logger****");
-		System.out.println("\nEvent Name: " + event.getClass().getName());
-		
-		/*if (event.getClass().getName().equals("org.geworkbench.events.ProjectEvent")) {
-			Method methods[] = event.getClass().getMethods();
-			for (Method m : methods) {
-				
-			}
-		}*/
+		log.info("\n***GenSpace Logger****");
+		log.info("\nEvent Name: " + event.getClass().getName());
+
+		/*
+		 * if
+		 * (event.getClass().getName().equals("org.geworkbench.events.ProjectEvent")) {
+		 * Method methods[] = event.getClass().getMethods(); for (Method m :
+		 * methods) { } }
+		 */
 		Field fields[] = event.getClass().getDeclaredFields();
-		for(Field f : fields) {
-			System.out.println("Field: " + f.getName());
+		for (Field f : fields) {
+			log.info("Field: " + f.getName());
 		}
 		Method methods[] = event.getClass().getDeclaredMethods();
-		for(Method m: methods) {
-			System.out.println("Method: " + m.getName());
+		for (Method m : methods) {
+			log.info("Method: " + m.getName());
 		}
-		/*System.out.println("\nSource Name: " + source.getClass().getName());
-		fields = source.getClass().getDeclaredFields();
-		for(Field f : fields) {
-			System.out.println("Field: " + f.getName());
-		}
-		methods = source.getClass().getDeclaredMethods();
-		for(Method m: methods) {
-			System.out.println("Method: " + m.getName());
-		}*/
+		/*
+		 * System.out.println("\nSource Name: " + source.getClass().getName());
+		 * fields = source.getClass().getDeclaredFields(); for(Field f : fields) {
+		 * System.out.println("Field: " + f.getName()); } methods =
+		 * source.getClass().getDeclaredMethods(); for(Method m: methods) {
+		 * System.out.println("Method: " + m.getName()); }
+		 */
 	}
 }
