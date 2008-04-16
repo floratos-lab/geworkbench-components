f63aa2046e90c00c9f77c0edd3b41c79f1ecdc86
Yih-Shien Chiang <chiangy@columbia.edu>
2008-04-16 17:17:34 +0000

    fix bug 0001161 add ability to handle exception come from server side.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3694 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/analysis/src/org/geworkbench/components/analysis/PollingThread.java
+++ components/analysis/src/org/geworkbench/components/analysis/PollingThread.java
@@ -1,8 +1,15 @@
 package org.geworkbench.components.analysis;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.swing.JOptionPane;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
+import org.geworkbench.bison.model.analysis.AlgorithmExecutionResults;
 import org.geworkbench.events.ProjectNodeCompletedEvent;
 import org.ginkgo.labs.ws.GridEndpointReferenceType;
 
@@ -12,7 +19,7 @@ import edu.columbia.geworkbench.cagrid.dispatcher.client.DispatcherClient;
  * A thread that handles remote service polling.
  * 
  * @author keshav
- * @version $Id: PollingThread.java,v 1.3 2008-01-29 18:09:48 chiangy Exp $
+ * @version $Id: PollingThread.java,v 1.4 2008-04-16 17:17:34 chiangy Exp $
  */
 public class PollingThread extends Thread {
 
@@ -44,7 +51,14 @@ public class PollingThread extends Thread {
 			}
 			ProjectNodeCompletedEvent completedEvent = new ProjectNodeCompletedEvent(
 					"Analysis Completed", gridEPR);
-			if (result != null && (result instanceof String)
+			if (result instanceof Exception){
+				//generate user understandalbe messages. Detailed information will shown on dispatcher server's log.debug
+				String errorMessage = "While executing analysis on remote server, an error occurred.\nYour analysis has been canceled.\nDetail error message as follow:\n";
+				errorMessage += ((Exception)result).getMessage();
+				
+				JOptionPane.showMessageDialog(null, errorMessage,
+						"Remote Analysis Error", JOptionPane.ERROR_MESSAGE);
+			}else if (result != null && (result instanceof String)
 					&& result.equals("null")) {
 				completedEvent.setDataSet(null);
 			} else if (result != null)
