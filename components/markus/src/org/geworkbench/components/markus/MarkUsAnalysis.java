939fa79a3a8f81c1453c9186be7ae602637c2c83
Meng Wang <mw2518@columbia.edu>
2008-07-29 14:33:47 +0000

    hardcoded markus results for a few pudge-generated pdbs for demo
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4065 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/markus/src/org/geworkbench/components/markus/MarkUsAnalysis.java
+++ components/markus/src/org/geworkbench/components/markus/MarkUsAnalysis.java
@@ -82,6 +82,9 @@ public class MarkUsAnalysis extends AbstractAnalysis implements ProteinAnnotatio
 		if (pdbname.equals("2pk7.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS569");
 		else if (pdbname.equals("1e09.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS580");
 		else if (pdbname.equals("input_txt_Q14994_8-83.pudge.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS662");
+		else if (pdbname.equals("fboxM1_gi_42544167.pudge.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS668");
+		else if (pdbname.equals("myb_gi_46361980.pudge.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS669");
+		else if (pdbname.equals("myc_gi_71774083.pudge.pdb")) return new AlgorithmExecutionResults(true, "No errors", "MUS670");
 
 		//send file from local client to server
 		try{
