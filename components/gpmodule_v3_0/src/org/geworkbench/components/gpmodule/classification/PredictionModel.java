38eab540f91d82c9c1279de55067fcee53236d21
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2008-08-25 19:30:50 +0000

    modified how the prediction model file is processed
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4123 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/classification/PredictionModel.java
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/classification/PredictionModel.java
@@ -11,180 +11,64 @@
 */
 package org.geworkbench.components.gpmodule.classification;
 
-import org.genepattern.io.odf.*;
-import org.genepattern.io.ParseException;
+import org.geworkbench.util.ClassifierException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
 import java.util.HashMap;
-
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
 
 /**
- * @author Marc-Danie Nazaire
+ * @author  Marc-Danie Nazaire
  */
 public class PredictionModel
 {
-    OdfObject model;
-    private File predFile;
+    private byte[] model;
+    private File modelFile;
 
-    public PredictionModel(File fileName)
+    public PredictionModel(File file)
     {
+        modelFile = file;
+        model = null;
         try
         {
-            model = new OdfObject(fileName.getAbsolutePath());
-
-            fileName.delete();
+            FileInputStream fis = new FileInputStream(file);
+            FileChannel fc = fis.getChannel();
+            model = new byte[(int)(fc.size())];
+            ByteBuffer bb = ByteBuffer.wrap(model);
+            fc.read(bb);
         }
-        catch(Exception e)
+        catch(IOException io)
         {
-            e.printStackTrace();
+           io.printStackTrace();
+
         }
     }
 
     public File getPredModelFile()
     {
+        if(modelFile!= null && modelFile.exists())
+            return modelFile;
+
         try
         {
-            if(predFile!= null && predFile.exists())
-                return predFile;
+            modelFile = File.createTempFile("predModel", ".odf", new File(System.getProperty("temporary.files.directory")));
+            modelFile.deleteOnExit();
 
-            predFile = File.createTempFile("predModel", ".odf", new File(System.getProperty("temporary.files.directory")));
-            predFile.deleteOnExit();
+            FileOutputStream out = new FileOutputStream(modelFile);
 
-            OdfWriter odfWriter = new OdfWriter(predFile.getAbsolutePath(), null,
-                                    model.getHeader("Model"), model.getIntHeader("DataLines"), false);
+            out.write(model);
 
-            String[] columnTypes = new String[model.getColumnCount()];
-            for(int i=0; i < model.getColumnCount(); i++)
-                columnTypes[i]= model.getColumnType(i);
-
-            odfWriter.setColumnTypes(columnTypes);
-
-            String[] assignments = model.getArrayHeader("ASSIGNMENTS");
-            odfWriter.addHeader("ASSIGNMENTS", assignments);
-
-            odfWriter.printHeader();
-            for(int r = 0; r < model.getRowCount(); r++)
-            {
-                for(int c = 0; c < model.getColumnCount(); c++ )
-                {
-                     odfWriter.write(model.getValueAt(r, c));
-                     odfWriter.write("\t");
-                }
-                odfWriter.write(System.getProperty("line.separator"));
-            }
-
-            odfWriter.close();
+            out.close();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
 
-        return predFile;
-    }
-
-    private class OdfObject
-    {
-        MyOdfHandler handler;
-        public OdfObject(String file) throws Exception
-        {
-            FileInputStream fis = null;
-            try
-            {
-                fis = new FileInputStream(file);
-                OdfParser parser = new OdfParser();
-
-                handler = new MyOdfHandler();
-                parser.setHandler(handler);
-                parser.parse(fis);
-            }
-            catch(Exception e)
-            {
-                throw e;
-            }
-        }
-
-        public String[] getArrayHeader(String key)
-        {
-            return (String[])handler.keyValuePair.get(key);
-        }
-
-        public int getColumnCount()
-        {
-            return handler.numColumns;
-        }
-
-        public int getRowCount()
-        {
-            return handler.numRows;
-        }
-
-        public String getHeader(String key)
-        {
-             return (String)handler.keyValuePair.get(key);
-        }
-
-        public int getIntHeader(String key)
-        {
-             return ((Integer)handler.keyValuePair.get(key)).intValue();
-        }
-
-        public String getColumnType(int col)
-        {
-            String[] columnTypes = (String[])handler.keyValuePair.get("COLUMN_TYPES");
-
-            return columnTypes[col];
-        }
-
-        public String getValueAt(int row, int col)
-        {
-            return handler.data[row][col];
-        }
-
-    }
-
-    private class MyOdfHandler implements OdfHandler
-    {
-        HashMap keyValuePair;
-        int numRows;
-        int numColumns;
-        String[][] data = null;
-
-        public MyOdfHandler()
-        {
-            keyValuePair = new HashMap();
-        }
-
-        public void endHeader(){}
-
-        public void header(String key, String[] values)throws ParseException
-        {
-            if(key.equalsIgnoreCase("COLUMN_TYPES"))
-                numColumns = values.length;
-            
-            keyValuePair.put(key,  values);
-        }
-
-        public void header(String key, String value) throws ParseException
-        {
-            if (key.equalsIgnoreCase("DataLines"))
-            {
-                numRows = Integer.parseInt(value);
-                keyValuePair.put(key, new Integer(value));
-            }
-            else
-                keyValuePair.put(key, value);
-        }
-
-        public void data(int row, int column, String d)
-        {
-            if(data == null)
-            {
-                data = new String[numRows][numColumns];
-            }
-
-            data[row][column] = d;
-        }
+        return modelFile;
     }
 }
