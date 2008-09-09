28de0476d6bb20b75b074fd00c0ee932d6b76a6c
Christine Hung <hungc@columbia.edu>
2008-09-09 14:28:07 +0000

    Mantis issue: 1517.  Better solution is to call stringBuilder.trimToSize() before calling stringBuilder.toString() to get rid of place-holder characters.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4180 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/netboost/src/org/geworkbench/components/netboost/NetBoostPlugin.java
+++ components/netboost/src/org/geworkbench/components/netboost/NetBoostPlugin.java
@@ -47,7 +47,7 @@ import org.jfree.data.xy.XYSeriesCollection;
  * NetBoost Plugin
  * 
  * @author ch2514
- * @version $Id: NetBoostPlugin.java,v 1.11 2008-09-09 14:03:42 hungc Exp $
+ * @version $Id: NetBoostPlugin.java,v 1.12 2008-09-09 14:28:07 hungc Exp $
  */
 
 public class NetBoostPlugin extends JPanel {
@@ -256,9 +256,10 @@ public class NetBoostPlugin extends JPanel {
 		headings.append(System.getProperty("line.separator"));
 		confused.insert(0, headings.toString());
 		confused.insert(0, "Confusion Matrix");
+		confused.trimToSize();
 		String s = confused.toString();
-		s = s.substring(0, s.length() - 2);
-
+		
+		
 		return scores.toString() + System.getProperty("line.separator")
 				+ System.getProperty("line.separator") + s;
 	}
