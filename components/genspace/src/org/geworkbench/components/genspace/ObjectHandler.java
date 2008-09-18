996108f2c91a8e730ad4f1b80551e3d752334acb
sheths <sheths@columbia.edu>
2008-09-18 00:27:32 +0000

    genspace - changed the log server to bambi.cs.columbia.edu, added code to include redundancy of looking up the log server in case the log server cannot be found
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4226 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/genspace/src/org/geworkbench/components/genspace/ObjectHandler.java
+++ components/genspace/src/org/geworkbench/components/genspace/ObjectHandler.java
@@ -2,8 +2,10 @@ package org.geworkbench.components.genspace;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.Socket;
 import java.util.Calendar;
 import java.util.Random;
+import java.util.Scanner;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,13 +15,15 @@ import org.apache.ojb.otm.lock.ObjectLock;
  * A handler used to log events.
  * 
  * @author sheths
- * @version $Id: ObjectHandler.java,v 1.7 2008-07-19 00:33:15 sheths Exp $
+ * @version $Id: ObjectHandler.java,v 1.8 2008-09-18 00:27:32 sheths Exp $
  */
 public class ObjectHandler {
 
 	private Log log = LogFactory.getLog(this.getClass());
-	private String host = "vesey.cs.columbia.edu";
+	private String host = "bambi.cs.columbia.edu";
 	private int port = 12346;
+	private String lookupServer = "york.cs.columbia.edu";
+	private int lookupPort = 44444;
 	private int frequency = 2;
 	private static int count = 0;
 	private static String lastRunDataSetName = "";
@@ -81,17 +85,74 @@ public class ObjectHandler {
 
 				count++;
 				if (count % frequency == 0) {
-					XmlClient x = new XmlClient(host, port);
-					boolean success = x.readAndSendFile("geworkbench_log.xml");
-
-					if (success == true) {
-						log.info("");
-						log.info("genspace - log file sent succesfully to the server");
-						log.info("");
-						o.deleteFile();
-					}
+					phoneHome(o);
 				}
 			}
+		}
+	}
+	
+	/**
+	 * This method will phone home and try to send the log file.
+	 * If this doesn't work, it will try updating the log server information in case the log server has been changed
+	 * @param logger The ObjectLogger object
+	 */
+	private void phoneHome(ObjectLogger logger) {
+		boolean success = sendLogFile(logger);
+
+		if (success != true) {
+			//could not send log file to the server.
+			//we have 2 possibilities - 1.the server is down, in which case look up the new server
+			//or 2.the client is not connected to the internet, so just try again later
+			//this is for possibility 1.
+			log.info("");
+			log.info("genspace - sending log file failed, trying to update the server information");
+			log.info("");
+			updateLogServer();
+			//try resending the log file to the new server
+			sendLogFile(logger);
+		}
+	}
+	
+	/**
+	 * This method will send the log files to the current log server
+	 * @param logger The ObjectLogger object
+	 * @return true if file sent successfully, false otherwise
+	 */
+	private boolean sendLogFile(ObjectLogger logger) {
+		XmlClient x = new XmlClient(host, port);
+		boolean success = x.readAndSendFile("geworkbench_log.xml");
+
+		if (success == true) {
+			log.info("");
+			log.info("genspace - log file sent succesfully to the server");
+			log.info("");
+			logger.deleteFile();
+		}
+		return success;
+	}
+	
+	/**
+	 * This method will check the lookup server to get the hostname and port number of the new log server and update the information
+	 */
+	private void updateLogServer() {
+		try {
+			Socket socket = new Socket(lookupServer, lookupPort);
+			Scanner in = new Scanner(socket.getInputStream());
+			if (in.hasNext()) {
+				String msg = in.nextLine();
+				String[] server = msg.split(":");
+				//set the host name and port to the new information received
+				host = server[0];
+				port = Integer.parseInt(server[1]);
+				log.info("");
+				log.info("genSpace - updated server:" + server[0] + "\nport:" + server[1]);
+				log.info("");
+			}
+			in.close();
+			socket.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
