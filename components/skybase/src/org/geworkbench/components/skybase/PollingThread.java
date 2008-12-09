66a427180ae46a661ac8ca1167ce3c23786a4667
Meng Wang <mw2518@columbia.edu>
2008-12-09 22:43:18 +0000

    added comments and applied magnet coding style
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4539 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/skybase/src/org/geworkbench/components/skybase/PollingThread.java
+++ components/skybase/src/org/geworkbench/components/skybase/PollingThread.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.geworkbench.events.ProjectNodeCompletedEvent;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
+import org.geworkbench.events.ProjectNodeCompletedEvent;
 import org.ginkgo.labs.ws.GridEndpointReferenceType;
 
 import edu.columbia.geworkbench.cagrid.dispatcher.client.DispatcherClient;
@@ -14,7 +14,7 @@ import edu.columbia.geworkbench.cagrid.dispatcher.client.DispatcherClient;
  * A thread that handles remote service polling.
  * 
  * @author keshav
- * @version $Id: PollingThread.java,v 1.1 2008-12-09 16:57:04 wangm Exp $
+ * @version $Id: PollingThread.java,v 1.2 2008-12-09 22:43:18 wangm Exp $
  */
 public class PollingThread extends Thread {
 
@@ -27,7 +27,7 @@ public class PollingThread extends Thread {
 	private DispatcherClient dispatcherClient = null;
 
 	private boolean cancelled = false;
-	
+
 	public PollingThread(SkyBaseComponent panel,
 			GridEndpointReferenceType gridEPR, DispatcherClient dispatcherClient) {
 
@@ -45,23 +45,30 @@ public class PollingThread extends Thread {
 			while (result == null) {
 				log.debug("polling");
 				Thread.sleep(2000);
-				if (cancelled) return;
+				if (cancelled)
+					return;
 				result = dispatcherClient.getResults(gridEPR);
 			}
 			ProjectNodeCompletedEvent completedEvent = new ProjectNodeCompletedEvent(
 					"Analysis Completed", gridEPR);
-			if (result instanceof Exception){
-				//generate user understandable messages. Detailed information will shown on dispatcher server's log.debug
+			if (result instanceof Exception) {
+				// generate user understandable messages. Detailed information
+				// will shown on dispatcher server's log.debug
 				String errorMessage = "";
-				errorMessage += ((Exception)result).getMessage();
-				//TODO: this filter out some messages, there's potential that message do have ":" in it. we should have a way to prevent this.
-				//errorMessage=org.geworkbench.bison.util.StringUtils.filter(errorMessage,"^.*:");
-				errorMessage=org.geworkbench.bison.util.StringUtils.filter(errorMessage,"java.rmi.RemoteException: "); 
-				errorMessage=org.geworkbench.bison.util.StringUtils.filter(errorMessage,"^.*::");
-				
+				errorMessage += ((Exception) result).getMessage();
+				// TODO: this filter out some messages, there's potential that
+				// message do have ":" in it. we should have a way to prevent
+				// this.
+				// errorMessage=org.geworkbench.bison.util.StringUtils.filter(errorMessage,"^.*:");
+				errorMessage = org.geworkbench.bison.util.StringUtils.filter(
+						errorMessage, "java.rmi.RemoteException: ");
+				errorMessage = org.geworkbench.bison.util.StringUtils.filter(
+						errorMessage, "^.*::");
+
 				JOptionPane.showMessageDialog(null, errorMessage,
-						"Your analysis has been canceled.", JOptionPane.ERROR_MESSAGE);
-			}else if (result != null && (result instanceof String)
+						"Your analysis has been canceled.",
+						JOptionPane.ERROR_MESSAGE);
+			} else if (result != null && (result instanceof String)
 					&& result.equals("null")) {
 				completedEvent.setDataSet(null);
 			} else if (result != null) {
@@ -69,7 +76,9 @@ public class PollingThread extends Thread {
 			}
 			panel.publishProjectNodeCompletedEvent(completedEvent);
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Cannot connect to cagrid server", "Cagrid Connection Failure", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+					"Cannot connect to cagrid server",
+					"Cagrid Connection Failure", JOptionPane.ERROR_MESSAGE);
 			ProjectNodeCompletedEvent cancelledEvent = new ProjectNodeCompletedEvent(
 					"Analysis Cancelled", gridEPR);
 			panel.publishProjectNodeCompletedEvent(cancelledEvent);
@@ -77,11 +86,11 @@ public class PollingThread extends Thread {
 
 	}
 
-	public void cancel(){
+	public void cancel() {
 		cancelled = true;
 	}
-	
-	public GridEndpointReferenceType getGridEPR(){
+
+	public GridEndpointReferenceType getGridEPR() {
 		return gridEPR;
 	}
 }
