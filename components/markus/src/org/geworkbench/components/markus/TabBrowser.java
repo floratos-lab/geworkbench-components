7e8d450538b6ec3d24fd92fa5d8d97c117f6171e
Zhou Ji <zji@c2b2.columbia.edu>
2009-03-05 16:22:51 +0000

    cleaning up; make the refresh behavior more consistent
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4881 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/markus/src/org/geworkbench/components/markus/TabBrowser.java
+++ components/markus/src/org/geworkbench/components/markus/TabBrowser.java
@@ -1,121 +1,93 @@
 package org.geworkbench.components.markus;
 
 import java.awt.BorderLayout;
-import java.net.*;
-import javax.swing.*;
+import java.net.URL;
 
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.geworkbench.components.markus.MarkUsBrowser.WebTabListener;
-import org.jdesktop.jdic.browser.*;
+import org.jdesktop.jdic.browser.WebBrowser;
 
-class TabBrowser extends WebBrowser
-{
-    	protected PopupBlockerOpenWindowRunnable openWindowRunnable = new PopupBlockerOpenWindowRunnable();
+/**
+ * Embedded browser for MarkUsResult
+ * 
+ * @author mwang
+ * @version $Id: TabBrowser.java,v 1.3 2009-03-05 16:22:51 jiz Exp $
+ *
+ */
+class TabBrowser extends WebBrowser {
+	private static final long serialVersionUID = -4833106758989294420L;
+	Log log = LogFactory.getLog(TabBrowser.class);
+
 	protected URL lastToOpenURL = null;
 	public static MarkUsBrowser mb;
-    private boolean useIE = true;
+	private boolean useIE = true;
 
-        public TabBrowser() { super(); }
+	public TabBrowser() {
+		super();
+	}
 
-	public TabBrowser(URL url)
-	{
+	public TabBrowser(URL url) {
 		super(url);
 		setLastToOpenURL(url);
 	}
 
-	public TabBrowser(URL url, boolean IE)
-	{
+	public TabBrowser(URL url, boolean IE) {
 		super(url);
 		setURL(url, url.getQuery(), "user-agent:geWorkbench");
 		useIE = IE;
 		setLastToOpenURL(url);
 	}
 
-	//return false to block popup window
-	protected boolean willOpenWindow(URL url)
-	{
-		openWindowRunnable.setUrl(url);
-		SwingUtilities.invokeLater(openWindowRunnable);
-		////		System.out.println("wow: "+url);
-		////		if (url.toString().indexOf("javascript:mot") >-1) return true;
+	// return false to block popup window
+	protected boolean willOpenWindow(URL url) {
+		if (SwingUtilities.isEventDispatchThread()) {
+			log.info("new url '" + url + "'");
+			createAllowedContent(url);
+		} else {
+			log.error("willOpenWindow called in a non-EDT thread.");
+		}
 		return false;
 	}
-	
-	//return false to block url navigation
-	protected boolean willOpenURL(URL url)
-	{
+
+	// return false to block url navigation
+	protected boolean willOpenURL(URL url) {
 		boolean rc = super.willOpenURL(url);
 		setLastToOpenURL(url);
 		return rc;
 	}
-	
-	protected void setLastToOpenURL(URL url)
-	{
+
+	protected void setLastToOpenURL(URL url) {
 		lastToOpenURL = url;
 	}
-	
-	protected URL getLastToOpenURL()
-	{
+
+	protected URL getLastToOpenURL() {
 		return lastToOpenURL;
 	}
-	
-	public void setMainBrowser(MarkUsBrowser mainbrowser)
-	{
+
+	public void setMainBrowser(MarkUsBrowser mainbrowser) {
 		mb = mainbrowser;
 	}
-	public MarkUsBrowser getMainBrowser()
-	{
+
+	public MarkUsBrowser getMainBrowser() {
 		return mb;
 	}
-	public void createAllowedContent(URL url)
-	{
-    		TabBrowser newtb = new TabBrowser(url, useIE);
-    		WebTabListener wtl = mb.new WebTabListener();
-    		newtb.addWebBrowserListener(wtl);
-    		JPanel subjp = new JPanel(new BorderLayout());
-    		subjp.add(newtb, BorderLayout.CENTER);
-    		mb.getTabbedPane().addTab("Tab", subjp);
-    		mb.getTabbedPane().setSelectedIndex(mb.getTabbedPane().getTabCount()-1);
+
+	/**
+	 * add a new tab for the main browser panel
+	 * @param url
+	 */
+	private void createAllowedContent(URL url) {
+		TabBrowser newtb = new TabBrowser(url, useIE);
+		WebTabListener wtl = mb.new WebTabListener();
+		newtb.addWebBrowserListener(wtl);
+		JPanel subjp = new JPanel(new BorderLayout());
+		subjp.add(newtb, BorderLayout.CENTER);
+		mb.getTabbedPane().addTab("Tab", subjp);
+		mb.getTabbedPane().setSelectedIndex(
+				mb.getTabbedPane().getTabCount() - 1);
 	}
-	
-	public class PopupBlockerOpenWindowRunnable implements Runnable
-	{
-		protected URL url;
-		
-		public PopupBlockerOpenWindowRunnable()
-		{
-		}
-		
-		public void run()
-		{
-			URL lastToOpenURL = getLastToOpenURL();
-			URL popupURL = url != null ? url : lastToOpenURL;
-			
-	/*		
-	 		URL srcURL = getURL();
-			if (shouldBlockPopup(srcURL, popupURL))
-			processBlockedPopup(srcURL, popupURL);
-			else
-	*/
-			createAllowedContent(popupURL);	
-		}
-		
-		
-		/**
-		* @return Returns the url.
-		*/
-		public URL getUrl()
-		{
-		return url;
-		}
-		
-		/**
-		* @param url
-		* The url to set.
-		*/
-		public void setUrl(URL url)
-		{
-		this.url = url;
-		}
-	}
-	
 }
