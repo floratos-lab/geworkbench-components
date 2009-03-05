d0f28ad9273e7f385b180583ea493bbed2cf724a
sheths <sheths@columbia.edu>
2009-03-05 20:40:44 +0000

    genSpace: moving the genSpace component to the Tools menu. genSpace is no longer a VisualComponent
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4887 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/genspace/src/org/geworkbench/components/genspace/GenSpace.java
+++ components/genspace/src/org/geworkbench/components/genspace/GenSpace.java
@@ -8,11 +8,12 @@ import java.awt.*;
 /**
  * This is the main class for genspace. 
  * This is a visual plugin and will be a tabbed pane. All other genspace components will be part of the tabbed pane. 
- * @author swapneel
+ * @author sheths
  */
-public class GenSpace extends JPanel implements VisualPlugin {
+public class GenSpace {
 
 	private JTabbedPane jtp;
+	private JFrame jframe;
 	
 	public GenSpace()
 	{	
@@ -22,6 +23,7 @@ public class GenSpace extends JPanel implements VisualPlugin {
 
 	
 	private void initComponents() {
+		jframe = new JFrame("genSpace");
 		jtp = new JTabbedPane();
 		
 		WorkflowVisualization wv = new WorkflowVisualization();
@@ -41,13 +43,16 @@ public class GenSpace extends JPanel implements VisualPlugin {
 		//login.run();
 		
 		
-		jtp.addTab("GenSpace Login", login);
+		jtp.addTab("genSpace Login", login);
 		jtp.addTab("Workflow Visualization", wv);
 		jtp.addTab("Real Time Workflow Suggestion", rtwfs);
 		jtp.addTab("Workflow Statistics", isbu);
 		//jtp.addTab("Message", new Message());		
 		
-		add(jtp);
+		jframe.add(jtp);
+		
+		jframe.setSize(800, 600);
+		jframe.setVisible(true);
 		
 		/*
 		System.out.println("wv: " + wv_thread.getId());
@@ -55,11 +60,5 @@ public class GenSpace extends JPanel implements VisualPlugin {
 		System.out.println("rtwfs: " + rtwfs_thread.getId());
 		System.out.println("login: " + login_thread.getId());
 		*/
-    }
-	
-	
-    public Component getComponent() {
-        // In this case, this object is also the GUI component.
-        return this;
     }
 }
