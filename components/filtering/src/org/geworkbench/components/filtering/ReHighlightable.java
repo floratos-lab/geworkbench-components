e205e21585682e06c75880bffaf66f1012598e53
Yih-Shien Chiang <chiangy@columbia.edu>
2009-05-18 19:00:56 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5128 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/filtering/src/org/geworkbench/components/filtering/ReHighlightable.java
+++ components/filtering/src/org/geworkbench/components/filtering/ReHighlightable.java
@@ -2,7 +2,7 @@ package org.geworkbench.components.filtering;
 
 /**
  * @author yc2480
- * @version $ID$
+ * @version $Id: ReHighlightable.java,v 1.3 2009-05-18 19:00:31 chiangy Exp $
  */
 public interface ReHighlightable {
 
