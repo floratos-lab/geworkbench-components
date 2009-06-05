ed57895371242d7e337cf4b345925809f40f17a2
Zhou Ji <zji@c2b2.columbia.edu>
2009-06-05 15:52:00 +0000

    cleanup only - no change in functionality/logic
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5176 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/alignment/src/org/geworkbench/components/alignment/panels/CSSequenceSetViewEventBase.java
+++ components/alignment/src/org/geworkbench/components/alignment/panels/CSSequenceSetViewEventBase.java
@@ -5,232 +5,177 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
 
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
-import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbench.bison.datastructure.biocollections.sequences.CSSequenceSet;
 import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
-import org.geworkbench.bison.datastructure.biocollections.views.CSMicroarraySetView;
-import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
-import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
 import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
-import org.geworkbench.engine.management.*;
-import org.geworkbench.events.*;
 import org.geworkbench.engine.config.VisualPlugin;
-
-import java.awt.Dimension;
+import org.geworkbench.engine.management.Asynchronous;
+import org.geworkbench.engine.management.Publish;
+import org.geworkbench.engine.management.Subscribe;
+import org.geworkbench.events.GeneSelectorEvent;
+import org.geworkbench.events.MicroarraySetViewEvent;
+import org.geworkbench.events.SubpanelChangedEvent;
 
 /**
- * <p>Title: </p>
- * <p/>
- * <p>Description: </p>
- * <p/>
- * <p>Copyright: Copyright (c) 2005</p>
- * <p/>
- * <p>Company: </p>
- *
+ * The only purpose of this class is to serve as the base class of
+ * BlastAppComponent.
+ * 
  * @author not attributable
- * @version 1.0
+ * @version $Id: CSSequenceSetViewEventBase.java,v 1.8 2009-06-05 15:52:00 jiz Exp $
  */
+@SuppressWarnings("unchecked")
 public class CSSequenceSetViewEventBase implements VisualPlugin {
+	protected DSSequenceSet sequenceDB = null;
+	protected CSSequenceSet activeSequenceDB = null;
 
-    // There should be some new class similar to MicroarraySetView
+	protected boolean activateMarkers = true;
+	protected JCheckBox chkAllMarkers = new JCheckBox("All Markers");
+	protected JPanel mainPanel;
+	protected JToolBar displayToolBar;
+	protected DSPanel<? extends DSGeneMarker> activatedMarkers = null;
 
-    protected DSSequenceSet sequenceDB = null;
-    protected CSSequenceSet activeSequenceDB = null;
-    /**
-     * The reference microarray set.
-     */
-    protected DSMicroarraySet<DSMicroarray> refMASet = null;
-    protected DSMicroarraySetView<DSGeneMarker, DSMicroarray> maSetView = null;
+	private JTextField sequenceNumberField;
 
+	public CSSequenceSetViewEventBase() {
+		try {
+			jbInit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    protected boolean activateMarkers = true;
-    // protected boolean activateArrays = false;
-    protected JCheckBox chkAllMarkers = new JCheckBox("All Markers");
-    protected JPanel mainPanel;
-    protected JToolBar displayToolBar;
-    protected DSPanel<? extends DSGeneMarker> activatedMarkers = null;
-    // protected DSPanel activatedArrays = null;
-    JTextField sequenceNumberField;
+	}
 
-    public CSSequenceSetViewEventBase() {
-        try {
-            jbInit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	/**
+	 * getComponent
+	 * 
+	 * @return Component
+	 */
+	public Component getComponent() {
+		return mainPanel;
+	}
 
-    }
+	@Publish
+	public SubpanelChangedEvent publishSubpanelChangedEvent(
+			org.geworkbench.events.SubpanelChangedEvent event) {
+		return event;
+	}
 
-    /**
-     * getComponent
-     *
-     * @return Component
-     */
-    public Component getComponent() {
-        return mainPanel;
-    }
+	/**
+	 * receiveProjectSelection
+	 * 
+	 * @param e -
+	 *            ProjectEvent
+	 */
+	@Subscribe
+	public void receive(org.geworkbench.events.ProjectEvent e, Object source) {
+		if (e.getMessage().equals(org.geworkbench.events.ProjectEvent.CLEARED)) {
+			fireModelChangedEvent(null);
+		} else {
+			DSDataSet dataSet = e.getDataSet();
+			if (dataSet instanceof DSSequenceSet) {
+				if (sequenceDB != dataSet) {
+					this.sequenceDB = (DSSequenceSet) dataSet;
 
-    @Publish
-    public SubpanelChangedEvent publishSubpanelChangedEvent(org.
-            geworkbench.events.SubpanelChangedEvent event) {
-        return event;
-    }
+					activatedMarkers = null;
+				}
+			}
+		}
+		refreshMaSetView();
+	}
 
-    /**
-     * receiveProjectSelection
-     *
-     * @param e ProjectEvent
-     */
-    @Subscribe
-    public void receive(org.geworkbench.events.ProjectEvent e,
-                        Object source) {
-        if (e.getMessage().equals(org.geworkbench.events.ProjectEvent.CLEARED)) {
-            refMASet = null;
-            fireModelChangedEvent(null);
-        } else {
-            DSDataSet dataSet = e.getDataSet();
-            if (dataSet instanceof DSSequenceSet) {
-                if (sequenceDB != dataSet) {
-                    this.sequenceDB = (DSSequenceSet) dataSet;
+	/**
+	 * geneSelectorAction
+	 * 
+	 * @param e
+	 *            GeneSelectorEvent
+	 */
+	@Subscribe(Asynchronous.class)
+	public void receive(GeneSelectorEvent e, Object source) {
+		if (e.getPanel() != null && e.getPanel().size() > 0) {
+			activatedMarkers = e.getPanel().activeSubset();
+		} else {
+			activatedMarkers = null;
+		}
+		refreshMaSetView();
+	}
 
-                    activatedMarkers = null;
-                }
-            }
-            //refreshMaSetView();
-        }
-        refreshMaSetView();
-    }
+	protected void refreshMaSetView() {
+		getDataSetView();
+		fireModelChangedEvent(null);
+	}
 
-    /**
-     * geneSelectorAction
-     *
-     * @param e GeneSelectorEvent
-     */
-    @Subscribe(Asynchronous.class)
-    public void receive(GeneSelectorEvent e,
-                        Object source) {
-        if (e.getPanel() != null && e.getPanel().size() > 0) {
-            activatedMarkers = e.getPanel().activeSubset();
-        } else {
-            activatedMarkers = null;
-        }
-        refreshMaSetView();
-    }
+	protected void fireModelChangedEvent(MicroarraySetViewEvent event) {
 
+	}
 
+	protected void jbInit() throws Exception {
+		mainPanel = new JPanel();
 
-    
-    protected void refreshMaSetView() {
-        getDataSetView();
-        fireModelChangedEvent(null);
-    }
+		displayToolBar = new JToolBar();
+		displayToolBar
+				.setLayout(new BoxLayout(displayToolBar, BoxLayout.X_AXIS));
+		chkAllMarkers.setToolTipText("Use All Markers.");
+		chkAllMarkers.setSelected(false);
+		chkAllMarkers.addActionListener(new ActionListener() {
 
-    protected void fireModelChangedEvent(MicroarraySetViewEvent event) {
+			public void actionPerformed(ActionEvent e) {
+				chkActivateMarkers_actionPerformed(e);
+			}
 
-    }
+		});
 
-    protected void jbInit() throws Exception {
-        mainPanel = new JPanel();
+		BorderLayout borderLayout2 = new BorderLayout();
+		mainPanel.setLayout(borderLayout2);
+		sequenceNumberField = new JTextField(20);
+		sequenceNumberField.setMaximumSize(sequenceNumberField.getPreferredSize());
+		sequenceNumberField.setEditable(false);
 
-        displayToolBar = new JToolBar();
-        displayToolBar.setLayout(new BoxLayout(displayToolBar, BoxLayout.X_AXIS));
-        chkAllMarkers.setToolTipText("USe All Markers.");
-        chkAllMarkers.setSelected(false);
-        chkAllMarkers.addActionListener(new
-                CSSequenceSetViewEventBase_chkActivateMarkers_actionAdapter(this));
+		sequenceNumberField.setText("Total Sequence Number:");
+		displayToolBar.add(chkAllMarkers, null);
+		displayToolBar.add(Box.createHorizontalStrut(5), null);
+		displayToolBar.add(sequenceNumberField);
+		mainPanel.add(displayToolBar, java.awt.BorderLayout.SOUTH);
+		this.activateMarkers = !chkAllMarkers.isSelected();
 
-        BorderLayout borderLayout2 = new BorderLayout();
-        mainPanel.setLayout(borderLayout2);
-        sequenceNumberField = new JTextField(20) {
-            @Override
-            public Dimension getMaximumSize() {
-                return getPreferredSize();
-            }
-            public void setEditable(boolean isEditable){
-                super.setEditable(false);
-            }
+	}
 
-        };
+	void chkActivateMarkers_actionPerformed(ActionEvent e) {
+		activateMarkers = !((JCheckBox) e.getSource()).isSelected();
+		refreshMaSetView();
+	}
 
-        sequenceNumberField.setText("Total Sequence Number:");
-        sequenceNumberField.setEditable(false);
-        displayToolBar.add(chkAllMarkers, null);
-        displayToolBar.add(Box.createHorizontalStrut(5), null);
-        displayToolBar.add(sequenceNumberField);
-        sequenceNumberField.setEditable(false);
-        mainPanel.add(displayToolBar, java.awt.BorderLayout.SOUTH);
-        this.activateMarkers = !chkAllMarkers.isSelected();
+	private void getDataSetView() {
+		activateMarkers = !chkAllMarkers.isSelected();
+		if (activateMarkers) {
+			if (activatedMarkers != null && activatedMarkers.size() > 0) {
 
-    }
+				if (activateMarkers && (sequenceDB != null)) {
+					activeSequenceDB = (CSSequenceSet) ((CSSequenceSet) sequenceDB)
+							.getActiveSequenceSet(activatedMarkers);
+					sequenceNumberField.setText("Activated Sequence Number: "
+							+ activeSequenceDB.size());
+				}
 
-//    void chkShowArrays_actionPerformed(ActionEvent e) {
-//        activateArrays = ((JCheckBox) e.getSource()).isSelected();
-//        refreshMaSetView();
-//    }
+			} else if (sequenceDB != null) {
+				sequenceNumberField.setText("Total Sequence Number: "
+						+ sequenceDB.size());
 
-    void chkActivateMarkers_actionPerformed(ActionEvent e) {
-        activateMarkers = !((JCheckBox) e.getSource()).isSelected();
-        refreshMaSetView();
-    }
+				activeSequenceDB = (CSSequenceSet) sequenceDB;
+			}
 
-    public void getDataSetView() {
-        activateMarkers = !chkAllMarkers.isSelected();
-        if (activateMarkers) {
-            if (activatedMarkers != null &&
-                    activatedMarkers.size() > 0) {
+		} else if (sequenceDB != null) {
+			sequenceNumberField.setText("Total Sequence Number: "
+					+ sequenceDB.size());
+		}
 
-
-                if (activateMarkers && (sequenceDB != null)) {
-                    // createActivatedSequenceSet();
-                    activeSequenceDB = (CSSequenceSet) ((CSSequenceSet)
-                            sequenceDB).
-                            getActiveSequenceSet(activatedMarkers);
-                    sequenceNumberField.setText("Activated Sequence Number: " +
-                            activeSequenceDB.size());
-                         sequenceNumberField.setEditable(false);
-                }
-
-            } else if (sequenceDB != null) {
-                sequenceNumberField.setText("Total Sequence Number: " +
-                        sequenceDB.size());
-
-
-                activeSequenceDB = (CSSequenceSet) sequenceDB;
-            }
-
-        } else if (sequenceDB != null) {
-            sequenceNumberField.setText("Total Sequence Number: " +
-                    sequenceDB.size());
-              sequenceNumberField.setEditable(false);
-        }
-
-
-    }
-
-    /**
-     * createActivatedMarkers
-     */
-    public void createActivatedSequenceSet() {
-
-    }
-
-}
-
-
-class CSSequenceSetViewEventBase_chkActivateMarkers_actionAdapter implements
-        ActionListener {
-    private CSSequenceSetViewEventBase adaptee;
-
-    CSSequenceSetViewEventBase_chkActivateMarkers_actionAdapter(
-            CSSequenceSetViewEventBase adaptee) {
-        this.adaptee = adaptee;
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        adaptee.chkActivateMarkers_actionPerformed(e);
-    }
+	}
 }
