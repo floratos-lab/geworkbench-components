acf22872fb4310c7919228349b56fe8f3340b943
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2009-07-07 20:30:16 +0000

    adding gsea results visualizer
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5384 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/gsea/browser/GSEABrowser.java
@@ -0,0 +1,256 @@
+package org.geworkbench.components.gpmodule.gsea.browser;
+
+import org.geworkbench.engine.management.AcceptTypes;
+import org.geworkbench.engine.config.VisualPlugin;
+import org.geworkbench.bison.datastructure.biocollections.gsea.CSGSEAResultDataSet;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.events.ProjectEvent;
+import org.geworkbench.components.gpmodule.gsea.browser.TabBrowser;
+import org.geworkbench.components.gpmodule.gsea.browser.CloseableTabbedPane;
+import org.genepattern.io.ArchiveUtil;
+import org.jdesktop.jdic.browser.*;
+
+import javax.swing.*;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.awt.*;
+import java.io.*;
+
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.lang.reflect.Method;
+
+/**
+ * User: nazaire
+ */
+
+@AcceptTypes({CSGSEAResultDataSet.class})
+public class GSEABrowser implements VisualPlugin
+{
+    private String gseaURL;
+
+    private CSGSEAResultDataSet gseaResultDataSet;
+    private DSDataSet dataSet;
+
+    private JPanel mainPanel = new JPanel(new BorderLayout());
+	private JPanel jp = new JPanel(new BorderLayout());
+    private static CloseableTabbedPane jtp = new CloseableTabbedPane();
+
+    private TabBrowser tb;
+    private WebBrowser wb = null;
+    private Object webBrowser = null;
+	private Method setURL = null;
+    private String tabtitle = null;
+
+    private String process_id = null;
+    //private MyStatusBar statusBar = new MyStatusBar();
+	private boolean initial = true;
+	private boolean link = true;
+
+    private static String osname = System.getProperty("os.name").toLowerCase();
+    private final static boolean is_mac = (osname.indexOf("mac") > -1);
+    private final static boolean is_windows = (osname.indexOf("windows") > -1);
+
+    private final static boolean useIE = is_windows;
+
+	private static Properties prop = new Properties();
+    private static String mozilla_path = null;
+    static
+    {
+		BrowserEngineManager bem = BrowserEngineManager.instance();
+		if (is_mac)
+			bem.setActiveEngine(BrowserEngineManager.WEBKIT);
+		else if (is_windows)
+			bem.setActiveEngine(BrowserEngineManager.IE);
+		else {
+			bem.setActiveEngine(BrowserEngineManager.MOZILLA);
+			IBrowserEngine be = bem.getActiveEngine();
+			try{
+				FileInputStream fis = new FileInputStream("conf/jdic.properties");
+				prop.load(fis);
+				mozilla_path = prop.getProperty("mozilla.path");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			be.setEnginePath(mozilla_path);
+		}
+	}
+
+    protected JTabbedPane getTabbedPane()
+    {
+        return jtp;
+    }
+
+    public Component getComponent()
+    {
+        return mainPanel;
+    }
+
+    private void display()
+    {
+        /*if (is_windows)
+	    {
+	        if(tb==null || !tb.isInitialized())
+            {
+		        try
+                {
+		            tb = new TabBrowser(new URL(MARKUS_RESULT_URL
+							+ process_id), useIE);
+				    tb.addWebBrowserListener(new WebTabListener());
+				    tb.setMainBrowser(this);
+				    jp.removeAll();
+				    jp.add(tb, BorderLayout.CENTER);
+				    jtp.addTab(tabtitle, jp);
+				    mainPanel.add(jtp, BorderLayout.CENTER);
+				    mainPanel.invalidate();
+				    mainPanel.repaint();
+	            }
+                catch (MalformedURLException e)
+                {
+		            e.printStackTrace();
+		            return;
+			    }
+	        }
+        } */
+
+        try
+        {
+            if (is_mac)
+		        setURL.invoke(webBrowser, gseaURL);
+		    else if (!is_windows)
+			{
+
+                wb = new WebBrowser(new URL(gseaURL));
+				jp.removeAll();
+				jp.add(wb, BorderLayout.CENTER);
+				jtp.addTab(gseaResultDataSet.getReportFile(), jp);
+				mainPanel.add(jtp, BorderLayout.CENTER);
+				mainPanel.invalidate();
+				mainPanel.repaint();
+		    }
+	    }
+        catch(Exception e)
+        {
+		    e.printStackTrace();
+        }
+    }
+
+    private void extractResults()
+    {        
+        ArchiveUtil.unzipRecursive(new File(gseaResultDataSet.getReportFile()), new File(System.getProperty("temporary.files.directory")));
+
+        System.out.println("report file:" + gseaResultDataSet.getReportFile());
+        try
+        {
+            gseaURL = (new File(System.getProperty("temporary.files.directory") +"index.html")).toURL().toString();
+        }
+        catch(MalformedURLException me)
+        {
+            me.printStackTrace();
+        }
+    }
+    
+    public void receive(ProjectEvent e, Object source)
+    {
+        if(e.getDataSet() != null && e.getDataSet() instanceof CSGSEAResultDataSet)
+        {
+            gseaResultDataSet = ((CSGSEAResultDataSet)e.getDataSet());
+            dataSet = gseaResultDataSet.getParentDataSet();
+
+            extractResults();
+            display();
+        }
+
+    }
+    // web browser listener
+	protected class WebTabListener implements WebBrowserListener
+    {
+		public void downloadStarted(WebBrowserEvent event) {
+			//updateStatusInfo("Loading started.");
+		}
+
+		public void initializationCompleted(WebBrowserEvent event) {
+			//updateStatusInfo("Initialization completed.");
+		}
+
+		public void downloadCompleted(WebBrowserEvent event) {
+			//updateStatusInfo("Loading completed.");
+		}
+
+		public void downloadError(WebBrowserEvent event) {
+			//updateStatusInfo("Loading error.");
+		}
+
+		public void documentCompleted(WebBrowserEvent event) {
+			if (link == true)
+				return;
+
+			// set url or content in this function to get the page displayed
+
+			try {
+				if (tb.getURL() != null) {
+					URL currentUrl = tb.getURL();
+					String curl = currentUrl.toString();
+					System.out.println("\nfrom " + curl + " to " + gseaURL);
+					if (!gseaURL.equals(curl)) {
+						System.out.println("isinitialized: "
+								+ tb.isInitialized() + "; newurl: " + gseaURL);
+
+						tb.setURL(new URL(gseaURL));
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			//updateStatusInfo("Document loading completed.");
+			link = true;
+		}
+
+		public void downloadProgress(WebBrowserEvent webEvent) {
+			String values = webEvent.getData();
+			if (values != null) {
+				StringTokenizer tokenizer = new StringTokenizer(values, " ");
+
+				if (tokenizer.hasMoreTokens()) {
+					String current = tokenizer.nextToken();
+					if (tokenizer.hasMoreTokens()) {
+						String max = tokenizer.nextToken();
+
+						int progress = Integer.parseInt(current);
+						int progressMax = Integer.parseInt(max);
+
+						onDownloadProgress(webEvent, progress, progressMax);
+					}
+				}
+			}
+		}
+
+		// whatever this was meant to do, it was not implemented
+		private void onDownloadProgress(WebBrowserEvent webEvent, int progress,
+				int progressMax) {
+		}
+
+		public void windowClose(WebBrowserEvent event) {
+			//updateStatusInfo("Closed by script.");
+			System.out.println("closed by script." + event.getData());
+			if (JOptionPane.YES_OPTION == JOptionPane
+					.showConfirmDialog(
+							tb,
+							"The webpage you are viewing is trying to close the window.\n Do you want to close this window?",
+							"Warning", JOptionPane.YES_NO_OPTION,
+							JOptionPane.QUESTION_MESSAGE)) {
+				System.exit(0);
+			}
+		}
+
+		public void titleChange(WebBrowserEvent event) {
+			tabtitle = event.getData();
+			jtp.setTitleAt(jtp.getTabCount() - 1, tabtitle);
+			//updateStatusInfo("Title of the browser window changed.");
+		}
+
+		public void statusTextChange(WebBrowserEvent event) {
+		}
+	}
+}
---
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/gsea/browser/TabBrowser.java
@@ -0,0 +1,76 @@
+package org.geworkbench.components.gpmodule.gsea.browser;
+
+import org.jdesktop.jdic.browser.WebBrowser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.swing.*;
+import java.net.URL;
+import java.awt.*;
+
+/**
+ * User: nazaire
+ */
+public class TabBrowser extends WebBrowser
+{
+    private static final long serialVersionUID = -4833106758989294420L;
+    Log log = LogFactory.getLog(TabBrowser.class);
+
+    public static GSEABrowser gb;
+    private boolean useIE = true;
+
+    public TabBrowser(URL url, boolean IE)
+    {
+        super(url);
+        setURL(url, url.getQuery(), "user-agent:geWorkbench");
+        useIE = IE;
+    }
+
+   // return false to block popup window
+    protected boolean willOpenWindow(URL url)
+    {
+        if (SwingUtilities.isEventDispatchThread())
+        {
+            log.info("new url '" + url + "'");
+            createAllowedContent(url);
+        }
+        else
+        {
+            log.error("willOpenWindow called in a non-EDT thread.");
+        }
+        return false;
+    }
+
+    // return false to block url navigation
+    protected boolean willOpenURL(URL url)
+    {
+        boolean rc = super.willOpenURL(url);
+        return rc;
+    }
+
+    public void setMainBrowser(GSEABrowser mainbrowser)
+    {
+        gb = mainbrowser;
+    }
+
+    public GSEABrowser getMainBrowser()
+    {
+        return gb;
+    }
+
+    /**
+     * add a new tab for the main browser panel
+     * @param url
+     */
+    private void createAllowedContent(URL url)
+    {
+        TabBrowser newtb = new TabBrowser(url, useIE);
+        GSEABrowser.WebTabListener wtl = gb.new WebTabListener();
+        newtb.addWebBrowserListener(wtl);
+        JPanel subjp = new JPanel(new BorderLayout());
+        subjp.add(newtb, BorderLayout.CENTER);
+        gb.getTabbedPane().addTab("Tab", subjp);
+        gb.getTabbedPane().setSelectedIndex(
+        gb.getTabbedPane().getTabCount() - 1);
+    }
+}
