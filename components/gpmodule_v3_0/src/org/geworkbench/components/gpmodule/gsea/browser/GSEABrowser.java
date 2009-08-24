b076788b226ece3da3a77cf64f30a6a1d8b9e742
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2009-08-24 21:06:18 +0000

    added browser buttons and code for running on Windows
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5495 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/gsea/browser/GSEABrowser.java
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/gsea/browser/GSEABrowser.java
@@ -1,12 +1,11 @@
 package org.geworkbench.components.gpmodule.gsea.browser;
 
 import org.geworkbench.engine.management.AcceptTypes;
+import org.geworkbench.engine.management.Subscribe;
 import org.geworkbench.engine.config.VisualPlugin;
 import org.geworkbench.bison.datastructure.biocollections.gsea.CSGSEAResultDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.events.ProjectEvent;
-import org.geworkbench.components.gpmodule.gsea.browser.TabBrowser;
-import org.geworkbench.components.gpmodule.gsea.browser.CloseableTabbedPane;
 import org.genepattern.io.ArchiveUtil;
 import org.jdesktop.jdic.browser.*;
 
@@ -14,6 +13,8 @@ import javax.swing.*;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
 import java.util.Properties;
@@ -43,8 +44,9 @@ public class GSEABrowser implements VisualPlugin
     private String tabtitle = null;
 
     private String process_id = null;
-    //private MyStatusBar statusBar = new MyStatusBar();
-	private boolean initial = true;
+    private MyStatusBar statusBar = new MyStatusBar();
+
+    private boolean initial = true;
 	private boolean link = true;
 
     private static String osname = System.getProperty("os.name").toLowerCase();
@@ -76,6 +78,8 @@ public class GSEABrowser implements VisualPlugin
 		}
 	}
 
+    protected GSEABrowser(){}
+
     protected JTabbedPane getTabbedPane()
     {
         return jtp;
@@ -88,13 +92,13 @@ public class GSEABrowser implements VisualPlugin
 
     private void display()
     {
-        /*if (is_windows)
+        if (is_windows)
 	    {
 	        if(tb==null || !tb.isInitialized())
             {
 		        try
                 {
-		            tb = new TabBrowser(new URL(MARKUS_RESULT_URL
+		            tb = new TabBrowser(new URL(gseaURL
 							+ process_id), useIE);
 				    tb.addWebBrowserListener(new WebTabListener());
 				    tb.setMainBrowser(this);
@@ -111,49 +115,149 @@ public class GSEABrowser implements VisualPlugin
 		            return;
 			    }
 	        }
-        } */
+        }
 
+        ToolTipManager.sharedInstance().setLightWeightPopupEnabled(false);
+
+		statusBar.setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
+		statusBar.lblDesc.setText("JDIC Browser");
+        
         try
         {
-            if (is_mac)
-		        setURL.invoke(webBrowser, gseaURL);
-		    else if (!is_windows)
-			{
+            if(initial)
+            {
+                initial = false;
+                if (is_mac)
+                {
+                    Class wkwbc = Class.forName("org.jdesktop.jdic.browser.WebKitWebBrowser");
+			        webBrowser = wkwbc.newInstance();
+                    Method setContent = wkwbc.getMethod("setContent", Class.forName("java.lang.String"));
+			        setURL = wkwbc.getMethod("setURL", Class.forName("java.net.URL"));
+			        setContent.invoke(webBrowser, "MacRoman");
+                    setURL.invoke(webBrowser, new URL(gseaURL));
+                    jp.add((java.awt.Component)webBrowser, BorderLayout.CENTER);
 
-                wb = new WebBrowser(new URL(gseaURL));
-				jp.removeAll();
-				jp.add(wb, BorderLayout.CENTER);
-				jtp.addTab(gseaResultDataSet.getReportFile(), jp);
-				mainPanel.add(jtp, BorderLayout.CENTER);
-				mainPanel.invalidate();
-				mainPanel.repaint();
-		    }
-	    }
+                    JButton jStopButton = new JButton("Stop",
+						new ImageIcon("src/images/Stop.png"));
+				    JButton jRefreshButton = new JButton("Refresh",
+						new ImageIcon("src/images/Reload.png"));
+				    JButton jForwardButton = new JButton("Forward",
+						new ImageIcon("src/images/Forward.png"));
+				    JButton jBackButton = new JButton("Back",
+						new ImageIcon("src/images/Back.png"));
+				    jBackButton.setHorizontalTextPosition(SwingConstants.TRAILING);
+				    jBackButton.setEnabled(true);
+				    jBackButton.setMaximumSize(new Dimension(75, 27));
+				    jBackButton.setPreferredSize(new Dimension(75, 27));
+				    jBackButton.addActionListener(new ActionListener(){
+					    public void actionPerformed(ActionEvent ae) {
+						((IWebBrowser)webBrowser).back(); }});
+				    jForwardButton.setEnabled(true);
+				    jForwardButton.addActionListener(new ActionListener(){
+					    public void actionPerformed(ActionEvent ae) {
+						((IWebBrowser)webBrowser).forward(); }});
+				    jRefreshButton.setEnabled(true);
+				    jRefreshButton.setMaximumSize(new Dimension(75, 27));
+				    jRefreshButton.setMinimumSize(new Dimension(75, 27));
+				    jRefreshButton.setPreferredSize(new Dimension(75, 27));
+				    jRefreshButton.addActionListener(new ActionListener(){
+					    public void actionPerformed(ActionEvent ae) {
+						((IWebBrowser)webBrowser).refresh(); }});
+				    jStopButton.setVerifyInputWhenFocusTarget(true);
+				    jStopButton.setText("Stop");
+				    jStopButton.setEnabled(true);
+				    jStopButton.setMaximumSize(new Dimension(75, 27));
+				    jStopButton.setMinimumSize(new Dimension(75, 27));
+				    jStopButton.setPreferredSize(new Dimension(75, 27));
+				    jStopButton.addActionListener(new ActionListener(){
+					    public void actionPerformed(ActionEvent ae){
+						((IWebBrowser)webBrowser).stop(); }});
+				    JToolBar jBrowserToolBar = new JToolBar();
+				    jBrowserToolBar.setFloatable(false);
+				    jBrowserToolBar.add(jBackButton, null);
+				    jBrowserToolBar.add(jForwardButton, null);
+				    jBrowserToolBar.addSeparator();
+				    jBrowserToolBar.add(jRefreshButton, null);
+				    jBrowserToolBar.add(jStopButton, null);
+				    jBrowserToolBar.setBorder(BorderFactory.createCompoundBorder(
+					BorderFactory.createEtchedBorder(),
+					BorderFactory.createEmptyBorder(2, 2, 2, 0)));
+
+                    //jp.add(jBrowserToolBar, BorderLayout.NORTH);
+				    jtp.addTab(tabtitle, jp);
+                }
+		        else if (is_windows)
+			    {
+					// Print out debug messages in the command line.
+					// tb.setDebug(true);
+					tb = new org.geworkbench.components.gpmodule.gsea.browser.TabBrowser(new URL(gseaURL), useIE);
+					tb.addWebBrowserListener(new WebTabListener());
+					tb.setMainBrowser(this);
+
+					jp.removeAll();
+					jp.add(tb, BorderLayout.CENTER);
+					jtp.addTab(tabtitle, jp);
+			    }
+			    else
+			    {
+					// set auto_dispose=false to avoid dead mozilla browser in linux
+					wb = new WebBrowser(new URL(gseaURL), useIE);
+					jp.removeAll();
+					jp.add(wb, BorderLayout.CENTER);
+					jtp.addTab(process_id, jp);
+			    }
+                mainPanel.add(jtp, BorderLayout.CENTER);
+			    mainPanel.add(statusBar, BorderLayout.SOUTH); 
+            }
+            else {
+				if (is_mac)
+				    setURL.invoke(webBrowser, new URL(gseaURL));
+				else if (is_windows)
+					tb.setURL(new URL(gseaURL)); // add this no matter what
+				else
+				{
+					wb = new WebBrowser(new URL(gseaURL));
+				    jp.removeAll();
+				    jp.add(wb, BorderLayout.CENTER);
+				    jtp.addTab(process_id, jp);
+				    mainPanel.add(jtp, BorderLayout.CENTER);
+				    mainPanel.invalidate();
+				    mainPanel.repaint();
+				}
+			}
+        }
         catch(Exception e)
         {
 		    e.printStackTrace();
         }
+
+        mainPanel.revalidate();
+		mainPanel.repaint();
     }
 
     private void extractResults()
     {        
-        ArchiveUtil.unzipRecursive(new File(gseaResultDataSet.getReportFile()), new File(System.getProperty("temporary.files.directory")));
+        ArchiveUtil.unzipRecursive(new File(gseaResultDataSet.getReportFile()), new File(System.getProperty("temporary.files.directory") + "/gsearesults"));
 
         System.out.println("report file:" + gseaResultDataSet.getReportFile());
         try
         {
-            gseaURL = (new File(System.getProperty("temporary.files.directory") +"index.html")).toURL().toString();
+            gseaURL = (new File(System.getProperty("temporary.files.directory") + "/gsearesults/"+ "index.html")).toURL().toString();
+            System.out.println("GSEA url:" + gseaURL);
         }
         catch(MalformedURLException me)
         {
             me.printStackTrace();
         }
     }
-    
+
+    @Subscribe    
     public void receive(ProjectEvent e, Object source)
     {
         if(e.getDataSet() != null && e.getDataSet() instanceof CSGSEAResultDataSet)
         {
+            System.out.println("GSEABrowser received project node added event.");
+
             gseaResultDataSet = ((CSGSEAResultDataSet)e.getDataSet());
             dataSet = gseaResultDataSet.getParentDataSet();
 
@@ -166,19 +270,19 @@ public class GSEABrowser implements VisualPlugin
 	protected class WebTabListener implements WebBrowserListener
     {
 		public void downloadStarted(WebBrowserEvent event) {
-			//updateStatusInfo("Loading started.");
+			updateStatusInfo("Loading started.");
 		}
 
 		public void initializationCompleted(WebBrowserEvent event) {
-			//updateStatusInfo("Initialization completed.");
+			updateStatusInfo("Initialization completed.");
 		}
 
 		public void downloadCompleted(WebBrowserEvent event) {
-			//updateStatusInfo("Loading completed.");
+			updateStatusInfo("Loading completed.");
 		}
 
 		public void downloadError(WebBrowserEvent event) {
-			//updateStatusInfo("Loading error.");
+			updateStatusInfo("Loading error.");
 		}
 
 		public void documentCompleted(WebBrowserEvent event) {
@@ -203,7 +307,7 @@ public class GSEABrowser implements VisualPlugin
 				e.printStackTrace();
 			}
 
-			//updateStatusInfo("Document loading completed.");
+			updateStatusInfo("Document loading completed.");
 			link = true;
 		}
 
@@ -232,7 +336,7 @@ public class GSEABrowser implements VisualPlugin
 		}
 
 		public void windowClose(WebBrowserEvent event) {
-			//updateStatusInfo("Closed by script.");
+			updateStatusInfo("Closed by script.");
 			System.out.println("closed by script." + event.getData());
 			if (JOptionPane.YES_OPTION == JOptionPane
 					.showConfirmDialog(
@@ -247,10 +351,22 @@ public class GSEABrowser implements VisualPlugin
 		public void titleChange(WebBrowserEvent event) {
 			tabtitle = event.getData();
 			jtp.setTitleAt(jtp.getTabCount() - 1, tabtitle);
-			//updateStatusInfo("Title of the browser window changed.");
+			updateStatusInfo("Title of the browser window changed.");
 		}
 
 		public void statusTextChange(WebBrowserEvent event) {
 		}
 	}
+
+    void updateStatusInfo(String statusMessage) {
+		statusBar.lblStatus.setText(statusMessage);
+	}
+
+    static class CustomExceptionHandler implements Thread.UncaughtExceptionHandler
+    {
+        public void uncaughtException(Thread t, Throwable e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
