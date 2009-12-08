691bf9717b332e88fe773e015b91f167bff87bb5
Oleg Shteynbuk <os2201@c2b2.columbia.edu>
2009-12-08 16:10:52 +0000

    bug 1962 component - discovery changed dir for temp files
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5793 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/discovery/src/org/geworkbench/util/session/SoapClient.java
+++ components/discovery/src/org/geworkbench/util/session/SoapClient.java
@@ -16,6 +16,8 @@ import org.geworkbench.bison.datastructure.biocollections.sequences.
         CSSequenceSet;
 import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
+import org.geworkbench.util.FilePathnameUtils;
+
 import java.io.IOException;
 
 
@@ -526,12 +528,7 @@ public class SoapClient {
     }
 
     public String getLocalFileName(String path) {
-        String tempFolder = System.getProperties().getProperty(
-                "temporary.files.directory");
-        if (tempFolder == null) {
-            tempFolder = "./";
-
-        }
+		String tempFolder = FilePathnameUtils.getTemporaryFilesDirectoryPath();
         return tempFolder + getFileName(path);
     }
 
