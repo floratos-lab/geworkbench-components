e84ae5ec3f238bc6e68dc6f5f1ff69191f087929
Zhou Ji <zji@c2b2.columbia.edu>
2010-01-14 17:08:43 +0000

    minor cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5916 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/viewers/src/org/geworkbench/components/viewers/ImageDisplay.java
+++ components/viewers/src/org/geworkbench/components/viewers/ImageDisplay.java
@@ -1,40 +1,44 @@
 package org.geworkbench.components.viewers;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 /**
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: First Genetic Trust Inc.</p>
- *
+ * A panel to display image.
+ * 
  * @author First Genetic Trust Inc.
  * @version 1.0
  */
 public class ImageDisplay extends JPanel {
-    /**
-     * Image painted on this <code>Component</code>
-     */
-    ImageIcon image = null;
+	private static final long serialVersionUID = -3330449026306112459L;
 
-    public ImageDisplay() {
-    }
+	/**
+	 * Image painted on this <code>Component</code>
+	 */
+	ImageIcon image = null;
 
-    public void setImage(ImageIcon i) {
-        image = i;
-    }
+	public ImageDisplay() {
+	}
 
-    /**
-     * {@link java.awt.Component Component} method
-     *
-     * @param g <code>Graphics</code> to be painted with
-     */
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.white);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        if (image != null)
-            g.drawImage(image.getImage(), 0, 0, Color.white, this);
-    }
+	public void setImage(ImageIcon i) {
+		image = i;
+	}
+
+	/**
+	 * {@link java.awt.Component Component} method
+	 * 
+	 * @param g
+	 *            <code>Graphics</code> to be painted with
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.white);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		if (image != null)
+			g.drawImage(image.getImage(), 0, 0, Color.white, this);
+	}
 
 }
-
--- components/viewers/src/org/geworkbench/components/viewers/ImageViewer.java
+++ components/viewers/src/org/geworkbench/components/viewers/ImageViewer.java
@@ -1,74 +1,84 @@
 package org.geworkbench.components.viewers;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
 import org.geworkbench.builtin.projects.ImageData;
 import org.geworkbench.engine.config.VisualPlugin;
 import org.geworkbench.engine.management.AcceptTypes;
 import org.geworkbench.engine.management.Subscribe;
 import org.geworkbench.events.ImageSnapshotEvent;
 
-import javax.swing.*;
-import java.awt.*;
-
 /**
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: First Genetic Trust Inc.</p>
- *
+ * A simple image viewer.
+ * 
  * @author First Genetic Trust Inc.
- * @version 1.0
+ * @version $Id$
  */
-@AcceptTypes({ImageData.class}) public class ImageViewer extends JPanel implements VisualPlugin {
-    /**
-     * Canvas on which Image is painted
-     */
-    ImageDisplay display = new ImageDisplay();
-    /**
-     * Visual Widgets
-     */
-    BorderLayout borderLayout1 = new BorderLayout();
-    JScrollPane jScrollPane1 = new JScrollPane();
+@AcceptTypes( { ImageData.class })
+public class ImageViewer extends JPanel implements VisualPlugin {
+	private static final long serialVersionUID = 8164752383066595089L;
 
-    public ImageViewer() {
-        try {
-            jbInit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	/**
+	 * Canvas on which Image is painted
+	 */
+	ImageDisplay display = new ImageDisplay();
+	/**
+	 * Visual Widgets
+	 */
+	BorderLayout borderLayout1 = new BorderLayout();
+	JScrollPane jScrollPane1 = new JScrollPane();
 
-    }
+	public ImageViewer() {
+		try {
+			jbInit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    /**
-     * Interface <code>VisualPlugin</code> method
-     *
-     * @return visual representation of this component
-     */
-    public Component getComponent() {
-        return this;
-    }
+	}
 
-    @Subscribe public void receive(ImageSnapshotEvent event, Object source) {
-        if (event.getAction() == org.geworkbench.events.ImageSnapshotEvent.Action.SHOW) {
-            ImageIcon image = event.getImage();
-            display.setImage(image);
-            if (image != null) {
-                display.setSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
-                display.setPreferredSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
-            }
+	/**
+	 * Interface <code>VisualPlugin</code> method
+	 * 
+	 * @return visual representation of this component
+	 */
+	public Component getComponent() {
+		return this;
+	}
 
-            if (this.getParent() instanceof JTabbedPane) {
-                ((JTabbedPane) this.getParent()).setSelectedComponent(this);
-            }
+	@Subscribe
+	public void receive(ImageSnapshotEvent event, Object source) {
+		if (event.getAction() == org.geworkbench.events.ImageSnapshotEvent.Action.SHOW) {
+			ImageIcon image = event.getImage();
+			display.setImage(image);
+			if (image != null) {
+				display.setSize(new Dimension(image.getIconWidth(), image
+						.getIconHeight()));
+				display.setPreferredSize(new Dimension(image.getIconWidth(),
+						image.getIconHeight()));
+			}
 
-            repaint();
-        } else {
-            // no-op
-        }
-    }
+			if (this.getParent() instanceof JTabbedPane) {
+				((JTabbedPane) this.getParent()).setSelectedComponent(this);
+			}
 
-    private void jbInit() throws Exception {
-        this.setLayout(borderLayout1);
-        jScrollPane1.getViewport().add(display, null);
-        this.add(jScrollPane1, BorderLayout.CENTER);
-    }
+			repaint();
+		} else {
+			// no-op
+		}
+	}
+
+	private void jbInit() throws Exception {
+		this.setLayout(borderLayout1);
+		jScrollPane1.getViewport().add(display, null);
+		this.add(jScrollPane1, BorderLayout.CENTER);
+	}
 
 }
-
