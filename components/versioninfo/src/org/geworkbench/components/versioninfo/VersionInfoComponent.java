9867af3209318ccc30e10e6dc924784637a04717
Zhou Ji <zji@c2b2.columbia.edu>
2010-01-15 16:29:40 +0000

    cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5919 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/versioninfo/src/org/geworkbench/components/versioninfo/VersionInfoComponent.java
+++ components/versioninfo/src/org/geworkbench/components/versioninfo/VersionInfoComponent.java
@@ -3,55 +3,43 @@ package org.geworkbench.components.versioninfo;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
-
+import java.util.Map;
 
 /**
- * <p>Title: caWorkbench</p>
- * <p/>
- * <p>Description: Modular Application Framework for Gene Expession, Sequence
- * and Genotype Analysis</p>
- * <p/>
- * <p>Copyright: Copyright (c) 2003 -2004</p>
- * <p/>
- * <p>Company: Columbia University</p>
- *
+ * The component to support the version dialog.
+ * 
  * @author not attributable
- * @version 3.0
+ * @version $Id$
  */
-public class VersionInfoComponent implements org.geworkbench.engine.config.MenuListener {
-    //Holds refrences to listeners of menu items for this component.
-    private HashMap listeners = new HashMap();
+public class VersionInfoComponent implements
+		org.geworkbench.engine.config.MenuListener {
+	// Holds references to listeners of menu items for this component.
+	private Map<String, ActionListener> listeners = new HashMap<String, ActionListener>();
 
-    public VersionInfoComponent() {
-        //Register menu items listener - sessions dialog
-        ActionListener allSession = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                helpAbout();
-            }
-        };
-        listeners.put("Help.About", allSession);
-    }
+	public VersionInfoComponent() {
+		// Register menu items listener - sessions dialog
+		ActionListener allSession = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VersionInfoDialog dlg = new VersionInfoDialog();
+				dlg.setLocationRelativeTo(null);
+				dlg.setModal(true);
+				dlg.setVisible(true);
+			}
+		};
+		listeners.put("Help.Version", allSession);
+	}
 
-    /**
-     * Display the About box.
-     */
-
-    void helpAbout() {
-        VersionInfoDialog dlg = new VersionInfoDialog();
-        dlg.setLocationRelativeTo(null);
-        dlg.setModal(true);
-        dlg.show();
-
-    }
-
-    /**
-     * Return a listener which registered with the var string.
-     *
-     * @param var - the name of the listener
-     * @return - the listener
-     */
-    public ActionListener getActionListener(String var) {
-        return (ActionListener) listeners.get(var);
-    }
+	/**
+	 * Return a listener which registered with the var string.
+	 * 
+	 * @param var
+	 *            - the name of the listener
+	 * @return - the listener
+	 */
+	public ActionListener getActionListener(String var) {
+		// in fact, this component only handles version dialog, so the map are not necessary 
+		// and it should always return an action listener that shows version dialog. 
+		return (ActionListener) listeners.get(var);
+	}
 
 }
