3a2135bdcb9e46190f5600922184a199d9d1f281
Zhou Ji <zji@c2b2.columbia.edu>
2010-02-25 20:42:19 +0000

    remove dead code (this round is relatively straightforward ones)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6068 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/alignment/src/org/geworkbench/components/alignment/grid/service/ServiceInformation.java
+++
@@ -1,8 +0,0 @@
-package org.geworkbench.components.alignment.grid.service;
-
-public interface ServiceInformation {
-    public Object getServiceUniqueInformation();
-
-    public void setServiceUniqueInformation(Object serviceUniqueInformation);
-
-}
