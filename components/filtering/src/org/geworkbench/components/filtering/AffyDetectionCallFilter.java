96f1bdc24a62e3a00efbc82232cbb5ffad59a0e5
Zhou Ji <zji@c2b2.columbia.edu>
2010-03-03 22:11:03 +0000

    refactoring and cleaning-up as the first step to implement feature request (mantis 1700)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6099 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/filtering/src/org/geworkbench/components/filtering/AffyDetectionCallFilter.java
+++ components/filtering/src/org/geworkbench/components/filtering/AffyDetectionCallFilter.java
@@ -1,11 +1,8 @@
 package org.geworkbench.components.filtering;
 
-import org.geworkbench.analysis.AbstractAnalysis;
-import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.CSMarkerValue;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSAffyMarkerValue;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
-import org.geworkbench.bison.model.analysis.AlgorithmExecutionResults;
 import org.geworkbench.bison.model.analysis.FilteringAnalysis;
 
 
@@ -13,7 +10,7 @@ import org.geworkbench.bison.model.analysis.FilteringAnalysis;
  * <p>Copyright: Copyright (c) 2003</p>
  * <p>Company: First Genetic Trust Inc.</p>
  * @author First Genetic Trust Inc.
- * @version 1.0
+ * @version $Id$
  */
 
 /**
@@ -22,7 +19,7 @@ import org.geworkbench.bison.model.analysis.FilteringAnalysis;
  * The user preferences are collected in this filter's associate parameters
  * GUI (<code>AffyDetectionCallFilterPanel</code>).
  */
-public class AffyDetectionCallFilter extends AbstractAnalysis implements FilteringAnalysis {
+public class AffyDetectionCallFilter extends FilteringAnalysis {
     /**
 	 * 
 	 */
@@ -35,65 +32,43 @@ public class AffyDetectionCallFilter extends AbstractAnalysis implements Filteri
         setDefaultPanel(new AffyDetectionCallFilterPanel());
     }
 
-    public int getAnalysisType() {
-        return AbstractAnalysis.AFFY_DETECTION_CALL_FILTER;
+    
+    @Override
+	protected boolean expectedType() {
+        // assuming the first marker has the same type as all other markers
+        DSMicroarray mArray = maSet.get(0);
+        CSMarkerValue mv = (CSMarkerValue) mArray.getMarkerValue(0);
+        return (mv instanceof DSAffyMarkerValue);
+	}
+    
+	protected String expectedTypeName = "Affymetrix";
+
+    @Override
+    protected boolean isMissing(int arrayIndex, int markerIndex) {
+        DSMicroarray mArray = maSet.get(arrayIndex);
+    	CSMarkerValue mv = (CSMarkerValue) mArray.getMarkerValue(markerIndex);
+
+    	if (mv == null || mv.isMissing()) {
+            return false;
+        }
+        if (mv.isPresent() && filterPresent) {
+            return true;
+        }
+        if (mv.isAbsent() && filterAbsent) {
+            return true;
+        }
+        if (mv.isMarginal() && filterMarginal) {
+            return true;
+        }
+        return false;
     }
-
-    public AlgorithmExecutionResults execute(Object input) {
-        if (input == null) return new AlgorithmExecutionResults(false, "Invalid input.", null);
-        assert input instanceof DSMicroarraySet;
-        DSMicroarraySet<DSMicroarray> maSet = (DSMicroarraySet) input;
-        int arrayCount = maSet.size();
-        int markerCount = maSet.getMarkers().size();
-
-        // Check that this is an Affy type chip
-        if ( markerCount > 0 && arrayCount > 0 &&
-             !(maSet.get(0).getMarkerValue(0) instanceof DSAffyMarkerValue))
-            return new AlgorithmExecutionResults(false, "Select an Affymetrix dataset.", null);
-
-        // Collect the parameters needed for the execution of the filter
+    
+	@Override
+	protected void getParametersFromPanel() {
         filterPresent = ((AffyDetectionCallFilterPanel) aspp).isPresentSelected();
         filterAbsent = ((AffyDetectionCallFilterPanel) aspp).isAbsentSelected();
         filterMarginal = ((AffyDetectionCallFilterPanel) aspp).isMarginalSelected();
-        for (int i = 0; i < arrayCount; i++) {
-            DSMicroarray mArray = maSet.get(i);
-            for (int j = 0; j < markerCount; ++j) {
-                CSMarkerValue mv = (CSMarkerValue) mArray.getMarkerValue(j);
-                if ((mv instanceof DSAffyMarkerValue)) {
-                    if (shouldBeFiltered((mv)))
-                        mv.setMissing(true);
-                } else
-                    return new AlgorithmExecutionResults(false, "This filter can only be used with Affymetrix datasets", null);
-            }
-
-        }
-
-        return new AlgorithmExecutionResults(true, "No errors", input);
-    }
-
-    /**
-     * Check is the argument marker value meets the criteria to be set to "missing".
-     *
-     * @param mv
-     * @return
-     */
-    private boolean shouldBeFiltered(CSMarkerValue mv) {
-            if (mv == null || mv.isMissing()) {
-                return false;
-            }
-            if (mv.isPresent() && filterPresent) {
-                return true;
-            }
-            if (mv.isAbsent() && filterAbsent) {
-                return true;
-            }
-            if (mv.isMarginal() && filterMarginal) {
-                return true;
-            }
-            return false;
-    }
-
-    public String getType() {
-        return "Affy detection call filter";
-    }
+        
+        filterOption = FilterOption.MARKING;
+	}
 }
