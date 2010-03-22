fe5104cb83308b1772bc930a34efa0f5fb927ec5
Zhou Ji <zji@c2b2.columbia.edu>
2010-03-22 21:11:06 +0000

    code cleaning-up (there should be no change in its behavior from the previous version)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6193 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/microarrays/src/org/geworkbench/components/microarrays/TabularMicroarrayPanel.java
+++ components/microarrays/src/org/geworkbench/components/microarrays/TabularMicroarrayPanel.java
@@ -1,11 +1,21 @@
 package org.geworkbench.components.microarrays;
 
-/*
- * The geworkbench_3.0 project
- *
- * Copyright (c) 2006 Columbia University
- *
- */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.text.NumberFormat;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JToolBar;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -15,86 +25,72 @@ import org.geworkbench.bison.datastructure.bioobjects.microarray.DSGenotypicMark
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMarkerValue;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMutableMarkerValue;
-import org.geworkbench.engine.config.VisualPlugin;
 import org.geworkbench.engine.management.AcceptTypes;
 import org.geworkbench.events.MicroarraySetViewEvent;
 import org.geworkbench.util.microarrayutils.MicroarrayViewEventBase;
 
-import javax.swing.*;
-import javax.swing.table.*;
-import java.awt.*;
-import java.text.NumberFormat;
-
 /**
- * <p>Title: caWorkbench</p>
- * <p/>
- * <p>Description: Modular Application Framework for Gene Expession, Sequence and Genotype
- * Analysis</p>
- * <p/>
- * <p>Copyright: Copyright (c) 2003 -2004</p>
- * <p/>
- * <p>Company: Columbia University</p>
+ * 
+ * <p>Copyright Columbia University</p>
  *
  * @author Adam Margolin
- * @version 3.0
+ * @author zji
+ * @version $Id$
  */
 @AcceptTypes({DSMicroarraySet.class})
-public class TabularMicroarrayPanel extends MicroarrayViewEventBase implements VisualPlugin {
+public class TabularMicroarrayPanel extends MicroarrayViewEventBase {
 
-	Log log = LogFactory.getLog(this.getClass());
+	private static Log log = LogFactory.getLog(TabularMicroarrayPanel.class);
 
     public TabularMicroarrayPanel() {
         try {
-            jbInit();
+        	initialize();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    TableCellRenderer renderer = new DefaultTableCellRenderer() {
+    private TableCellRenderer renderer = new DefaultTableCellRenderer() {
         private static final long serialVersionUID = -2148986327315654796L;
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
             Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
-            int visualCol = col;
 
-			if (visualColumnPosition.length != microarrayTableModel.getColumnCount()) {
-				visualColumnPosition = new int[microarrayTableModel.getColumnCount()];
-			}
-			
 			/* Convert the model index to the visual index */
-			visualCol = table.convertColumnIndexToModel(col);
-			visualColumnPosition[col] = visualCol;
+			int modelCol = table.convertColumnIndexToModel(col);
             
-            if (visualCol == 0) {
+            if (modelCol == 0) {
                 c.setBackground(Color.lightGray);
                 ((JLabel) c).setHorizontalAlignment(JLabel.LEFT);
-                ((JLabel) c).setBorder(BorderFactory.createRaisedBevelBorder());
+                ((JComponent) c).setBorder(BorderFactory.createRaisedBevelBorder());
             } else {
                 DSGeneMarker stats = uniqueMarkers.get(row);
                 if (stats != null) {
-                    DSMutableMarkerValue marker = maSetView.items().get(visualCol - 1).getMarkerValue(stats.getSerial());
+                    DSMutableMarkerValue marker = maSetView.items().get(modelCol - 1).getMarkerValue(stats.getSerial());
                     if (marker.isMissing()) {
                         c.setBackground(Color.yellow);
                     } else if (marker.isMasked()) {
                         c.setBackground(Color.pink);
-                    } else if (marker.isMissing()) {
-                        c.setBackground(Color.cyan);
                     } else {
                         c.setBackground(Color.white);
                     }
                     ((JLabel) c).setHorizontalAlignment(JLabel.CENTER);
                     if (isSelected) {
-                        ((JLabel) c).setBorder(BorderFactory.createEtchedBorder());
+                        ((JComponent) c).setBorder(BorderFactory.createEtchedBorder());
                     } else {
-                        ((JLabel) c).setBorder(BorderFactory.createEmptyBorder());
+                        ((JComponent) c).setBorder(BorderFactory.createEmptyBorder());
                     }
                 }
             }
             return c;
         }
     };
-    TableModel microarrayTableModel = new AbstractTableModel() {
-        NumberFormat nf = NumberFormat.getInstance();
+    
+    private AbstractTableModel microarrayTableModel = new AbstractTableModel() {
+		private static final long serialVersionUID = -6400581862850298421L;
+		final NumberFormat nf = NumberFormat.getInstance();
+		{
+			nf.setMaximumFractionDigits(2);
+		}
 
         public int getColumnCount() {
             if (maSetView == null || refMASet == null) {
@@ -114,43 +110,34 @@ public class TabularMicroarrayPanel extends MicroarrayViewEventBase implements V
         }
 
         public Object getValueAt(int row, int col) {
-            nf.setMaximumFractionDigits(2);
-            if (uniqueMarkers.size() > row) {
-                DSGeneMarker stats = uniqueMarkers.get(row);
-                if (stats != null) {
-                    if (col == 0) {
-                        return stats.toString() + stats.getDescription();
-                    } else {
-                        DSMicroarray array = maSetView.get(col - 1);
-                        DSMarkerValue value = array.getMarkerValue(row);
-                        if (value instanceof DSGenotypicMarkerValue) {
-                            return value.toString();
-                        } else {
-                            //                        String value = maSetView.items().get(col - 1).getMarkerValue(stats).toString();
-                            double v = maSetView.getValue(row, col - 1);
-                            return nf.format(v);
-                        }
-                        //                        return value + "";
-                    }
-                } else {
-                    return "Invalid";
-                }
-            } else {
-                return "";
+            if (row >= uniqueMarkers.size()) {
+            	return "";
             }
+
+            DSGeneMarker stats = uniqueMarkers.get(row);
+			if (stats != null) {
+				if (col == 0) {
+					return stats.toString() + stats.getDescription();
+				} else {
+					DSMicroarray array = maSetView.get(col - 1);
+					DSMarkerValue value = array.getMarkerValue(row);
+					if (value instanceof DSGenotypicMarkerValue) {
+						return value.toString();
+					} else {
+						return nf.format(maSetView.getValue(row, col - 1));
+					}
+				}
+			} else {
+				return "Invalid";
+			}
         }
 
         public String getColumnName(int col) {
-            if (mainPanel.isVisible()) {
-                if (maSetView.items().size() > col - 1) {
-                    if (col == 0) {
-                        return "Marker";
-                    } else {
-                        return maSetView.items().get(col - 1).getLabel();
-                    }
-                }
+            if (col == 0) {
+                return "Marker";
+            } else {
+                return maSetView.items().get(col - 1).getLabel();
             }
-            return "";
         }
 
         public boolean isCellEditable(int row, int col) {
@@ -158,20 +145,18 @@ public class TabularMicroarrayPanel extends MicroarrayViewEventBase implements V
         }
     };
 
-    JTable jTable1 = null;
-    private int [] visualColumnPosition = new int[0];
+    private JTable jTable1 = null;
 
-    protected void jbInit() throws Exception {
-        super.jbInit();
-        //DecimalFormat format = new DecimalFormat("#.###"); TODO - is this here as a reminder (not used)?
+    private void initialize() {
         JScrollPane jScrollPane1 = new JScrollPane();
         JToolBar jToolBar1 = new JToolBar();
         JCheckBox jShowAllMArrays = new JCheckBox();
         JCheckBox jShowAllMarkers = new JCheckBox();
 
-        ///JTableHeader jTableHeader1; - TODO unused
         jTable1 = new JTable(microarrayTableModel) {
-            public Dimension getPreferredScrollableViewportSize() {
+			private static final long serialVersionUID = 8762298664180020948L;
+
+			public Dimension getPreferredScrollableViewportSize() {
                 return new Dimension(20 * getModel().getColumnCount(), 20 * getModel().getRowCount());
             }
 
@@ -180,7 +165,6 @@ public class TabularMicroarrayPanel extends MicroarrayViewEventBase implements V
             }
         };
 
-        //jTableHeader1 = jTable1.getTableHeader();
         jTable1.setEnabled(true);
         jTable1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         jTable1.setDefaultRenderer(Object.class, renderer);
@@ -188,89 +172,16 @@ public class TabularMicroarrayPanel extends MicroarrayViewEventBase implements V
 
         jToolBar1.setBorder(null);
         mainPanel.add(jScrollPane1, BorderLayout.CENTER);
-        //        mainPanel.add(jToolBar1, BorderLayout.SOUTH);
         jToolBar1.add(jShowAllMArrays, null);
         jToolBar1.add(jShowAllMarkers, null);
         jScrollPane1.getViewport().add(jTable1, null);
-        // Popup Menu Added to the DrawArea
-        // this.add(jDisplayPanelPopup);
     }
 
-    //public void notifyComponent(Object subscriber, Class anInterface) {
-    // All standard interfaces are handled by the superclass
-    //visualizer.notifyComponent(subscriber, anInterface);
-    //}
-
+    @SuppressWarnings("unchecked")
+	@Override
     protected void fireModelChangedEvent(MicroarraySetViewEvent event) {
-    	if(SwingUtilities.isEventDispatchThread()) {
-    		resetTablularView();
-    	} else {
-    		SwingUtilities.invokeLater(new Thread() {
-    			public void run() {
-    				resetTablularView();
-    			}
-    		});
-    	}
-    }
-
-    private void resetTablularView() {
-
-		if (chkAllMarkers.isSelected()){
-			reset();
-		} else if (onlyActivatedMarkers && activatedMarkers!= null && activatedMarkers.size() > 0){
-			reset();
-		}
-		/* TODO - selection process in this panel is a candidate for refactoring
-		 * it turns out that panels().size() represents the number of "unselected" marker panels (it doesn't matter if 'Selection' is selected)...
-		 * when (multiple) panels (ie. p1, p2) are selected, this size is 1 (=Selection panel).
-		 * For now, I have allowed this to handle the "remove" case.
-		 */
-		else if (markers != null && markers.panels().size() >= 1){
-			reset();
-		} else{
-			log.debug(" static tabular view ");
-		}
-	}
-
-    protected void reset() {
-        synchronized (jTable1) {
-            jTable1.setModel(new DefaultTableModel());
-            jTable1.setModel(microarrayTableModel);
-//    		restoreVisualColumnOrder();
-        }
+    	microarrayTableModel.fireTableStructureChanged();
+    	log.debug("fireModelChangedEvent");
     }
     
-    /**
-     *	Restore the column order in the GUI 
-     * 
-     */
-    private void restoreVisualColumnOrder() {
-		if (microarrayTableModel == null
-				|| microarrayTableModel.getColumnCount() == 0) {
-			return;
-		}
-		if (visualColumnPosition == null
-				|| visualColumnPosition.length != microarrayTableModel
-						.getColumnCount()) {
-			visualColumnPosition = new int[0];
-			return;
-		}
-
-		String[] visualColumnNames = new String[visualColumnPosition.length];
-		for (int i = 0; i < visualColumnPosition.length; i++) {
-			visualColumnNames[i] = jTable1.getColumnName(visualColumnPosition[i]);
-		}
-		
-		for (int to = 0; to < visualColumnPosition.length; to++) {
-			String toName = visualColumnNames[to];
-			for (int from = 0; from < visualColumnPosition.length; from++) {
-				String fromName = jTable1.getColumnName(from);
-				if (fromName.equals(toName)) {
-					/* Restore column position */
-					jTable1.moveColumn(from, to);
-					break;
-				}
-			}
-		}
-	}
 }
