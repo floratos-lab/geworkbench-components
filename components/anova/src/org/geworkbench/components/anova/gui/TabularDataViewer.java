accc0b7578c6b6db4d5a69c0d5ffa51a6cfaf7a4
Zhou Ji <zji@c2b2.columbia.edu>
2010-05-14 21:30:34 +0000

    cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6565 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/anova/src/org/geworkbench/components/anova/gui/TabularDataViewer.java
+++ components/anova/src/org/geworkbench/components/anova/gui/TabularDataViewer.java
@@ -2,7 +2,6 @@ package org.geworkbench.components.anova.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Panel;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.BufferedWriter;
@@ -19,8 +18,8 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.filechooser.FileFilter;
 
-import org.geworkbench.analysis.AbstractSaveableParameterPanel;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSAnovaResultSet;
 import org.geworkbench.engine.config.VisualPlugin;
@@ -39,7 +38,7 @@ import com.jgoodies.forms.layout.FormLayout;
  * This is an example geWorkbench component.
  * 
  * @author Mark Chiang
- * @version $Id: TabularDataViewer.java,v 1.11 2009-02-18 21:26:36 chiangy Exp $
+ * @version $Id$
  */
 // This annotation lists the data set types that this component accepts.
 // The component will only appear when a data set of the appropriate type is
@@ -47,12 +46,9 @@ import com.jgoodies.forms.layout.FormLayout;
 @AcceptTypes({DSAnovaResultSet.class})
 public class TabularDataViewer extends JPanel implements
 		VisualPlugin {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private DSAnovaResultSet anovaResultSet;
+	private static final long serialVersionUID = 2021859129692430268L;
+	
+	private DSAnovaResultSet<? extends DSGeneMarker> anovaResultSet;
 	private TableViewer TV = null;
 
 	//preferences
@@ -87,46 +83,6 @@ public class TabularDataViewer extends JPanel implements
 		    }
 		});
 		
-/*
-		infoLabel = new JLabel("");
-		add(infoLabel);
-		JButton testButton = new JButton("Test Annotations");
-		add(testButton);
-		JButton testButton2 = new JButton("Test GUI");
-		add(testButton2);
-		JTextArea testT = new JTextArea("Test GUI");
-		add(testT);
-
-		Integer[][] A = new Integer[3][6];
-		A[0][0] = new Integer(9);
-		A[1][0] = new Integer(7);
-		A[2][0] = new Integer(3);
-		A[0][1] = new Integer(3);
-		A[1][1] = new Integer(4);
-		A[2][1] = new Integer(5);
-		A[0][2] = new Integer(6);
-		A[1][2] = new Integer(7);
-		A[2][2] = new Integer(8);
-		A[0][3] = new Integer(2);
-		A[1][3] = new Integer(3);
-		A[2][3] = new Integer(4);
-		A[0][4] = new Integer(4);
-		A[1][4] = new Integer(5);
-		A[2][4] = new Integer(6);
-		A[0][5] = new Integer(7);
-		A[1][5] = new Integer(8);
-		A[2][5] = new Integer(10);
-		String[] header = new String[3];
-		header[0] = "asdf";
-		header[1] = "qwer";
-		header[2] = "zxcv";
-		if (TV == null) {
-			TV = new TableViewer(header, A);
-			add(TV);
-		} else {
-			TV = new TableViewer(header, A);
-		}
-*/
 		//hold the place for the table, because we need to add export after it.
 		if (TV == null) {
 			TV = new TableViewer();
@@ -170,111 +126,15 @@ public class TabularDataViewer extends JPanel implements
 	 * @param source
 	 *            the entity that published the object.
 	 */
+	@SuppressWarnings("unchecked")
 	@Subscribe
 	public void receive(ProjectEvent event, Object source) {
-		DSDataSet dataSet = event.getDataSet();
-		// We will act on this object if it is a DSMicroarraySet
-		/*
-		if (dataSet instanceof DSMicroarraySet) {
-			microarraySet = (DSMicroarraySet) dataSet;
-			// We just received a new microarray set, so populate the info label
-			// with some basic stats.
-			String htmlText = "<html><body>" + "<h3>"
-					+ microarraySet.getLabel() + "</h3><br>" + "<table>"
-					+ "<tr><td>Arrays:</td><td><b>" + microarraySet.size()
-					+ "</b></td></tr>" + "<tr><td>Markers:</td><td><b>"
-					+ microarraySet.getMarkers().size() + "</b></td></tr>"
-					+ "</table>" + microarraySet.getDataSetName()
-					+ "</body></html>";
-			infoLabel.setText(htmlText);
+		DSDataSet<? extends DSBioObject> dataSet = event.getDataSet();
 
-		}
-		 */
-		// MultipleArrayData MADdata = new MultipleArrayData();
-		// MADdata.
-		/*
-		 * Experiment experiment;
-		 * 
-		 * ISlideDataElement slideDataElement; for(int i = 0; i < maxRows ;
-		 * i++){ for(int j = 0; j < maxColumns; j++){ if(!realData[i][j]){
-		 * slideDataElement = new SlideDataElement(new int[]{i+1, 1, 1}, new
-		 * int[]{j+1, 1,1}, intensities, new String[0]);
-		 * slideData.insertElementAt(slideDataElement, i*maxColumns+j); } } }
-		 */
-
-		/*
-		 * AlgorithmData data = new AlgorithmData();
-		 * 
-		 * 
-		 * //FloatMatrix FM=new
-		 * FloatMatrix(doubleArray2floatArray(microarraySet.getRow(cx)));
-		 * float[][] A=new float[3][6]; A[0][0]=1; A[1][0]=2; A[2][0]=3;
-		 * A[0][1]=3; A[1][1]=4; A[2][1]=5; A[0][2]=6; A[1][2]=7; A[2][2]=8;
-		 * A[0][3]=2; A[1][3]=3; A[2][3]=4; A[0][4]=4; A[1][4]=5; A[2][4]=6;
-		 * A[0][5]=7; A[1][5]=8; A[2][5]=10;
-		 * 
-		 * 
-		 * FloatMatrix FM=new FloatMatrix(A); int[] groupAssignments=new int[6];
-		 * groupAssignments[0]=1; groupAssignments[1]=2; groupAssignments[2]=3;
-		 * groupAssignments[3]=1; groupAssignments[4]=2; groupAssignments[5]=3;
-		 * int numGroups=3; data.addMatrix("experiment", FM);
-		 * data.addIntArray("group-assignments", groupAssignments);
-		 * data.addParam("numGroups", String.valueOf(numGroups));
-		 * 
-		 * OneWayANOVA OWA=new OneWayANOVA(); try{ AlgorithmData
-		 * result=OWA.execute(data); System.out.println(result.toString());
-		 * }catch (AlgorithmException AE){ AE.printStackTrace(); }
-		 */
-
-		/* test new type start */
-		//dataSet = event.getDataSet();
 		// We will act on this object if it is a DSMicroarraySet
 		if (dataSet instanceof DSAnovaResultSet) {
 			anovaResultSet = (DSAnovaResultSet) dataSet;
 			refreshTableViewer();
-/*			
-			System.out.println(anovaResultSet.getLabels(0));
-			int groupNum=anovaResultSet.getLabels(0).length;
-			String[] header=new String[4+groupNum*2]; 
-			header[0]="Marker Name";
-			header[1]="P-Value";
-			header[2]="Adj-P-Value";
-			header[3]="F-statistic"; 
-			for (int cx=0;cx<groupNum;cx++){
-				header[4+cx*2+0]=anovaResultSet.getLabels(0)[cx]+"_Mean";
-				header[4+cx*2+1]=anovaResultSet.getLabels(0)[cx]+"_Std";
-			}
-			
-			System.out.println(anovaResultSet.getResult2DArray().length);
-			System.out.println(anovaResultSet.getResult2DArray()[0].length);
-			Object[][] A = new Object[anovaResultSet.getSignificantMarkers().size()][header.length];
-			for (int cx = 0; cx < anovaResultSet.getSignificantMarkers().size(); cx++) {
-				A[cx][0] = anovaResultSet.getSignificantMarkers().toArray()[cx].toString();
-				A[cx][1] = anovaResultSet.getPValue((DSGeneMarker)anovaResultSet.getSignificantMarkers().toArray()[cx]);
-				A[cx][2] = anovaResultSet.getAdjPValue((DSGeneMarker)anovaResultSet.getSignificantMarkers().toArray()[cx]);
-				A[cx][3] = anovaResultSet.getFStatistic((DSGeneMarker)anovaResultSet.getSignificantMarkers().toArray()[cx]);				
-				for (int gc=0;gc<groupNum;gc++){
-					A[cx][4+gc*2+0]=anovaResultSet.getResult2DArray()[3+gc*2][cx];
-					A[cx][4+gc*2+1]=anovaResultSet.getResult2DArray()[4+gc*2][cx];
-				}
-			}
-			if (TV == null) {
-				TV = new TableViewer(header, A);
-				//TODO: according to total number of columns, software choose one of the following. 
-				//this line make the table sizable, but probably not large enough, and can not be scrolled horizontal.
-//				TV.getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-				//this line make the table large enough and horizontal scrollalbe, but non-sizable. 				
-//				TV.getTable().setPreferredSize(new Dimension(TV.getTable().getColumnCount()*100,TV.getTable().getRowHeight()*TV.getTable().getRowCount()));
-				add(TV,java.awt.BorderLayout.CENTER);
-				TV.updateUI();
-			} else {
-				remove(TV);
-				TV = new TableViewer(header, A);
-				TV.getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-				add(TV,java.awt.BorderLayout.CENTER);
-				TV.updateUI();
-			}
-*/			
 		}
 	}
 	
@@ -300,8 +160,7 @@ public class TabularDataViewer extends JPanel implements
 				header[meanStdStartAtIndex+cx*((mean?1:0)+(std?1:0))+(mean?1:0)]=anovaResultSet.getLabels(0)[cx]+"_Std";
 			}
 		}
-//		System.out.println(anovaResultSet.getResult2DArray().length);
-//		System.out.println(anovaResultSet.getResult2DArray()[0].length);
+
 		Object[][] A = new Object[anovaResultSet.getSignificantMarkers().size()][header.length];
 		ProgressBar pb=null;
 		if (useProgressBar){
@@ -338,37 +197,7 @@ public class TabularDataViewer extends JPanel implements
 			}
 			Thread.yield();
 		}
-/*
-        //straight forward version, easier to maintain (slower);
-		for (int cx = 0; cx < anovaResultSet.getSignificantMarkers().size(); cx++) {
-			fieldIndex=0;
-			//label is too long, changed to short name.
-			//A[cx][fieldIndex++] = anovaResultSet.getSignificantMarkers().toArray()[cx].toString();
-			//DSGeneMarker aMarker=(DSGeneMarker)(anovaResultSet.getSignificantMarkers().toArray()[cx]);
-			DSGeneMarker aMarker=(DSGeneMarker) anovaResultSet.getSignificantMarkers().get(cx);
-			A[cx][fieldIndex++] = aMarker.getShortName();
-			if (pVal){A[cx][fieldIndex++] = anovaResultSet.getPValue(aMarker);};
-			//if (pVal){A[cx][fieldIndex++] = anovaResultSet.getPValue(cx);};
-			if (adjPVal){A[cx][fieldIndex++] = anovaResultSet.getAdjPValue(aMarker);};
-			//if (adjPVal){A[cx][fieldIndex++] = anovaResultSet.getAdjPValue(cx);};
-			if (fStat){A[cx][fieldIndex++] = anovaResultSet.getFStatistic(aMarker);};
-			//if (fStat){A[cx][fieldIndex++] = anovaResultSet.getFStatistic(cx);};
-			for (int gc=0;gc<groupNum;gc++){
-				if (mean){
-					A[cx][meanStdStartAtIndex+gc*((mean?1:0)+(std?1:0))+0]=anovaResultSet.getResult2DArray()[3+gc*2][cx];
-					A[cx][meanStdStartAtIndex+gc*((mean?1:0)+(std?1:0))+0]=anovaResultSet.getMean(aMarker, anovaResultSet.getLabels(0)[gc]);
-				}
-				if (std){
-					A[cx][meanStdStartAtIndex+gc*((mean?1:0)+(std?1:0))+(mean?1:0)]=anovaResultSet.getResult2DArray()[4+gc*2][cx];
-					A[cx][meanStdStartAtIndex+gc*((mean?1:0)+(std?1:0))+(mean?1:0)]=anovaResultSet.getDeviation(aMarker, anovaResultSet.getLabels(0)[gc]);
-				}
-			}
-//			System.out.println(cx+"/"+anovaResultSet.getSignificantMarkers().size()+"completed.");
-			pb.setMessage(cx+"/"+anovaResultSet.getSignificantMarkers().size()+" finished...");
-			pb.updateTo(cx*100/anovaResultSet.getSignificantMarkers().size());
-			Thread.yield();
-		}
-*/		
+
 		if (TV == null) {
 			TV = new TableViewer(header, A);
 			//TODO: according to total number of columns, software choose one of the following. 
@@ -406,7 +235,6 @@ public class TabularDataViewer extends JPanel implements
         
 		int returnVal = jFC.showSaveDialog(this);
 	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-//	    	System.out.println("You chose to open this file: " +jFC.getCurrentDirectory()+ "\\"+jFC.getSelectedFile().getName());
 		    try {
 				String tabFilename; 
 				tabFilename = jFC.getSelectedFile().getAbsolutePath();
@@ -422,7 +250,7 @@ public class TabularDataViewer extends JPanel implements
 	    }
 	}
 	
-	public String toCVS(){
+	private String toCVS(){
 		String answer = "";
 		
         boolean newLine=true;
@@ -579,7 +407,7 @@ public class TabularDataViewer extends JPanel implements
         }
     }
 
-    public class BooleanBean extends Model {
+    private class BooleanBean extends Model {
         /**
 		 * 
 		 */
