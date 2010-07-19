94d8a8a6eaa580b7013ede6a209b0e816c8d24ba
Zhou Ji <zji@c2b2.columbia.edu>
2010-07-19 15:59:46 +0000

    minor cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6878 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/evidenceintegration/src/org/geworkbench/components/ei/EIProgress.java
+++ components/evidenceintegration/src/org/geworkbench/components/ei/EIProgress.java
@@ -1,20 +1,22 @@
 package org.geworkbench.components.ei;
 
-import org.geworkbench.util.threading.SwingWorker;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JProgressBar;
 
 /**
  * @author mhall
  */
 public class EIProgress extends JDialog {
 
-    Thread aracneThread;
+    private Thread aracneThread;
     private JButton cancelButton = new JButton("Cancel");
 
     public EIProgress(Thread thread) throws HeadlessException {
