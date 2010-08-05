62e515da1cd1dee5519352ca23a9e3af1aab15b2
Zhou Ji <zji@c2b2.columbia.edu>
2010-08-05 19:00:41 +0000

    remove the image shapshot code that was not wired up properly (thus not working)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6952 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/sequenceretriever/src/org/geworkbench/components/sequenceretriever/SingleSequenceViewPanel.java
+++ components/sequenceretriever/src/org/geworkbench/components/sequenceretriever/SingleSequenceViewPanel.java
@@ -1,14 +1,5 @@
 package org.geworkbench.components.sequenceretriever;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-
-import javax.swing.ImageIcon;
-import javax.swing.JMenuItem;
-
 import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
 import org.geworkbench.util.sequences.SequenceViewWidgetPanel;
@@ -21,33 +12,9 @@ import org.geworkbench.util.sequences.SequenceViewWidgetPanel;
 public final class SingleSequenceViewPanel extends SequenceViewWidgetPanel {
 	private static final long serialVersionUID = 7100195393198518647L;
 
-	private JMenuItem imageSnapshotItem = new JMenuItem("Image Snapshot");
-
-	public SingleSequenceViewPanel() {
-			imageSnapshotItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					createImageSnapshot();
-				}
-
-			});
-	}
-
 	public void setRetrievedSequencesPanel(
 			RetrievedSequencesPanel retrievedSequencesPanel) {
 		// no effect
-	}
-
-	private org.geworkbench.events.ImageSnapshotEvent createImageSnapshot() {
-		Dimension panelSize = this.getSize();
-		BufferedImage image = new BufferedImage(panelSize.width,
-				panelSize.height, BufferedImage.TYPE_INT_RGB);
-		Graphics g = image.getGraphics();
-		this.paint(g);
-		ImageIcon icon = new ImageIcon(image, "Promoter Snapshot");
-		org.geworkbench.events.ImageSnapshotEvent event = new org.geworkbench.events.ImageSnapshotEvent(
-				"Promoter Snapshot", icon,
-				org.geworkbench.events.ImageSnapshotEvent.Action.SAVE);
-		return event;
 	}
 
 	public void setSingleSequenceView(boolean singleSequenceView) {
