e8ab9ce4c8212d1c36bceebbe72ba454c218f66f
Min You <my2248@c2b2.columbia.edu>
2010-11-29 22:13:28 +0000

    add something for testing
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7252 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/cytoscape/src/org/geworkbench/components/cytoscape/CytoscapeWidget.java
+++ components/cytoscape/src/org/geworkbench/components/cytoscape/CytoscapeWidget.java
@@ -856,7 +856,10 @@ public class CytoscapeWidget implements VisualPlugin {
 	}
 
 	private void init() {
-
+		
+		/*if (System.getProperty("os.name").startsWith("Mac")) {
+			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Cytoscape");
+		}*/
 		Cytoscape.getDesktop().setVisible(false);
 		CytoscapeInit initializer = new CytoscapeInit();
 
