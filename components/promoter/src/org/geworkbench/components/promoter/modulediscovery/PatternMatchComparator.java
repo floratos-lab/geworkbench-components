200b12ab4fe8c090845eb3b705bce67fb6ad521d
Zhou Ji <zji@c2b2.columbia.edu>
2010-12-16 22:47:42 +0000

    cleaning-up. see mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7343 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/promoter/src/org/geworkbench/components/promoter/modulediscovery/PatternMatchComparator.java
+++ components/promoter/src/org/geworkbench/components/promoter/modulediscovery/PatternMatchComparator.java
@@ -1,5 +1,6 @@
 package org.geworkbench.components.promoter.modulediscovery;
 
+import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
 import org.geworkbench.bison.datastructure.complex.pattern.DSPatternMatch;
 import org.geworkbench.bison.datastructure.complex.pattern.sequence.CSSeqRegistration;
 
@@ -7,47 +8,55 @@ import java.util.Comparator;
 
 /**
  * Note: this comparator imposes orderings that are inconsistent with equals."
- * <p>Title: Bioworks</p>
- * <p>Description: Modular Application Framework for Gene Expession, Sequence and Genotype Analysis</p>
- * <p>Copyright: Copyright (c) 2003 -2004</p>
- * <p>Company: Columbia University</p>
- *
+ * <p>
+ * Title: Bioworks
+ * </p>
+ * <p>
+ * Description: Modular Application Framework for Gene Expession, Sequence and
+ * Genotype Analysis
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2003 -2004
+ * </p>
+ * <p>
+ * Company: Columbia University
+ * </p>
+ * 
  * @author not attributable
- * @version 1.0
+ * @version $Id$
  */
-public class PatternMatchComparator implements Comparator {
+public class PatternMatchComparator implements
+		Comparator<DSPatternMatch<DSSequence, CSSeqRegistration>> {
 
+	/**
+	 * equals
+	 * 
+	 * @param obj
+	 *            Object
+	 * @return boolean
+	 */
+	public boolean equals(Object obj) {
+		return false;
+	}
 
-    /**
-     * equals
-     *
-     * @param obj Object
-     * @return boolean
-     */
-    public boolean equals(Object obj) {
-        return false;
-    }
+	/**
+	 * compare
+	 * 
+	 * @param o1
+	 *            Object
+	 * @param o2
+	 *            Object
+	 * @return int
+	 */
+	public int compare(DSPatternMatch<DSSequence, CSSeqRegistration> m1,
+			DSPatternMatch<DSSequence, CSSeqRegistration> m2) {
+		CSSeqRegistration reg1 = (CSSeqRegistration) m1.getRegistration();
+		CSSeqRegistration reg2 = (CSSeqRegistration) m2.getRegistration();
 
-    /**
-     * compare
-     *
-     * @param o1 Object
-     * @param o2 Object
-     * @return int
-     */
-    public int compare(Object o1, Object o2) {
-//        DSSeqPatternMatch m1 = (DSSeqPatternMatch) o1;
-//        DSSeqPatternMatch m2 = (DSSeqPatternMatch) o2;
-//        CSSeqRegistration reg1 = m1.getRegistration();
-//        CSSeqRegistration reg2 = m2.getRegistration();
-        DSPatternMatch m1 = (DSPatternMatch) o1;
-        DSPatternMatch m2 = (DSPatternMatch) o2;
-        CSSeqRegistration reg1 =(CSSeqRegistration) m1.getRegistration();
-        CSSeqRegistration reg2 =(CSSeqRegistration) m2.getRegistration();
-
-        if (reg1.x1 > reg2.x1)
-            return 1;
-        else if (reg1.x1 < reg2.x1) return -1;
-        return 0;
-    }
+		if (reg1.x1 > reg2.x1)
+			return 1;
+		else if (reg1.x1 < reg2.x1)
+			return -1;
+		return 0;
+	}
 }
