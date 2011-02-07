26d5abb382ea59baa211865cd98feb0c7a37f2e7
Zhou Ji <zji@c2b2.columbia.edu>
2011-02-07 22:54:45 +0000

    make this class public so to be consistent with other components and to avoid problem using this outside geWorkbench classloading
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7418 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/hierarchicalclustering/src/org/geworkbench/components/hierarchicalclustering/FastHierClustAnalysis.java
+++ components/hierarchicalclustering/src/org/geworkbench/components/hierarchicalclustering/FastHierClustAnalysis.java
@@ -37,10 +37,10 @@ import org.geworkbench.util.SpearmanRankDistance;
 /**
  * 
  * @author unattributable
- * @version $Id: FastHierClustAnalysis.java,v 1.4 2009-09-10 16:40:26 chiangy Exp $
+ * @version $Id$
  * 
  */
-class FastHierClustAnalysis extends AbstractGridAnalysis implements
+public class FastHierClustAnalysis extends AbstractGridAnalysis implements
 		ClusteringAnalysis {
 
 	private static final long serialVersionUID = 4486758109656693283L;
