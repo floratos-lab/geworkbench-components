7c34ea276c44b82d3cada6a7e5cb23b1a46e0def
Zheng Ma <maz@columbia.edu>
2011-02-28 21:49:26 +0000

    bug 2431: fold change analysis
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7506 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/foldchange/src/org/geworkbench/components/foldchange/FoldChangeAnalysis.java
@@ -0,0 +1,367 @@
+package org.geworkbench.components.foldchange;
+
+import java.util.HashSet;
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.analysis.AbstractAnalysis;
+import org.geworkbench.bison.annotation.CSAnnotationContext;
+import org.geworkbench.bison.annotation.CSAnnotationContextManager;
+import org.geworkbench.bison.annotation.DSAnnotationContext;
+import org.geworkbench.bison.annotation.DSAnnotationContextManager;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
+import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
+import org.geworkbench.bison.datastructure.bioobjects.FoldChangeResult;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+import org.geworkbench.bison.datastructure.complex.panels.CSAnnotPanel;
+import org.geworkbench.bison.datastructure.complex.panels.DSAnnotatedPanel;
+import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
+import org.geworkbench.bison.model.analysis.AlgorithmExecutionResults;
+import org.geworkbench.bison.model.analysis.ClusteringAnalysis;
+import org.geworkbench.builtin.projects.ProjectPanel;
+import org.geworkbench.engine.management.Publish;
+import org.geworkbench.events.SubpanelChangedEvent;
+import org.geworkbench.util.ProgressBar;
+
+/**
+ *
+ * @author zm2165
+ * @version $Id$
+ */
+
+public class FoldChangeAnalysis extends AbstractAnalysis implements
+		ClusteringAnalysis {	
+	
+	private static final long serialVersionUID = -8885144423769088617L;
+	private static final int GROUP_A = 1;
+	private static final int GROUP_B = 2;
+	private static final int NEITHER_GROUP = 3;
+	private static final double LOG2=Math.log(2);
+	private static final double SMALLDOUBLE= 1E-99;
+	
+	private double[][] expMatrix;
+
+	private int localAnalysisType;
+	private static Log log = LogFactory.getLog(FoldChangeAnalysis.class);	
+
+	private int numGenes,numExps;
+	private double alpha;
+	private boolean isLinear, isRatio;
+	private int[] groupAssignments;	
+
+	private int numberGroupA = 0;
+	private int numberGroupB = 0;	
+	
+	public FoldChangeAnalysis() {
+		localAnalysisType = AbstractAnalysis.FOLD_CHANGE_TYPE;
+		setDefaultPanel(new FoldChangePanel());
+	}
+	
+	
+
+	public int getAnalysisType() {
+		return localAnalysisType;
+	}
+
+	@Override
+	public AlgorithmExecutionResults execute(Object input) {
+		return calculate(input, false);
+	}
+
+	@SuppressWarnings("unchecked")
+	AlgorithmExecutionResults calculate(Object input,
+			boolean calledFromOtherComponent) {
+		
+		if (input == null || !(input instanceof DSMicroarraySetView)) {
+			return new AlgorithmExecutionResults(false, "Invalid input.", null);
+		}
+		
+
+		DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray> data = (DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray>) input;
+		boolean allArrays = !data.useItemPanel();
+		log.info("All arrays: " + allArrays);
+
+		numGenes = data.markers().size();
+		numExps = data.items().size();
+
+		ProgressBar pbFCtest = ProgressBar
+				.create(ProgressBar.INDETERMINATE_TYPE);
+		pbFCtest.addObserver(this);
+		pbFCtest.setTitle("Fold Change Analysis");
+		pbFCtest.setBounds(new ProgressBar.IncrementModel(0, numGenes, 0,
+				numGenes, 1));
+
+		pbFCtest.setMessage("Calculating Fold Change, please wait...");
+		pbFCtest.start();
+		this.stopAlgorithm = false;
+
+		groupAssignments = new int[numExps];
+
+		DSDataSet<? extends DSBioObject> set = data.getDataSet();
+
+		if (!(set instanceof DSMicroarraySet)) {
+			pbFCtest.dispose();
+			return null;
+		}
+
+		DSMicroarraySet<DSMicroarray> maSet = (DSMicroarraySet<DSMicroarray>) set;
+		DSAnnotationContextManager manager = CSAnnotationContextManager
+				.getInstance();
+		DSAnnotationContext<DSMicroarray> context = manager
+				.getCurrentContext(maSet);
+
+		numberGroupA = 0;
+		numberGroupB = 0;
+		
+		for (int i = 0; i < numExps; i++) {
+			DSMicroarray ma = data.items().get(i);
+			String[] labels = context.getLabelsForItem(ma);
+			if ((labels.length == 0) && allArrays) {
+				groupAssignments[i] = GROUP_B;
+				numberGroupB++;
+			}
+			for (String label : labels) {
+				if (context.isLabelActive(label) || allArrays) {
+					String v = context.getClassForLabel(label);
+					if (v.equals(CSAnnotationContext.CLASS_CASE)) {
+						groupAssignments[i] = GROUP_A;
+						numberGroupA++;						
+					} else if (v.equals(CSAnnotationContext.CLASS_CONTROL)) {
+						groupAssignments[i] = GROUP_B;
+						numberGroupB++;						
+					} else {
+						groupAssignments[i] = NEITHER_GROUP;
+					}
+				}
+			}
+		}
+		if (numberGroupA == 0 && numberGroupB == 0) {
+			pbFCtest.dispose();
+			return new AlgorithmExecutionResults(
+					false,
+					"Please activate at least one set of arrays for \"case\", and one set of arrays for \"control\".",
+					null);
+		}
+		if (numberGroupA == 0) {
+			pbFCtest.dispose();
+			return new AlgorithmExecutionResults(false,
+					"Please activate at least one set of arrays for \"case\".",
+					null);
+		}
+		if (numberGroupB == 0) {
+			pbFCtest.dispose();
+			return new AlgorithmExecutionResults(
+					false,
+					"Please activate at least one set of arrays for \"control\".",
+					null);
+		}		
+
+		try{
+			alpha = Double.parseDouble(((FoldChangePanel) aspp).getAlpha());
+			isLinear=((FoldChangePanel) aspp).isLinear();			
+			isRatio=((FoldChangePanel) aspp).isRatio();			
+		}
+		catch(NumberFormatException e){
+			pbFCtest.dispose();
+			return new AlgorithmExecutionResults(false,
+					"Parameters are not valid.", null);
+		}
+		
+		expMatrix = new double[numGenes][numExps];
+		
+		for (int i = 0; i < numGenes; i++) {			
+			if (this.stopAlgorithm) {
+				pbFCtest.dispose();
+				return null;
+			}
+			
+			for (int j = 0; j < numExps; j++) {
+				if((!isLinear)&&isRatio)
+					expMatrix[i][j] = Math.pow(2,(double) data.getValue(i, j));
+				else if(isLinear&&(!isRatio)){
+					if (data.getValue(i, j)<SMALLDOUBLE) {
+						pbFCtest.dispose();
+						return new AlgorithmExecutionResults(false, "Input data is invalid for the parameter settings", null);
+					}
+					expMatrix[i][j]=Math.log( data.getValue(i, j))/LOG2;
+				}
+				else
+					expMatrix[i][j]= data.getValue(i, j);
+			}
+			
+		}
+
+		
+		String[][] labels = new String[2][];
+		labels[0] = context.getLabelsForClass(CSAnnotationContext.CLASS_CASE);
+		labels[1] = context
+				.getLabelsForClass(CSAnnotationContext.CLASS_CONTROL);
+		HashSet<String> caseSet = new HashSet<String>();
+		HashSet<String> controlSet = new HashSet<String>();
+
+		String groupAndChipsString = "";
+
+		// case
+		String[] classLabels = labels[0];
+		groupAndChipsString += "\t case group(s): \n";
+		for (int i = 0; i < classLabels.length; i++) {
+			String label = classLabels[i];
+			if (context.isLabelActive(label) || !data.useItemPanel()) {
+				caseSet.add(label);
+				groupAndChipsString += GenerateGroupAndChipsString(context
+						.getItemsWithLabel(label));
+			}
+		}
+
+		// control
+		classLabels = labels[1];
+		groupAndChipsString += "\t control group(s): \n";
+		for (int i = 0; i < classLabels.length; i++) {
+			String label = classLabels[i];
+			if (context.isLabelActive(label) || !data.useItemPanel()) {
+				controlSet.add(label);
+				groupAndChipsString += GenerateGroupAndChipsString(context
+						.getItemsWithLabel(label));
+			}
+		}
+
+		int totalSelectedGroup = caseSet.size() + controlSet.size();
+		String histHeader = null;
+		String histMarkerString = GenerateMarkerString(data);
+
+		groupAndChipsString = totalSelectedGroup + " groups analyzed:\n"
+				+ groupAndChipsString;	
+
+		Vector<Integer> clusterVector = sortGenesBySignificance(pbFCtest);			
+		
+		DSAnnotatedPanel<DSGeneMarker, Float> panelSignificant = new CSAnnotPanel<DSGeneMarker, Float>(
+				"Fold_change_"+alpha);
+		
+		for (Integer index : clusterVector) {
+			DSGeneMarker item = data.markers().get(index);
+			panelSignificant.add(item, new Float(index));			
+		}
+		
+		publishSubpanelChangedEvent(new SubpanelChangedEvent<DSGeneMarker>(
+				DSGeneMarker.class, panelSignificant,
+				SubpanelChangedEvent.NEW));			
+	
+		FoldChangeResult analysisResult = new FoldChangeResult(maSet,"Fold_change_"+alpha);
+		AlgorithmExecutionResults results = new AlgorithmExecutionResults(true,
+				"Fold Change Analysis", analysisResult);
+		
+		// add data set history.
+		histHeader = GenerateHistoryHeader();
+		String stemp=histHeader + groupAndChipsString + histMarkerString;
+		ProjectPanel.addToHistory(analysisResult, stemp );
+		
+		pbFCtest.dispose();
+		
+		return results;
+		
+	} // end of method calculate
+
+	@Publish
+	public org.geworkbench.events.SubpanelChangedEvent<DSGeneMarker> publishSubpanelChangedEvent(
+			org.geworkbench.events.SubpanelChangedEvent<DSGeneMarker> event) {
+		return event;
+	}
+
+	protected String GenerateHistoryHeader() {
+
+		String histStr = "";
+		// Header
+		histStr += "Fold Change Analysis with the following parameters:\n";
+		histStr += "----------------------------------------------------------\n";
+
+		histStr += "Input Parameters:" + "\n";			
+		histStr += "\t" + "Criterion: " + alpha + "\n";
+		histStr += "\t" + "Input data format: ";
+		if (isLinear)
+			histStr +="Linear\n";
+		else
+			histStr +="Log2-transformed\n";
+		histStr += "\t" + "Caculation method: ";
+		if (isRatio)
+			histStr +="Ratio" +"\n";
+		else
+			histStr +="Difference" +"\n";
+		
+		return histStr;
+	}
+
+	private String GenerateGroupAndChipsString(DSPanel<DSMicroarray> panel) {
+		String histStr = null;
+
+		histStr = "\t     " + panel.getLabel() + " (" + panel.size()
+				+ " chips)" + ":\n";
+
+		int aSize = panel.size();
+		for (int aIndex = 0; aIndex < aSize; aIndex++)
+			histStr += "\t\t" + panel.get(aIndex) + "\n";
+
+		return histStr;
+	}
+
+	String GenerateMarkerString(
+			DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray> view) {
+		String histStr = null;
+
+		histStr = view.markers().size() + " markers analyzed:\n";
+		for (DSGeneMarker marker : view.markers()) {
+			histStr += "\t" + marker.getLabel() + "\n";
+		}
+
+		return histStr;
+
+	}
+	
+	private Vector<Integer> sortGenesBySignificance(ProgressBar pbFCtest) {
+
+		Vector<Integer> sigGenes = new Vector<Integer>();
+		for (int i = 0; i < numGenes; i++) {
+			if (this.stopAlgorithm) {
+				pbFCtest.dispose();
+				return null;
+			}
+
+			if(isSignificant(i))
+				sigGenes.add(new Integer(i));		
+		}
+		return sigGenes;
+	
+	}
+	
+	private boolean isSignificant(int gene) {
+		boolean sig = false;
+		double numbValidValuesA = 0;
+		double numbValidValuesB = 0;
+		
+		for(int i=0;i<numExps;i++){
+			int g=groupAssignments[i];
+			if(g==GROUP_A)
+				numbValidValuesA+=expMatrix[gene][i];
+			else if (g==GROUP_B)
+				numbValidValuesB+=expMatrix[gene][i];
+		}
+		
+		numbValidValuesA/=numberGroupA;	//Average
+		numbValidValuesB/=numberGroupB;
+		
+		
+		 double fcValue=0;
+		if (isRatio&&(numbValidValuesB>SMALLDOUBLE))
+			fcValue=numbValidValuesA/numbValidValuesB;
+		else if(!isRatio)
+			fcValue=numbValidValuesA-numbValidValuesB;
+		
+		if (fcValue>alpha) sig=true;
+		
+		return sig;		
+	}	
+
+}
---
+++ components/foldchange/src/org/geworkbench/components/foldchange/FoldChangePanel.java
@@ -0,0 +1,159 @@
+package org.geworkbench.components.foldchange;
+
+import java.awt.BorderLayout;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import org.geworkbench.analysis.AbstractSaveableParameterPanel;
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+/**
+ * 
+ * @author zm2165 
+ * @version $Id$
+ */
+
+public class FoldChangePanel extends AbstractSaveableParameterPanel {
+	
+	private static final long serialVersionUID = -6317805205581289163L;
+	
+	private JPanel selectionPanel = null;
+    private JTextField alpha = new JTextField(5);
+    private static final double ALPHA_DEFAULT = 2.0f;
+    
+    private ButtonGroup group1 = new ButtonGroup();
+    private ButtonGroup group2 = new ButtonGroup();
+    private JRadioButton linearBtn=new JRadioButton();
+    private JRadioButton logBtn=new JRadioButton();
+    private JRadioButton ratioBtn=new JRadioButton();
+    private JRadioButton diffBtn=new JRadioButton();
+    
+	/*
+	 * (non-Javadoc)
+	 * @see org.geworkbench.analysis.AbstractSaveableParameterPanel#setParameters(java.util.Map)
+	 * Set inputed parameters to GUI.
+	 */
+    public void setParameters(Map<Serializable, Serializable> parameters){
+        Set<Map.Entry<Serializable, Serializable>> set = parameters.entrySet();
+        for (Iterator<Map.Entry<Serializable, Serializable>> iterator = set.iterator(); iterator.hasNext();) {
+        	Map.Entry<Serializable, Serializable> parameter = iterator.next();
+			Object key = parameter.getKey();
+			Object value = parameter.getValue();
+
+			if (key.equals("alpha")){
+				alpha.setText((String)value);
+			}
+			if (key.equals("linearBtn")){
+				linearBtn.setSelected((Boolean)value);
+			}
+			if (key.equals("logBtn")){
+				logBtn.setSelected((Boolean)value);
+			}
+			if (key.equals("ratioBtn")){
+				ratioBtn.setSelected((Boolean)value);
+			}
+			if (key.equals("diffBtn")){
+				diffBtn.setSelected((Boolean)value);
+			}
+			
+		}
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geworkbench.analysis.AbstractSaveableParameterPanel#getParameters()
+	 */
+    public Map<Serializable, Serializable> getParameters() {
+		Map<Serializable, Serializable> parameters = new HashMap<Serializable, Serializable>();		
+		parameters.put("alpha", alpha.getText());
+		parameters.put("linearBtn", linearBtn.isSelected());
+		parameters.put("logBtn", logBtn.isSelected());
+		parameters.put("ratioBtn", ratioBtn.isSelected());
+		parameters.put("diffBtn", diffBtn.isSelected());
+		
+		return parameters;
+	}
+
+    
+    public FoldChangePanel() {
+    	linearBtn.setText("Linear");    	
+    	logBtn.setText("Log2-transformed");    	
+    	group1.add(linearBtn);
+    	group1.add(logBtn);
+    	logBtn.setSelected(false);
+    	linearBtn.setSelected(true);
+    	ratioBtn.setText("Ratio");    	
+    	diffBtn.setText("Difference");    	
+    	group2.add(ratioBtn);
+    	group2.add(diffBtn);
+    	diffBtn.setSelected(false);
+    	ratioBtn.setSelected(true);
+    	
+    	this.setLayout(new BorderLayout());		
+		selectionPanel = new JPanel();
+		selectionPanel.setLayout(new BorderLayout());
+		this.add(selectionPanel, BorderLayout.CENTER);	 
+		
+		FormLayout layout = new FormLayout(
+				"right:max(10dlu;pref), 3dlu, pref, 7dlu, "
+						+ "left:max(10dlu;pref), 3dlu, pref, 7dlu, "
+						+ "right:max(10dlu;pref), 3dlu, pref, 7dlu ", "");
+		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+		builder.setDefaultDialogBorder();
+
+		builder.appendSeparator("Fold Change Parameters");	
+		builder.append("Criterion");   	
+		alpha.setText(Double.toString(ALPHA_DEFAULT));
+		alpha.setToolTipText("The value is given in linear measure, regardless of the state of the data.");
+		builder.append(alpha);
+		builder.nextLine();
+		builder.append(new JLabel("Input Data Format: "));
+		builder.append(linearBtn);
+		builder.append(logBtn);
+		builder.nextLine();
+		builder.append(new JLabel("Calculation Method: "));
+		builder.append(ratioBtn);
+		builder.append(diffBtn);
+       
+		selectionPanel.add(builder.getPanel(), BorderLayout.CENTER);
+    }
+
+    public String getAlpha() {
+       return alpha.getText();
+    }
+    public boolean isLinear() {
+        return linearBtn.isSelected();
+    }
+    public boolean isLog() {
+        return logBtn.isSelected();
+    }
+    public boolean isRatio() {
+        return ratioBtn.isSelected();
+    }
+    public boolean isDiff() {
+        return diffBtn.isSelected();
+    }
+   
+   
+	@Override
+	public void fillDefaultValues(Map<Serializable, Serializable> parameters) {
+		// TODO Auto-generated method stub
+		//parameters.put("linearBtn", true);		
+		//parameters.put("ratioBtn", ratioBtn.isSelected());		
+		/*
+		alpha.setText(Double.toString(ALPHA_DEFAULT));
+		linearBtn.setSelected(true);
+		ratioBtn.setSelected(true);
+		*/
+	}
+}
