2e7b7838e61ed2874b545e654640ce877cc654c2
Min You <my2248@c2b2.columbia.edu>
2011-03-09 18:48:26 +0000

    fix for properties
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7553 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/interactions/src/org/geworkbench/components/interactions/cellularnetwork/Constants.java
+++ components/interactions/src/org/geworkbench/components/interactions/cellularnetwork/Constants.java
@@ -46,6 +46,8 @@ public class Constants  {
 	public static final String CNKB_SELECTION = "cnkb selection";
 
 	public static final String CNKB_SELECTION_INDEX = "CNKB_SELECTION_INDEX";
+	
+	public static final String CNKB_THROTTLE_GRAPH_PROPERTIES = "CNKB_THROTTLE_GRAPH_PROPERTIES";
 
 	public static final String SELECTCONTEXT = "Select Context";
 
