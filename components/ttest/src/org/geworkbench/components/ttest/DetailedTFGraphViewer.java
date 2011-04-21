81a7d46e042e1563acc3a87e84fe06bdb6ae9df1
Min You <my2248@c2b2.columbia.edu>
2011-04-21 21:01:04 +0000

    change for t-value position
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7794 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/ttest/src/org/geworkbench/components/ttest/DetailedTFGraphViewer.java
+++ components/ttest/src/org/geworkbench/components/ttest/DetailedTFGraphViewer.java
@@ -112,6 +112,8 @@ public class DetailedTFGraphViewer extends JPanel {
 			
 
 			// draw the lines for geneMarkers
+			
+			double maxAbsValue = Math.max(Math.abs(minTValue), Math.abs(maxTValue));
 			for (int i = 0; i < numMarkers; i++) {			 		 
 				DSItemList<DSGeneMarker> genesInRegulonList = mraResultSet.getGenesInRegulon(tfA);					 
 				if (genesInRegulonList == null) { // if user selected wrong
@@ -141,9 +143,9 @@ public class DetailedTFGraphViewer extends JPanel {
 							Color save = g.getColor();
 							int center = 0;
 							if (tValue >= 0)
-								center = (int) (width / 2 + width * Math.abs(tValue) / Math.max(Math.abs(minTValue), Math.abs(maxTValue)));	
+								center = (int) (width / 2 + width * Math.abs(tValue) / maxAbsValue);	
 							else
-								center = (int) (width / 2 - width * Math.abs(tValue) / Math.max(Math.abs(minTValue), Math.abs(maxTValue)));
+								center = (int) (width / 2 - width * Math.abs(tValue) / maxAbsValue);
 							
 							if (SR.getR() >= 0) {								 
 								g.setColor(Color.BLACK);
