3fc7c4a682a6bb967f27cdd2d762b078edcc212a
Meng Wang <mw2518@columbia.edu>
2011-06-01 17:54:03 +0000

    #2525: warn user if step is not a positive integer
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7949 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/poshistogram/src/org/geworkbench/components/poshistogram/PositionHistogramWidget.java
+++ components/poshistogram/src/org/geworkbench/components/poshistogram/PositionHistogramWidget.java
@@ -13,6 +13,7 @@ import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
@@ -132,7 +133,17 @@ public final class PositionHistogramWidget extends JPanel {
 
     private void plotAction(ActionEvent e) {
         int maxLen = getMaxLength();
-        int step = Integer.parseInt(jStepBox.getText());
+        int step = 0;
+		try {
+			step = Integer.parseInt(jStepBox.getText());
+		} catch (NumberFormatException nfe) {
+			JOptionPane.showMessageDialog(null, "Step should be a positive integer");
+			return;
+		}
+		if (step <= 0) {
+			JOptionPane.showMessageDialog(null, "Step should be a positive integer");
+			return;
+		}	
         int wind = 1;
         int maxBin = maxLen / step + 1;
 
