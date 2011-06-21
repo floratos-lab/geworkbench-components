34aa779c8d7f10cdc2c95a56e30cbabaa5fb9bf3
Meng Wang <mw2518@columbia.edu>
2011-06-21 21:46:13 +0000

    #2622: added email and title to markus job submission fields
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8013 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/markus/src/org/geworkbench/components/markus/MarkUsConfigPanel.java
+++ components/markus/src/org/geworkbench/components/markus/MarkUsConfigPanel.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Serializable;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -95,6 +97,8 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 	private JFormattedTextField priorTf = new JFormattedTextField();
 	private JFormattedTextField keyTf = new JFormattedTextField();
 	private static final Pattern pattern = Pattern.compile("^MUS\\d+");
+	private JFormattedTextField email = new JFormattedTextField();
+	private JFormattedTextField title = new JFormattedTextField();
 
 	// markus parameters 1st part - totally 9
 	public boolean getskanValue() {
@@ -139,6 +143,16 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 
 	public boolean getkeyValue() {
 		return cbkey.isSelected();
+	}
+
+	public String getEmail() {
+		String e = email.getText();
+		return escapeHtml(e);
+	}
+	
+	public String getTitle() {
+		String t = title.getText();
+		return escapeHtml(t);
 	}
 
 	// delphi part - totally 10
@@ -267,6 +281,8 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 
 		cbxChain.addActionListener(parameterActionListener);
 		cbkey.addActionListener(parameterActionListener);
+		email.addActionListener(parameterActionListener);
+		title.addActionListener(parameterActionListener);
 
 		// delphi parameters
 		gridsize.addPropertyChangeListener(parameterActionListener);
@@ -295,18 +311,23 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 	}
 
 	private JComponent buildMainPanel() {
-		JPanel jp = new JPanel(new GridLayout(2, 2));
+		JPanel jp = new JPanel(new GridLayout(1, 2));
 		jp.add(buildLeftPanel());
 		jp.add(buildRightPanel());
+		JPanel jpw = new JPanel(new BorderLayout());
+		jpw.add(jp, BorderLayout.CENTER);
 
 		FormLayout layout = new FormLayout(
-				"left:max(25dlu;pref), 10dlu, left:max(35dlu;pref), 2dlu, max(25dlu;pref), 5dlu, "
-						+ "left:max(25dlu;pref), 2dlu, max(25dlu;pref)", "");
+				"left:max(5dlu;pref), 2dlu, left:max(5dlu;pref), 12dlu, left:max(5dlu;pref), 2dlu, "
+						+ "left:max(5dlu;pref), 12dlu, left:max(5dlu;pref), 2dlu, max(90dlu;pref), " +
+								"12dlu, left:max(5dlu;pref), 2dlu, max(200dlu;pref)", "");
 		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
 		builder.append("Chain", cbxChain);
 		builder.append("Private Key", cbkey);
-		jp.add(builder.getPanel());
-		return jp;
+		builder.append("Email (optional)", email);
+		builder.append("Title (optional)", title);
+		jpw.add(builder.getPanel(), BorderLayout.SOUTH);
+		return jpw;
 	}
 
 	private JComponent buildLeftPanel() {
@@ -678,6 +699,12 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 			if (key.equals("cbkey")) {
 				cbkey.setSelected((Boolean) value);
 			}
+			if (key.equals("email")) {
+				email.setText((String) value);
+			}
+			if (key.equals("title")) {
+				title.setText((String) value);
+			}
 			if (key.equals("gridsize")) {
 				gridsize.setValue((Integer) value);
 			}
@@ -827,4 +854,66 @@ public class MarkUsConfigPanel extends AbstractSaveableParameterPanel {
 		// TODO Auto-generated method stub
 		
 	}
+	
+    /**
+     * Escape control characters so that they will not be executed by the browser. 
+     * Replace the control characters with their escaped equivalents.
+     * @param aText
+     * @return escaped string
+     */
+    public static String escapeHtml(String aText) {
+		final StringBuilder result = new StringBuilder();
+		final StringCharacterIterator iterator = new StringCharacterIterator(aText);
+		char character = iterator.current();
+		while (character != CharacterIterator.DONE) {
+			if (character == ' ')       result.append("%20");
+			else if (character == '<')  result.append("&lt;");
+			else if (character == '>')  result.append("&gt;");
+			else if (character == '&')  result.append("&amp;");
+			else if (character == '\"') result.append("&quot;");
+			else if (character == '\t') addCharEntity(9, result);
+			else if (character == '!')  addCharEntity(33, result);
+			else if (character == '#')  addCharEntity(35, result);
+			else if (character == '$')  addCharEntity(36, result);
+			else if (character == '%')  addCharEntity(37, result);
+			else if (character == '\'') addCharEntity(39, result);
+			else if (character == '(')  addCharEntity(40, result);
+			else if (character == ')')  addCharEntity(41, result);
+			else if (character == '*')  addCharEntity(42, result);
+			else if (character == '+')  addCharEntity(43, result);
+			else if (character == ',')  addCharEntity(44, result);
+			else if (character == '-')  addCharEntity(45, result);
+			else if (character == '.')  addCharEntity(46, result);
+			else if (character == '/')  addCharEntity(47, result);
+			else if (character == ':')  addCharEntity(58, result);
+			else if (character == ';')  addCharEntity(59, result);
+			else if (character == '=')  addCharEntity(61, result);
+			else if (character == '?')  addCharEntity(63, result);
+			else if (character == '@')  addCharEntity(64, result);
+			else if (character == '[')  addCharEntity(91, result);
+			else if (character == '\\') addCharEntity(92, result);
+			else if (character == ']')  addCharEntity(93, result);
+			else if (character == '^')  addCharEntity(94, result);
+			else if (character == '_')  addCharEntity(95, result);
+			else if (character == '`')  addCharEntity(96, result);
+			else if (character == '{')  addCharEntity(123, result);
+			else if (character == '|')  addCharEntity(124, result);
+			else if (character == '}')  addCharEntity(125, result);
+			else if (character == '~')  addCharEntity(126, result);
+			//the char is not a special one, add it to the result as is
+			else   result.append(character);
+			character = iterator.next();
+		}
+		return result.toString();
+	}
+
+	private static void addCharEntity(Integer aIdx, StringBuilder aBuilder) {
+		String padding = "";
+		if (aIdx <= 9)  padding = "00";
+		else if (aIdx <= 99) padding = "0";
+
+		String number = padding + aIdx.toString();
+		aBuilder.append("&#" + number + ";");
+	}
+
 }
