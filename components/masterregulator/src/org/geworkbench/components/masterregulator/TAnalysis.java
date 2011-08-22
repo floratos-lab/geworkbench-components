9b2de6c84751798d4ce5dfee60d3322c50bb9de9
Zhou Ji <zji@c2b2.columbia.edu>
2011-08-22 19:02:57 +0000

    code clean-up, first step. see bug 2542 note 10082
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8233 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/masterregulator/src/org/geworkbench/components/masterregulator/TAnalysis.java
+++ components/masterregulator/src/org/geworkbench/components/masterregulator/TAnalysis.java
@@ -1,7 +1,6 @@
 package org.geworkbench.components.masterregulator;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -10,30 +9,31 @@ import org.geworkbench.bison.annotation.CSAnnotationContext;
 import org.geworkbench.bison.annotation.CSAnnotationContextManager;
 import org.geworkbench.bison.annotation.DSAnnotationContext;
 import org.geworkbench.bison.annotation.DSAnnotationContextManager;
-import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
-import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
-import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
 
+/**
+ * This class supports the calculation of T-value for master regulation analysis.
+ * It is trimmed based on the code of t-test component to the part that is relevant here.
+ * @author zji
+ *
+ */
 public class TAnalysis {
 	
-	private static final int GROUP_A = 1;
-	private static final int GROUP_B = 2;
-	private static final int NEITHER_GROUP = 3;
+	private static enum Group {CASE, CONTROL, NEITHER};
 
 	private static Log log = LogFactory.getLog(TAnalysis.class);
 
-	private float[][] expMatrix;
+	final private float[][] expMatrix;
 
-	private int numGenes, numExps;
+	final private int numGenes, numExps;
 
-	private int[] groupAssignments;
+	final private Group[] groupAssignments;
 
-	private int numberGroupA = 0;
-	private int numberGroupB = 0;
+	private int numberOfCase = 0;
+	private int numberOfControl = 0;
 
 	static class TAnalysisException extends Exception {
 
@@ -45,61 +45,55 @@ public class TAnalysis {
 		
 	};
 	
-	@SuppressWarnings("unchecked")
-	Map<DSGeneMarker, Double> calculate(Object input) throws TAnalysisException {
-
-		DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray> data = (DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray>) input;
-		boolean allArrays = !data.useItemPanel();
+	private final DSMicroarraySetView<DSGeneMarker, DSMicroarray> datasetView;
+	TAnalysis(final DSMicroarraySetView<DSGeneMarker, DSMicroarray> datasetView) throws TAnalysisException {
+		this.datasetView = datasetView;
+		
+		boolean allArrays = !datasetView.useItemPanel();
 		log.info("All arrays: " + allArrays);
 
-		numGenes = data.markers().size();
-		numExps = data.items().size();
+		numGenes = datasetView.markers().size();
+		numExps = datasetView.items().size();
 
-		groupAssignments = new int[numExps];
+		groupAssignments = new Group[numExps];
 
-		DSDataSet<? extends DSBioObject> set = data.getDataSet();
-
-		if (!(set instanceof DSMicroarraySet)) {
-			return null;
-		}
-
-		DSMicroarraySet<DSMicroarray> maSet = (DSMicroarraySet<DSMicroarray>) set;
+		DSMicroarraySet<DSMicroarray> maSet = (DSMicroarraySet<DSMicroarray>) datasetView.getDataSet();
 		DSAnnotationContextManager manager = CSAnnotationContextManager
 				.getInstance();
 		DSAnnotationContext<DSMicroarray> context = manager
 				.getCurrentContext(maSet);
 
-		numberGroupA = 0;
-		numberGroupB = 0;
+		numberOfCase = 0;
+		numberOfControl = 0;
 		for (int i = 0; i < numExps; i++) {
-			DSMicroarray ma = data.items().get(i);
+			DSMicroarray ma = datasetView.items().get(i);
 			String[] labels = context.getLabelsForItem(ma);
 			if ((labels.length == 0) && allArrays) {
-				groupAssignments[i] = GROUP_B;
-				numberGroupB++;
+				groupAssignments[i] = Group.CONTROL;
+				numberOfControl++;
 			}
 			for (String label : labels) {
 				if (context.isLabelActive(label) || allArrays) {
 					String v = context.getClassForLabel(label);
 					if (v.equals(CSAnnotationContext.CLASS_CASE)) {
-						groupAssignments[i] = GROUP_A;
-						numberGroupA++;
+						groupAssignments[i] = Group.CASE;
+						numberOfCase++;
 					} else if (v.equals(CSAnnotationContext.CLASS_CONTROL)) {
-						groupAssignments[i] = GROUP_B;
-						numberGroupB++;
+						groupAssignments[i] = Group.CONTROL;
+						numberOfControl++;
 					} else {
-						groupAssignments[i] = NEITHER_GROUP;
+						groupAssignments[i] = Group.NEITHER;
 					}
 				}
 			}
 		}
-		if (numberGroupA == 0 && numberGroupB == 0) {
+		if (numberOfCase == 0 && numberOfControl == 0) {
 			throw new TAnalysisException("Please activate at least one set of arrays for \"case\", and one set of arrays for \"control\".");
 		}
-		if (numberGroupA == 0) {
+		if (numberOfCase == 0) {
 			throw new TAnalysisException("Please activate at least one set of arrays for \"case\".");
 		}
-		if (numberGroupB == 0) {
+		if (numberOfControl == 0) {
 			throw new TAnalysisException("Please activate at least one set of arrays for \"control\".");
 		}
 
@@ -107,51 +101,17 @@ public class TAnalysis {
 
 		for (int i = 0; i < numGenes; i++) {
 			for (int j = 0; j < numExps; j++) {
-				expMatrix[i][j] = (float) data.getValue(i, j);
+				expMatrix[i][j] = (float) datasetView.getValue(i, j);
 			}
 		}
-
-		String[][] labels = new String[2][];
-		labels[0] = context.getLabelsForClass(CSAnnotationContext.CLASS_CASE);
-		labels[1] = context
-				.getLabelsForClass(CSAnnotationContext.CLASS_CONTROL);
-		HashSet<String> caseSet = new HashSet<String>();
-		HashSet<String> controlSet = new HashSet<String>();
-
-		String groupAndChipsString = "";
-
-		// case
-		String[] classLabels = labels[0];
-		groupAndChipsString += "\t case group(s): \n";
-		for (int i = 0; i < classLabels.length; i++) {
-			String label = classLabels[i];
-			if (context.isLabelActive(label) || !data.useItemPanel()) {
-				caseSet.add(label);
-				groupAndChipsString += GenerateGroupAndChipsString(context
-						.getItemsWithLabel(label));
-			}
-		}
-
-		// control
-		classLabels = labels[1];
-		groupAndChipsString += "\t control group(s): \n";
-		for (int i = 0; i < classLabels.length; i++) {
-			String label = classLabels[i];
-			if (context.isLabelActive(label) || !data.useItemPanel()) {
-				controlSet.add(label);
-				groupAndChipsString += GenerateGroupAndChipsString(context
-						.getItemsWithLabel(label));
-			}
-		}
-
-		int totalSelectedGroup = caseSet.size() + controlSet.size();
-
-		groupAndChipsString = totalSelectedGroup + " groups analyzed:\n"
-				+ groupAndChipsString;
+		
+	}
+	
+	Map<DSGeneMarker, Double> calculateTValues() {
 
 		Map<DSGeneMarker, Double> tValues = new HashMap<DSGeneMarker, Double>();
 		for (int i = 0; i < numGenes; i++) {
-			DSGeneMarker m = data.markers().get(i);
+			DSGeneMarker m = datasetView.markers().get(i);
 			tValues.put(m, new Double(getTValue(i)) );
 		}
 
@@ -247,11 +207,11 @@ public class TAnalysis {
 		int numbValidValuesB = 0;
 
 		for (int i = 0; i < groupAssignments.length; i++) {
-			if (groupAssignments[i] == GROUP_A) {
+			if (groupAssignments[i] == Group.CASE) {
 				if (!Float.isNaN(geneValues[i])) {
 					numbValidValuesA++;
 				}
-			} else if (groupAssignments[i] == GROUP_B) {
+			} else if (groupAssignments[i] == Group.CONTROL) {
 				if (!Float.isNaN(geneValues[i])) {
 					numbValidValuesB++;
 				}
@@ -263,52 +223,30 @@ public class TAnalysis {
 		}
 
 		getGroupValues(gene);
-		return calculateTValue(groupAValues, groupBValues);
+		return calculateTValue(groupCaseValues, groupControlValues);
 	}
 
-	private String GenerateGroupAndChipsString(DSPanel<DSMicroarray> panel) {
-		StringBuffer histStr =  new StringBuffer( "\t     " + panel.getLabel() + " (" + panel.size()
-				+ " chips)" + ":\n" );
-
-		int aSize = panel.size();
-		for (int aIndex = 0; aIndex < aSize; aIndex++)
-			histStr.append(  "\t\t" ).append( panel.get(aIndex) ).append( "\n" );
-
-		return histStr.toString();
-	}
-
-	String GenerateMarkerString(
-			DSMicroarraySetView<? extends DSGeneMarker, ? extends DSMicroarray> view) {
-		StringBuffer histStr = new StringBuffer( view.markers().size()+" markers analyzed:\n" );
-		for (DSGeneMarker marker : view.markers()) {
-			histStr.append( "\t" ).append( marker.getLabel() ).append( "\n" );
-		}
-
-		return histStr.toString();
-
-	}
-
-	private transient float[] groupAValues = null;
-	private transient float[] groupBValues = null;;
+	private transient float[] groupCaseValues = null;
+	private transient float[] groupControlValues = null;;
 	private void getGroupValues(int gene) {
 		float[] geneValues = new float[numExps];
 		for (int i = 0; i < numExps; i++) {
 			geneValues[i] = expMatrix[gene][i];
 		}
 
-		groupAValues = new float[numberGroupA];
-		groupBValues = new float[numberGroupB];
+		groupCaseValues = new float[numberOfCase];
+		groupControlValues = new float[numberOfControl];
 
-		int groupACounter = 0;
-		int groupBCounter = 0;
+		int caseCounter = 0;
+		int controlCounter = 0;
 
 		for (int i = 0; i < numExps; i++) {
-			if (groupAssignments[i] == GROUP_A) {
-				groupAValues[groupACounter] = geneValues[i];
-				groupACounter++;
-			} else if (groupAssignments[i] == GROUP_B) {
-				groupBValues[groupBCounter] = geneValues[i];
-				groupBCounter++;
+			if (groupAssignments[i] == Group.CASE) {
+				groupCaseValues[caseCounter] = geneValues[i];
+				caseCounter++;
+			} else if (groupAssignments[i] == Group.CONTROL) {
+				groupControlValues[controlCounter] = geneValues[i];
+				controlCounter++;
 			}
 		}
 	}
