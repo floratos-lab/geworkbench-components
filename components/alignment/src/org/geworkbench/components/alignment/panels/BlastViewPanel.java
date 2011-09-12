598efd1fefd1729a00c25977648c1cd78da5d87f
Zhou Ji <zji@c2b2.columbia.edu>
2011-09-12 19:24:58 +0000

    remove the code that does not function. see bug 2044 note 10165
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8269 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/alignment/src/org/geworkbench/components/alignment/panels/BlastViewPanel.java
+++ components/alignment/src/org/geworkbench/components/alignment/panels/BlastViewPanel.java
@@ -35,7 +35,6 @@ import org.geworkbench.bison.datastructure.bioobjects.sequence.CSSequence;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
 import org.geworkbench.bison.util.RandomNumberGenerator;
 import org.geworkbench.components.alignment.blast.BlastObj;
-import org.geworkbench.engine.management.Subscribe;
 import org.geworkbench.util.BrowserLauncher;
 import org.geworkbench.util.FilePathnameUtils;
 import org.geworkbench.util.JAutoList;
@@ -91,17 +90,6 @@ public class BlastViewPanel extends JPanel implements HyperlinkListener {
 
 	public void setBlastViewComponent(BlastViewComponent bc) {
 		blastViewComponent = bc;
-	}
-
-	@Subscribe
-	public void receive(org.geworkbench.events.ProjectNodeAddedEvent pnae,
-			Object source) {
-		/**
-		 * TODO Implement this
-		 * medusa.components.listeners.ProjectNodeAddedListener method
-		 */
-		throw new java.lang.UnsupportedOperationException(
-				"Method projectNodeAdded() not yet implemented.");
 	}
 
 	public void hyperlinkUpdate(HyperlinkEvent event) {
