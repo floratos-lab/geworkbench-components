29951ad58c4b8319bdfeff4929573d34d6658f63
Zheng Ma <maz@columbia.edu>
2011-10-18 18:53:50 +0000

    bug 2746
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8417 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/idea/src/org/geworkbench/components/idea/IdeaNetworkEdge.java
+++ components/idea/src/org/geworkbench/components/idea/IdeaNetworkEdge.java
@@ -1,5 +1,7 @@
 package org.geworkbench.components.idea;
 
+import java.io.Serializable;
+
 import org.geworkbench.bison.datastructure.bioobjects.IdeaEdge.InteractionType;
 
 /**
@@ -8,7 +10,9 @@ import org.geworkbench.bison.datastructure.bioobjects.IdeaEdge.InteractionType;
  * @author zji
  * @version $Id$
  */
-public class IdeaNetworkEdge {
+public class IdeaNetworkEdge implements Serializable, Comparable<IdeaNetworkEdge>{
+	
+	private static final long serialVersionUID = -5741396026577511626L;
 	final int geneId1;
 	final int geneId2;
 	final InteractionType interactionType;
@@ -18,6 +22,12 @@ public class IdeaNetworkEdge {
 		geneId1 = Integer.parseInt(tokens[0]);
 		geneId2 = Integer.parseInt(tokens[1]);
 		interactionType = stringToInteractionType(tokens[3]);
+	}
+	
+	IdeaNetworkEdge(int node1, int node2){
+		geneId1=node1;
+		geneId2=node2;
+		interactionType =InteractionType.PROTEIN_DNA;	//defaut setting
 	}
 
 	private static InteractionType stringToInteractionType(String str) {
@@ -31,4 +41,22 @@ public class IdeaNetworkEdge {
 
 		return interactionType;
 	}
+	
+	public int getGene1(){
+		return geneId1;
+	}
+	
+	public int getGene2(){
+		return geneId2;
+	}
+
+	@Override
+	public int compareTo(IdeaNetworkEdge o) {
+		if(((this.geneId1==o.getGene1())&&(this.geneId2==o.getGene2()))||((this.geneId1==o.getGene2())&&(this.geneId2==o.getGene1()))){
+			return 0;
+		}
+		else return 1;
+	}
+
+	
 }
