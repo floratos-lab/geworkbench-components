4a630813c22edbb8469fcb6fc3a1fb6a3198cba1
Meng Wang <mw2518@columbia.edu>
2011-11-28 21:38:09 +0000

    modified a warning message
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8543 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/masterregulator/src/org/geworkbench/components/masterregulator/MasterRegulatorAnalysis.java
+++ components/masterregulator/src/org/geworkbench/components/masterregulator/MasterRegulatorAnalysis.java
@@ -492,7 +492,7 @@ public class MasterRegulatorAnalysis extends AbstractGridAnalysis implements
 			return new ParamValidationResults(false, "Please activate at least one control array.");
 		Iterator<String> casei = mraAnalysisPanel.getIxClass(CSAnnotationContext.CLASS_CASE).iterator();
 		if (!casei.hasNext()){
-			int c = JOptionPane.showConfirmDialog(null, "Are you sure to use only control group?");
+			int c = JOptionPane.showConfirmDialog(null, "Are you sure to use only a control group?");
 			if (c != JOptionPane.YES_OPTION)
 				return new ParamValidationResults(false, "Please activate both control and case.");
 		}
