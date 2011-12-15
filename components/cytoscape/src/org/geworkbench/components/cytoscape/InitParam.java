03ef886e3285ee97ece965e2f486c3972b2834a2
Min You <my2248@c2b2.columbia.edu>
2011-12-15 20:50:41 +0000

    upgrade to 2.8.2
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8611 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/cytoscape/src/org/geworkbench/components/cytoscape/InitParam.java
+++ components/cytoscape/src/org/geworkbench/components/cytoscape/InitParam.java
@@ -65,24 +65,27 @@ public class InitParam implements CyInitParams {
 	public List<String> getPlugins() {
 		List<String> plugins = new ArrayList<String>();
 		final String pluginDirectory = UILauncher.getComponentsDirectory()+"/cytoscape/lib/";
-		plugins.add(pluginDirectory+"AdvancedNetworkMerge.jar");
-		plugins.add(pluginDirectory+"AutomaticLayout.jar");
-		plugins.add(pluginDirectory+"biomartClient.jar");
-		//plugins.add(pluginDirectory+"biopax.jar"); // LinkageError due to its own collection15.MutilMap
-		plugins.add(pluginDirectory+"browser.jar");
-		plugins.add(pluginDirectory+"cPath.jar");
-		plugins.add(pluginDirectory+"cpath2.jar");
-		//plugins.add(pluginDirectory+"CytoscapeEditor.jar"); // prevent blocking the network view 
-		plugins.add(pluginDirectory+"filter.jar");
-		plugins.add(pluginDirectory+"filters.jar");
-		plugins.add(pluginDirectory+"linkout.jar");
-		plugins.add(pluginDirectory+"ManualLayout.jar");
-		plugins.add(pluginDirectory+"ncbiClient.jar");
-		plugins.add(pluginDirectory+"psi_mi.jar");
-		plugins.add(pluginDirectory+"quick_find.jar");
-		plugins.add(pluginDirectory+"SBMLReader.jar");
-		plugins.add(pluginDirectory+"TableImport.jar");
-		plugins.add(pluginDirectory+"yLayouts.jar");
+		plugins.add(pluginDirectory+"advanced-network-merge-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"attribute-browser-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"automatic-layout-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"biomart-client-2.8.2-jar-with-dependencies.jar"); // LinkageError due to its own collection15.MutilMap
+		plugins.add(pluginDirectory+"biopax-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"core-commands-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"cpath-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"cpath2-2.8.2-jar-with-dependencies.jar"); // prevent blocking the network view 
+		plugins.add(pluginDirectory+"editor-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"equation-functions-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"filters-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"linkout-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"manual-layout-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"network-analyzer-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"psi-mi-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"quickfind-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"rfilters-2.8.2-jar-with-dependencies.jar");
+		plugins.add(pluginDirectory+"sbml-reader-2.8.2-jar-with-dependencies.jar");		
+		plugins.add(pluginDirectory+"table-import-2.8.2-jar-with-dependencies.jar");	
+		plugins.add(pluginDirectory+"y-layouts-1.0.1.jar");
+		//plugins.add(pluginDirectory+"ncbi-client-2.8.2-jar-with-dependencies.jar");
 		return plugins;
 	}
 
