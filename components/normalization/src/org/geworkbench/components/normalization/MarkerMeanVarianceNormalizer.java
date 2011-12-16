e3308f0a33036c57b9c50f68f1bf3b17bc2e5c50
Zhou Ji <zji@c2b2.columbia.edu>
2011-12-16 20:56:37 +0000

    minor cleaning-up. see bug 2856
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8617 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/normalization/src/org/geworkbench/components/normalization/MarkerMeanVarianceNormalizer.java
+++ components/normalization/src/org/geworkbench/components/normalization/MarkerMeanVarianceNormalizer.java
@@ -24,12 +24,12 @@ import org.geworkbench.builtin.projects.history.HistoryPanel;
  * values in the profile. The normalizer also offers options for handling missing
  * values. In particular, the available choices are:
  * <UL>
- * <LI>“Min? replace with the smallest profile value resulting after the
+ * <LI>Min: replace with the smallest profile value resulting after the
  * mean/deviation adjustment ,</LI>
- * <LI>“Max? replace with the largest profile value resulting after the
+ * <LI>Max: replace with the largest profile value resulting after the
  * mean/deviation adjustment,</LI>
- * <LI>“Zero? replace with 0,</LI>
- * <LI>“Ignore? No change.</LI>.
+ * <LI>Zero: replace with 0,</LI>
+ * <LI>Ignore: No change.</LI>.
  * </UL>
  */
 public class MarkerMeanVarianceNormalizer extends AbstractAnalysis implements NormalizingAnalysis {
