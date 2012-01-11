3adb2d8f2ca61bcd589a842e44495a34e9dc1f5f
Zhou Ji <zji@c2b2.columbia.edu>
2012-01-11 23:01:49 +0000

    EDT problem. see bug 2868
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8697 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/classification/GPTrainingPanel.java
+++ components/gpmodule_v3_0/src/org/geworkbench/components/gpmodule/classification/GPTrainingPanel.java
@@ -12,12 +12,14 @@
 package org.geworkbench.components.gpmodule.classification;
 
 import java.awt.BorderLayout;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
 
 import org.geworkbench.bison.model.analysis.ParameterPanel;
 import org.geworkbench.components.gpmodule.GPConfigPanel;
@@ -70,8 +72,32 @@ public abstract class GPTrainingPanel extends AbstractTrainingPanel
         return label;
     }
     
-    protected void addParameters(DefaultFormBuilder builder)
+    // FIXME it is a design problem to allow this method to be invoked from both EDT and non-EDT.
+    // It is actually invoked both ways.
+    // For now, let's keep it safe first.
+    protected void addParameters(final DefaultFormBuilder builder)
     {
+    	if(SwingUtilities.isEventDispatchThread()) {
+    		addParameterFromEDT(builder);
+    	} else {
+    		try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+
+					@Override
+					public void run() {
+						addParameterFromEDT(builder);
+					}
+					
+				});
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    private void addParameterFromEDT(DefaultFormBuilder builder) {
         gpTabbedPane.removeAll();
 
         JPanel parameterPanel = getParameterPanel();
