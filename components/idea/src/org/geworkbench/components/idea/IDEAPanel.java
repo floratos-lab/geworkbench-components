7c316aadf169905f7f95399cda0d3abe77ab088c
Zheng Ma <maz@columbia.edu>
2012-01-19 15:57:30 +0000

    bug 2746, load from ADJ format file is added.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8736 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/idea/src/org/geworkbench/components/idea/IDEAPanel.java
+++ components/idea/src/org/geworkbench/components/idea/IDEAPanel.java
@@ -1,8 +1,14 @@
 package org.geworkbench.components.idea;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -23,6 +29,7 @@ import java.util.Set;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -36,13 +43,17 @@ import org.apache.commons.logging.LogFactory;
 import org.geworkbench.analysis.AbstractSaveableParameterPanel;
 import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix.Edge;
 import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix.NodeType;
+import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix;
 import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrixDataSet;
 import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
 import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
 import org.geworkbench.events.listeners.ParameterActionListener;
+import org.geworkbench.parsers.AdjacencyMatrixFileFormat;
+import org.geworkbench.parsers.InputFileFormatException;
 import org.geworkbench.util.FilePathnameUtils;
+import org.geworkbench.util.Util;
 import org.geworkbench.util.annotation.AffyAnnotationUtil;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
@@ -66,9 +77,10 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 	private ArrayList<String> adjModel=new ArrayList<String>();
 	private static final String FROM_FILE = "From File";
 	private static final String FROM_FILE_LAB = "From File (Lab Format)";
+	private static final String FROM_FILE_ADJ = "From File (ADJ Format)";
 	private static final String FROM_SETS = "From Set";
 	private static final String FROM_PROJECT = "From Project";
-	private static final String[] NETWORK_FROM = { FROM_FILE_LAB, FROM_PROJECT};
+	private static final String[] NETWORK_FROM = { FROM_PROJECT, FROM_FILE_ADJ, FROM_FILE_LAB};
 	private static final String[] PHENOTYPE_FROM = { FROM_SETS, FROM_FILE};
 	private static final String[] EXCLUDE_FROM = { FROM_SETS, FROM_FILE};
 	static final String[] DEFAULT_SET = { " " };	
@@ -99,6 +111,11 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 	private DSMicroarraySet maSet=null;
 	private boolean firstRunFlag;
 	private AdjacencyMatrixDataSet selectedAdjSet=null;
+	
+	private static final String lastDirConf = FilePathnameUtils.getUserSettingDirectoryPath()
+	+ "idea" + FilePathnameUtils.FILE_SEPARATOR + "lastDir.conf";
+	private String networkFilename = "";
+	private HashMap<String, AdjacencyMatrixDataSet> adjMatrix = new HashMap<String, AdjacencyMatrixDataSet>();
 	
 	public IDEAPanel() {
 		try {
@@ -148,53 +165,51 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 		selectionPanel.setLayout(new BorderLayout());
 		this.add(selectionPanel, BorderLayout.CENTER);		
 		
-		{
-			networkField.setEnabled(true);
-			networkField.setEditable(false);
-			networkLoadButton.setEnabled(true);		
-			includeLoadButton.setEnabled(false);
-			excludeLoadButton.setEnabled(false);			
+		networkMatrix.setEnabled(true);
+		networkField.setEnabled(false);
+		networkField.setEditable(false);
+		networkLoadButton.setEnabled(false);		
+		includeLoadButton.setEnabled(false);
+		excludeLoadButton.setEnabled(false);			
 			
-			FormLayout layout = new FormLayout(
-					"left:max(100dlu;pref), 10dlu, 100dlu, 10dlu, "
-							+ "100dlu, 10dlu, 100dlu, 10dlu, 100dlu", "");
-			DefaultFormBuilder builder = new DefaultFormBuilder(layout);
-			builder.setDefaultDialogBorder();
+		FormLayout layout = new FormLayout(
+				"left:max(100dlu;pref), 10dlu, 100dlu, 10dlu, "
+						+ "100dlu, 10dlu, 100dlu, 10dlu, 100dlu", "");
+		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+		builder.setDefaultDialogBorder();
 
-			builder.appendSeparator("Inputs required");			
-			builder.append("Load Network");
-			builder.append(networkFrom);			
-			networkMatrix.setEnabled(false);
-			builder.append(networkMatrix);
-			builder.append(networkField);
-			builder.append(networkLoadButton);						
-			builder.nextLine();			
+		builder.appendSeparator("Inputs required");			
+		builder.append("Load Network");
+		builder.append(networkFrom);			
+		builder.append(networkMatrix);
+		builder.append(networkField);
+		builder.append(networkLoadButton);						
+		builder.nextLine();			
 			
-			builder.append("Arrays Defining Phenotype");
-			builder.append(phenotypeFrom);						
-			builder.append(includeSets);
-			builder.append(includeField);
-			builder.append(includeLoadButton);
-			builder.nextLine();			
-			
-			builder.append(excludeLabel);
-			builder.append(excludeFrom);
-			builder.append(excludeSets);
-			builder.append(excludeField);
-			builder.append(excludeLoadButton);
-			builder.nextLine();			
-			
-			//builder.appendSeparator("Significance Threshold");	//pvalue is temporarily off	on GUI
-			builder.append(pvalueLabel);
-			if (pValueTextField == null)
-				pValueTextField = new JTextField();
-			pValueTextField.setText(Float.toString(PValueThresholdDefault));
-			builder.append(pValueTextField);
-			builder.nextLine();
-			
-			selectionPanel.add(builder.getPanel(), BorderLayout.CENTER);
-		}		
+		builder.append("Arrays Defining Phenotype");
+		builder.append(phenotypeFrom);						
+		builder.append(includeSets);
+		builder.append(includeField);
+		builder.append(includeLoadButton);
+		builder.nextLine();			
 		
+		builder.append(excludeLabel);
+		builder.append(excludeFrom);
+		builder.append(excludeSets);
+		builder.append(excludeField);
+		builder.append(excludeLoadButton);
+		builder.nextLine();			
+		
+		//builder.appendSeparator("Significance Threshold");	//pvalue is temporarily off	on GUI
+		builder.append(pvalueLabel);
+		if (pValueTextField == null)
+			pValueTextField = new JTextField();
+		pValueTextField.setText(Float.toString(PValueThresholdDefault));
+		builder.append(pValueTextField);
+		builder.nextLine();
+		
+		selectionPanel.add(builder.getPanel(), BorderLayout.CENTER);
+			
 		phenotypeFrom.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {				
 				includeField.setText("");				
@@ -242,19 +257,19 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 			}
 		});		
 		
-		 includeSets.addActionListener(new ActionListener() {
-	    		public void actionPerformed(ActionEvent actionEvent) {
-	    			String selectedLabel = (String) includeSets.getSelectedItem();
-	    			if ((!StringUtils.isEmpty(selectedLabel))&&(!selectedLabel.equals(""))
-	    					&&(!selectedLabel.equals(" "))){	//!StringUtils.isEmpty(selectedLabel)
-	    				if (!chooseArraysFromSet(selectedLabel, includeField)) {
-	    					includeSets.setSelectedIndex(0);	    					
-	    				}
+		includeSets.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent actionEvent) {
+	    		String selectedLabel = (String) includeSets.getSelectedItem();
+	    		if ((!StringUtils.isEmpty(selectedLabel))&&(!selectedLabel.equals(""))
+	    				&&(!selectedLabel.equals(" "))){	//!StringUtils.isEmpty(selectedLabel)
+	    			if (!chooseArraysFromSet(selectedLabel, includeField)) {
+	    				includeSets.setSelectedIndex(0);	    					
 	    			}
-	    			else
-						includeField.setText("");
 	    		}
-	    	});
+	    		else
+					includeField.setText("");
+	    	}
+	    });
 		
 		excludeSets.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
@@ -280,15 +295,10 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
     				}
     		}
     	});
+	
+		networkLoadButton.addActionListener(new LoadNetworkButtonListener(
+				adjMatrix));
 		
-		networkLoadButton.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				networkLoadPressed();
-			}	
-			
-		});		
 
 		includeLoadButton.addActionListener(new ActionListener(){			
 			public void actionPerformed(ActionEvent e) {
@@ -300,9 +310,6 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 				excludeLoadPressed();
 			}});
 		
-		// define the 'update/refreshing'behavior of GUI components - see the
-		// examples
-		// they are (basically) all the same
 		ParameterActionListener parameterActionListener = new ParameterActionListener(
 				this);
 		networkField.addActionListener(parameterActionListener);		
@@ -332,9 +339,8 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 			if (key.equals("networkText")) {
 				networkField.setText((String)value);
 				try {
-					getNetworkFromFile(networkField.getText());
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					getNetworkFromLabFile(networkField.getText());
+				} catch (IOException e) {					
 					ideaNetwork = null;
 					e.printStackTrace();
 				}
@@ -403,20 +409,261 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 			histStr.append("\nNetwork: "+getSelectedAdjMatrix());
 		}
 		else
-			histStr.append("\nNetwork: "+networkField.getText());
-		//histStr.append("\nPhenotype:");
+			histStr.append("\nNetwork: "+networkField.getText());		
 		String includeStr=phenotype.getPhenotypeInArrayNames();		
 		histStr.append("\n"+includeStr);
 		String excludeStr=phenotype.getExcludeInArrayNames();
 		histStr.append("\n"+excludeStr);		
-		//histStr.append("\np-value: "+pValueTextField.getText());
 		histStr.append("\n");
 		return histStr.toString();
 	}
 
 	public String getPvalue(){
 		return pValueTextField.getText();
-	}	
+	}
+	
+	public class LoadNetworkButtonListener implements
+		java.awt.event.ActionListener {
+		private HashMap<String, AdjacencyMatrixDataSet> adjMatrixHolder;
+		
+		public LoadNetworkButtonListener(
+				HashMap<String, AdjacencyMatrixDataSet> adjMatrixHolder) {
+			this.adjMatrixHolder = adjMatrixHolder;
+		}
+		
+		public void actionPerformed(java.awt.event.ActionEvent e) {			
+			if (e.getActionCommand().equals("Load")&&(networkFrom.getSelectedItem().equals(FROM_FILE_ADJ))) {
+				if (maSet != null) {
+					String adjMatrixFileStr = "C:\\Documents and Settings\\zm2165\\eclipse_geworkbench_workspace\\geworkbench-core\\data\\testaracne4.adjmat";
+					File adjMatrixFile = new File(adjMatrixFileStr);
+					JFileChooser chooser = new JFileChooser(adjMatrixFile
+							.getParent());
+					String lastDir = null;
+					if ((lastDir = getLastDir()) != null) {
+						chooser.setCurrentDirectory(new File(lastDir));
+					}
+					chooser.setFileFilter(new AdjacencyMatrixFileFormat().getFileFilter());
+					chooser.showOpenDialog(IDEAPanel.this);
+					if (chooser.getSelectedFile() != null) {
+						File selectedFile = chooser.getSelectedFile();
+						adjMatrixFileStr = selectedFile.getPath();
+						networkField.setText(adjMatrixFileStr);
+						networkFilename = selectedFile.getName();
+						saveLastDir(selectedFile.getParent());
+		
+						if (!openDialog()) return;
+						
+							try {
+								AdjacencyMatrix matrix = AdjacencyMatrixDataSet
+								.parseAdjacencyMatrix(adjMatrixFileStr, maSet,
+										interactionTypeMap, selectedFormat,
+										selectedRepresentedBy, isRestrict);
+	
+								AdjacencyMatrixDataSet adjMatrix = new AdjacencyMatrixDataSet(matrix, 
+										0, adjMatrixFileStr, adjMatrixFileStr, maSet);
+								this.adjMatrixHolder.remove("adjMatrix");
+								this.adjMatrixHolder.put("adjMatrix", adjMatrix);
+								getNetworkFromProject(adjMatrix);
+								
+							} catch (InputFileFormatException e1) {
+								log.error(e1.getMessage());
+								e1.printStackTrace();
+							}
+						
+					} else {
+						// user canceled
+					}
+				}
+			}
+			else if(e.getActionCommand().equals("Load")&&(networkFrom.getSelectedItem().equals(FROM_FILE_LAB))){
+				networkLoadPressed();
+			}
+		}
+	}
+	
+	private String[] representedByList;
+	private String selectedRepresentedBy = AdjacencyMatrixDataSet.PROBESET_ID;
+	private HashMap<String, String> interactionTypeMap = null;
+	private boolean isRestrict = true;
+	private boolean isCancel = false;
+	String selectedFormat = AdjacencyMatrixDataSet.ADJ_FORMART;	
+
+	private class LoadInteractionNetworkPanel extends JPanel {
+
+		static final long serialVersionUID = -1855255412334333328L;
+
+		final JDialog parent;
+
+		private JComboBox formatJcb;
+		private JComboBox presentJcb;
+
+		public LoadInteractionNetworkPanel(JDialog parent) {
+
+			setLayout(new BorderLayout());
+			this.parent = parent;
+			init();
+
+		}
+
+		private void init() {
+
+			JPanel panel1 = new JPanel(new GridLayout(3, 2));
+			JPanel panel3 = new JPanel(new GridLayout(0, 3));
+			JLabel label1 = new JLabel("File Format:    ");
+
+			formatJcb = new JComboBox();
+			formatJcb.addItem(AdjacencyMatrixDataSet.ADJ_FORMART);
+			formatJcb.addItem(AdjacencyMatrixDataSet.SIF_FORMART);			
+			JLabel label2 = new JLabel("Node Represented By:   ");
+
+			representedByList = new String[4];
+			representedByList[0] = AdjacencyMatrixDataSet.PROBESET_ID;
+			representedByList[1] = AdjacencyMatrixDataSet.GENE_NAME;
+			representedByList[2] = AdjacencyMatrixDataSet.ENTREZ_ID;
+			representedByList[3] = AdjacencyMatrixDataSet.OTHER;
+			presentJcb = new JComboBox(representedByList);
+
+			JButton continueButton = new JButton("Continue");
+			JButton cancelButton = new JButton("Cancel");
+			formatJcb.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent evt) {
+					if (formatJcb.getSelectedItem().toString().equals(
+							AdjacencyMatrixDataSet.ADJ_FORMART)) {
+						representedByList = new String[4];
+						representedByList[0] = AdjacencyMatrixDataSet.PROBESET_ID;
+						representedByList[1] = AdjacencyMatrixDataSet.GENE_NAME;
+						representedByList[2] = AdjacencyMatrixDataSet.ENTREZ_ID;
+						representedByList[3] = AdjacencyMatrixDataSet.OTHER;
+						presentJcb.setModel(new DefaultComboBoxModel(
+								representedByList));
+					} else {
+						representedByList = new String[3];
+						representedByList[0] = AdjacencyMatrixDataSet.GENE_NAME;
+						representedByList[1] = AdjacencyMatrixDataSet.ENTREZ_ID;
+						representedByList[2] = AdjacencyMatrixDataSet.OTHER;
+						presentJcb.setModel(new DefaultComboBoxModel(
+								representedByList));
+					}
+				}
+			});
+
+			if (networkFilename.toLowerCase().endsWith(".sif"))
+				formatJcb.setSelectedItem(AdjacencyMatrixDataSet.SIF_FORMART);
+			else
+				formatJcb.setSelectedItem(AdjacencyMatrixDataSet.ADJ_FORMART);
+			continueButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					continueButtonActionPerformed();
+					parent.dispose();
+					isCancel = false;
+				}
+			});
+			cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					parent.dispose();
+					isCancel = true;
+				}
+			});
+
+			panel1.add(label1);
+			panel1.add(formatJcb);
+
+			panel1.add(label2);
+			panel1.add(presentJcb);
+
+			panel3.add(cancelButton);
+			panel3.add(new JLabel("  "));
+			panel3.add(continueButton);
+			
+			this.add(panel1, BorderLayout.CENTER);
+			this.add(panel3, BorderLayout.SOUTH);
+			parent.getRootPane().setDefaultButton(continueButton);
+		}
+
+		private void continueButtonActionPerformed() {
+			selectedFormat = formatJcb.getSelectedItem().toString();
+			selectedRepresentedBy = presentJcb.getSelectedItem().toString();
+		}
+
+	}
+	
+	private String getLastDir(){
+		String dir = null;
+		try {
+			File file = new File(lastDirConf);
+			if (file.exists()) {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				dir = br.readLine();
+				br.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return dir;
+	}
+	private void saveLastDir(String dir){
+		//save as last used dir
+		try {
+			BufferedWriter br = new BufferedWriter(new FileWriter(lastDirConf));
+			br.write(dir);
+			br.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	private boolean openDialog(){
+		JDialog loadDialog = new JDialog();
+
+		loadDialog.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				isCancel = true;
+			}
+		});
+
+		isCancel = false;
+		loadDialog.setTitle("Load Interaction Network");
+		LoadInteractionNetworkPanel loadPanel = new LoadInteractionNetworkPanel(
+				loadDialog);
+
+		loadDialog.add(loadPanel);
+		loadDialog.setModal(true);
+		loadDialog.pack();
+		Util.centerWindow(loadDialog);
+		loadDialog.setVisible(true);
+
+		if (isCancel)
+			return false;
+
+		if ((selectedFormat
+				.equalsIgnoreCase(AdjacencyMatrixDataSet.SIF_FORMART) && !networkFilename
+				.toLowerCase().endsWith(".sif"))
+				|| (networkFilename.toLowerCase().endsWith(".sif") && !selectedFormat
+						.equalsIgnoreCase(AdjacencyMatrixDataSet.SIF_FORMART))) {
+			String theMessage = "The network format selected may not match that of the file.  \nClick \"Cancel\" to terminate this process.";
+			Object[] optionChoices = { "Continue", "Cancel"};
+			int result = JOptionPane.showOptionDialog(
+				(Component) null, theMessage, "Warning",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+				null, optionChoices, optionChoices[1]);
+			if (result == JOptionPane.NO_OPTION)
+				return false;
+
+		} 
+
+		if (selectedFormat
+				.equalsIgnoreCase(AdjacencyMatrixDataSet.SIF_FORMART)) {
+			interactionTypeMap = new org.geworkbench.parsers.AdjacencyMatrixFileFormat().getInteractionTypeMap();
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
 	public void networkLoadPressed(){
 
 		JFileChooser fc = new JFileChooser(this.getLastDirectory());
@@ -426,7 +673,7 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 				String filename = fc.getSelectedFile().getAbsolutePath();			
 				String filepath = fc.getCurrentDirectory().getCanonicalPath();
                 setLastDirectory(filepath);			
-                getNetworkFromFile(filename);				
+                getNetworkFromLabFile(filename);				
 				networkField.setText(filename);			
 			} catch (FileNotFoundException e1) {
 				e1.printStackTrace();
@@ -438,7 +685,7 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 		}	
 	}
 	
-	private void getNetworkFromFile(String filename) throws IOException{
+	private void getNetworkFromLabFile(String filename) throws IOException{
 		BufferedReader br;
 		br = new BufferedReader(new FileReader(filename));
 		String line = br.readLine(); // skip the header line
@@ -475,8 +722,6 @@ public class IDEAPanel extends AbstractSaveableParameterPanel {
 			}
 		}		
 		else if (nt.equals(NodeType.GENE_SYMBOL)) {
-		// FIXME this is a temporary solution to make the gene symbol node works
-		// both the design and the implementation should be carefully reviewed and probably redone later
 			DSMicroarraySet dataset = (DSMicroarraySet) adjDataSet.getParentDataSet();
 			Map<String, List<Integer>> geneNameToMarkerIdMap = AffyAnnotationUtil
 			.getGeneNameToMarkerIDMapping(dataset);
