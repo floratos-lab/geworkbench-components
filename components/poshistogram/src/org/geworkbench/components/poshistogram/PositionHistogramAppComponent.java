c46b06ab3bd08c7a701509bcc229a6556830d55e
Zhou Ji <zji@c2b2.columbia.edu>
2012-01-27 16:19:50 +0000

    trivial clean-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8792 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/poshistogram/src/org/geworkbench/components/poshistogram/PositionHistogramAppComponent.java
+++ components/poshistogram/src/org/geworkbench/components/poshistogram/PositionHistogramAppComponent.java
@@ -1,7 +1,6 @@
 package org.geworkbench.components.poshistogram;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
