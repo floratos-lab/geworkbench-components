082b24fca7889dbd48379c52fdc840294a6992ea
Ken Smith <kcs3@columbia.edu>
2012-03-09 00:11:58 +0000

    update for 2.3.0.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9060 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/annotations/src/org/geworkbench/components/annotations/help/markerAnnotationPanelHelpSet.hs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<?TestTarget The help set file for the Marker Annotation Panel component.?>
+
+<helpset version="1.0">
+
+	<!-- title -->
+
+	<title>Marker Annotation Panel</title>
+
+	<!-- maps -->
+
+	<maps>
+
+		<homeID>overview</homeID>
+
+		<mapref location="markerAnnotationPanelMap.jhm"/>
+
+	</maps>
+
+	<!-- views -->
+
+	<view>
+
+		<name>TOC</name>
+
+		<label>Marker Annotation Panel</label>
+
+		<type>javax.help.TOCView</type>
+
+		<data>markerAnnotationPaneltoc.xml</data>
+
+	</view>
+
+</helpset>
+
