956d457f62ff4e6c534af21264e707cac0a564a4
Ken Smith <kcs3@columbia.edu>
2012-03-13 18:16:29 +0000

    Added new help chapter in 2.3.0.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9146 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ components/caarray/src/org/geworkbench/components/caarray/arraydata/help/caArray_HelpSet.hs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<?TestTarget The help set file for the caArray component.?>
+
+<helpset version="1.0">
+
+	<!-- title -->
+
+	<title>caArray</title>
+
+	<!-- maps -->
+
+	<maps>
+
+		<homeID>overview</homeID>
+
+		<mapref location="caArray_Map.jhm"/>
+
+	</maps>
+
+	<!-- views -->
+
+	<view>
+
+		<name>TOC</name>
+
+		<label>caArray</label>
+
+		<type>javax.help.TOCView</type>
+
+		<data>caArray_toc.xml</data>
+
+	</view>
+
+</helpset>
+
