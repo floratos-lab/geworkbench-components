5d745fbddb8ddeee5015c1aa65dce1b9b003df50
Zhou Ji <zji@c2b2.columbia.edu>
2012-04-13 18:33:10 +0000

    code cleaning-up. see bug 3009.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9310 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/alignment/src/org/geworkbench/components/alignment/panels/BlastViewComponent.java
+++ components/alignment/src/org/geworkbench/components/alignment/panels/BlastViewComponent.java
@@ -1,23 +1,18 @@
 package org.geworkbench.components.alignment.panels;
 
 import java.awt.Component;
-import java.awt.Container;
 import java.util.ArrayList;
 import java.util.Vector;
-
-import javax.swing.JTabbedPane;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
-import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
-import org.geworkbench.bison.datastructure.biocollections.sequences.CSSequenceSet;
-import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
-import org.geworkbench.bison.datastructure.bioobjects.sequence.BlastObj; 
+import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
+import org.geworkbench.bison.datastructure.bioobjects.sequence.BlastObj;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSAlignmentResultSet;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
 import org.geworkbench.builtin.projects.ProjectPanel;
-import org.geworkbench.builtin.projects.ProjectSelection; 
+import org.geworkbench.builtin.projects.ProjectSelection;
 import org.geworkbench.engine.config.VisualPlugin;
 import org.geworkbench.engine.management.AcceptTypes;
 import org.geworkbench.engine.management.Publish;
@@ -71,6 +66,8 @@ public class BlastViewComponent implements VisualPlugin {
 	public Component getComponent() {
 		return blastViewPanel;
 	}
+	
+	transient DSAlignmentResultSet resultSet = null;
 
 	/**
 	 * receiveProjectSelection
@@ -78,48 +75,37 @@ public class BlastViewComponent implements VisualPlugin {
 	 * @param e
 	 *            ProjectEvent
 	 */
-	@SuppressWarnings("unchecked")
 	@Subscribe
 	public void receive(org.geworkbench.events.ProjectEvent e, Object source) {
 
-		Container parent = blastViewPanel.getParent();
-		if (parent instanceof JTabbedPane) {
-			((JTabbedPane) parent).setSelectedComponent(blastViewPanel);
-		}
-
 		ProjectSelection selection = ((ProjectPanel) source).getSelection();
-		DSAncillaryDataSet<DSBioObject> df = selection.getDataSubSet();
-		// Get the sequenceDb from DAncillaryDataset not from project.
-		DSDataSet<? extends DSSequence> sequenceDB = selection.getDataSet();
-		if (df != null && df instanceof DSAlignmentResultSet) {
-			// sequenceDB = df.getParentDataSet();
-			sequenceDB = ((DSAlignmentResultSet) df).getBlastedParentDataSet();
+		DSAncillaryDataSet<?> df = selection.getDataSubSet();
+
+		if (df == resultSet || !(df instanceof DSAlignmentResultSet)) {
+			return; // do nothing
 		}
-		if (sequenceDB instanceof CSSequenceSet && df != null) {
-			if (df instanceof DSAlignmentResultSet) {
-				log.debug("update blast result view panel");
-				DSAlignmentResultSet resultSet = (DSAlignmentResultSet) df;
-				ArrayList<Vector<BlastObj>> blastDataSet = resultSet
-						.getBlastDataSet();
 
-				blastViewPanel
-						.setSequenceDB((CSSequenceSet<DSSequence>) sequenceDB);
-				blastViewPanel.setBlastDataSet(blastDataSet);
+		log.debug("update blast result view panel");
+		resultSet = (DSAlignmentResultSet) df;
 
-				String summary = resultSet.getSummary();
-				blastViewPanel.setSummary(summary);
-				df.setDescription(summary);
+		// Get the sequenceDb from DAncillaryDataset, not from project.
+		DSSequenceSet<? extends DSSequence> sequenceDB = resultSet
+				.getBlastedParentDataSet();
+		ArrayList<Vector<BlastObj>> blastDataSet = resultSet.getBlastDataSet();
 
-				if (resultSet.getHitCount() == 0) {
-					blastViewPanel.resetToWhite("No alignment hit is found.");
-					blastViewPanel.setSummaryPanelOff();
-				} else {
-					blastViewPanel.setSummaryPanelOn();
-				}
-			} else {
-				blastViewPanel.resetToWhite();
-			}
+		// if (sequenceDB instanceof CSSequenceSet) // always true
+		blastViewPanel.setSequenceDB(sequenceDB);
+		blastViewPanel.setBlastDataSet(blastDataSet);
 
+		String summary = resultSet.getSummary();
+		blastViewPanel.setSummary(summary);
+		resultSet.setDescription(summary);
+
+		if (resultSet.getHitCount() == 0) {
+			blastViewPanel.resetToWhite("No alignment hit is found.");
+			blastViewPanel.setSummaryPanelOff();
+		} else {
+			blastViewPanel.setSummaryPanelOn();
 		}
 
 	}
