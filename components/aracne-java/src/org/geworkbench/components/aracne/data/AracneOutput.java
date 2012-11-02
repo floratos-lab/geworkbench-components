8dc1051bc6fa6cdcff1c85a5b9d3d6bd221335f9
Min You <my2248@c2b2.columbia.edu>
2012-11-02 20:52:17 +0000

    change for #3192
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@10107 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/aracne-java/src/org/geworkbench/components/aracne/data/AracneOutput.java
+++ components/aracne-java/src/org/geworkbench/components/aracne/data/AracneOutput.java
@@ -4,15 +4,15 @@ public class AracneOutput {
 
 	private AracneGraphEdge[] graphEdges;
 	private String paramterDescription;
-	
-	public AracneOutput(){};
-	
-	public AracneOutput(AracneGraphEdge[] graphEdges, String paramterDescription)
-	{
-		this.graphEdges = graphEdges;		
+
+	public AracneOutput() {
+	}
+
+	public AracneOutput(AracneGraphEdge[] graphEdges, String paramterDescription) {
+		this.graphEdges = graphEdges;
 		this.paramterDescription = paramterDescription;
 	}
-	
+
 	public AracneGraphEdge[] getGraphEdges() {
 		return this.graphEdges;
 	}
@@ -20,7 +20,7 @@ public class AracneOutput {
 	public void setGraphEdges(AracneGraphEdge[] graphEdges) {
 		this.graphEdges = graphEdges;
 	}
-	
+
 	public String getParamterDescription() {
 		return this.paramterDescription;
 	}
@@ -28,24 +28,25 @@ public class AracneOutput {
 	public void setParamterDescription(String paramterDescription) {
 		this.paramterDescription = paramterDescription;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuffer sb = new StringBuffer("Aracne Result\n");
-		 
+
 		sb.append("AracneGraphEdges:").append(" \n");
-		for(int i=0; i< graphEdges.length; i++)
-		{	 
-			sb.append(graphEdges[i].getNode1() + " " + graphEdges[i].getNode2() + graphEdges[i].getWeight());
-		    sb.append("\n");
+		if (graphEdges != null) {
+			for (int i = 0; i < graphEdges.length; i++) {
+				sb.append(graphEdges[i].getNode1() + " "
+						+ graphEdges[i].getNode2() + graphEdges[i].getWeight());
+				sb.append("\n");
+			}
 		}
 		sb.append("\n");
 		sb.append("paramterDescription:");
-		sb.append("\n");		 
+		sb.append("\n");
 		sb.append(paramterDescription + " ");
-	    
+
 		return sb.toString();
-	} 
-	
-	
+	}
+
 }
