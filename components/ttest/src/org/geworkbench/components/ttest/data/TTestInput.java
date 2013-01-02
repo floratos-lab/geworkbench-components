ef7ccd018ecf7d64220fb63ffc3a0894d3a6ded9
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2013-01-02 21:56:16 +0000

    Added getters and setters to the TTestInput class and made all the class members as private to use TTest as webservice. See bug: 3323 (geWorkbench-Web)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@10250 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/ttest/src/org/geworkbench/components/ttest/data/TTestInput.java
+++ components/ttest/src/org/geworkbench/components/ttest/data/TTestInput.java
@@ -4,24 +4,24 @@ import org.geworkbench.components.ttest.SignificanceMethod;
 
 /* Fields complete. */
 public class TTestInput {
-	public double[][] caseArray; // [rowCount][caseCount]
-	public double[][] controlArray; // [rowCount][controlCount]
+	private double[][] caseArray; // [rowCount][caseCount]
+	private double[][] controlArray; // [rowCount][controlCount]
 
-	public int rowCount;
-	public int caseCount;
-	public int controlCount;
+	private int rowCount;
+	private int caseCount;
+	private int controlCount;
 
 	// options. please some of them are supposed to be ignored depending on
 	// other ones
 	// approximately in the order that affects the algorithm flow more
-	public SignificanceMethod significanceMethod;
-	public boolean byPermutation;
-	public double alpha;
-	public boolean useWelch; // only affect the calculation of DF. thus orthogonal to other parameters
-	public boolean useAllCombinations;
-	public int numberCombinations;
+	private SignificanceMethod significanceMethod;
+	private boolean byPermutation;
+	private double alpha;
+	private boolean useWelch; // only affect the calculation of DF. thus orthogonal to other parameters
+	private boolean useAllCombinations;
+	private int numberCombinations;
 
-	public boolean isLogNormalized; // only affect folder change
+	private boolean isLogNormalized; // only affect folder change
 	
 	public TTestInput(int rowCount, int caseCount, int controlCount, double[][] caseArray, double[][]controlArray,
 			SignificanceMethod significanceMethod, double alpha, boolean byPermutation, boolean useWelch, boolean useAllCombinations, int numberCombinations,
@@ -40,6 +40,102 @@ public class TTestInput {
 		this.useWelch = useWelch;
 		this.useAllCombinations = useAllCombinations;
 		this.numberCombinations = numberCombinations;
+		this.isLogNormalized = isLogNormalized;
+	}
+	
+	public double[][] getCaseArray() {
+		return caseArray;
+	}
+
+	public void setCaseArray(double[][] caseArray) {
+		this.caseArray = caseArray;
+	}
+
+	public double[][] getControlArray() {
+		return controlArray;
+	}
+
+	public void setControlArray(double[][] controlArray) {
+		this.controlArray = controlArray;
+	}
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	public int getCaseCount() {
+		return caseCount;
+	}
+
+	public void setCaseCount(int caseCount) {
+		this.caseCount = caseCount;
+	}
+
+	public int getControlCount() {
+		return controlCount;
+	}
+
+	public void setControlCount(int controlCount) {
+		this.controlCount = controlCount;
+	}
+
+	public SignificanceMethod getSignificanceMethod() {
+		return significanceMethod;
+	}
+
+	public void setSignificanceMethod(SignificanceMethod significanceMethod) {
+		this.significanceMethod = significanceMethod;
+	}
+
+	public boolean isByPermutation() {
+		return byPermutation;
+	}
+
+	public void setByPermutation(boolean byPermutation) {
+		this.byPermutation = byPermutation;
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+	public boolean isUseWelch() {
+		return useWelch;
+	}
+
+	public void setUseWelch(boolean useWelch) {
+		this.useWelch = useWelch;
+	}
+
+	public boolean isUseAllCombinations() {
+		return useAllCombinations;
+	}
+
+	public void setUseAllCombinations(boolean useAllCombinations) {
+		this.useAllCombinations = useAllCombinations;
+	}
+
+	public int getNumberCombinations() {
+		return numberCombinations;
+	}
+
+	public void setNumberCombinations(int numberCombinations) {
+		this.numberCombinations = numberCombinations;
+	}
+
+	public boolean isLogNormalized() {
+		return isLogNormalized;
+	}
+
+	public void setLogNormalized(boolean isLogNormalized) {
 		this.isLogNormalized = isLogNormalized;
 	}
 }
