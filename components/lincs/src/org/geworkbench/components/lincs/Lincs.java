8edf040c1455f29341c847216e69a3c9c950a528
Min You <my2248@c2b2.columbia.edu>
2013-02-25 05:00:32 +0000

    change String[] to string for web service
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@10343 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/lincs/src/org/geworkbench/components/lincs/Lincs.java
+++ components/lincs/src/org/geworkbench/components/lincs/Lincs.java
@@ -3,8 +3,14 @@
  */
 package org.geworkbench.components.lincs;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.geworkbench.service.lincs.LincsService;
 import org.geworkbench.service.lincs.data.xsd.ComputationalData;
 import org.geworkbench.service.lincs.data.xsd.ExperimentalData;;
@@ -19,10 +25,25 @@ import org.geworkbench.service.lincs.data.xsd.ExperimentalData;;
 // to be replaced by actual calling to web services.
 public class Lincs {
 
-	LincsService lincsService = new LincsService();
-
+	private static final Log log = LogFactory.getLog(LincsInterface.class);
+	private static final String DEL = "|";
+	
+	LincsService lincsService = null;;
+	 
+	
 	public Lincs(String url, String username, String passwprd) {
-
+		
+		URL baseUrl;
+		baseUrl = org.geworkbench.service.lincs.LincsService.class
+				.getResource(".");
+		try {
+			baseUrl = new URL(baseUrl, url);
+		} catch (MalformedURLException e) {			 
+			log.warn("Failed to create URL for the wsdl Location: " + url +", retrying as a local file");
+			log.warn(e.getMessage());
+		}
+		lincsService = new LincsService(baseUrl, new QName(
+				"http://lincs.service.geworkbench.org", "LincsService"));
 	}
 
 	List<String> getAllTissueNames() throws Exception {
@@ -41,7 +62,7 @@ public class Lincs {
 		List<String> names = null;
 	 
 		names =  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getCellLineNamesForTissueType(tissueTypes);
+				.getCellLineNamesForTissueType(convertToString(tissueTypes));
 		if (names != null && names.size() > 0)
 			   names.add(0, "All");
 	 
@@ -84,7 +105,7 @@ public class Lincs {
 
 		List<String> names = null;
 		names =  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getCompound1NamesFromExperimental(tyssueTypes, cellLines);
+				.getCompound1NamesFromExperimental(convertToString(tyssueTypes), convertToString(cellLines));
 
 		if (names != null && names.size() > 0)
 			   names.add(0, "All");
@@ -95,7 +116,7 @@ public class Lincs {
 
 		List<String> names = null;
 		names =  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getCompound2NamesFromExperimental(tissueTypes, cellLines, drug1Names) ;
+				.getCompound2NamesFromExperimental(convertToString(tissueTypes), convertToString(cellLines), convertToString(drug1Names)) ;
 		if (names != null && names.size() > 0)
 		   names.add(0, "All");
 		return names;
@@ -104,7 +125,7 @@ public class Lincs {
 	List<String> getDrug1NamesFromComputational(List<String> tissueTypes, List<String>cellLines) throws Exception {
 
 		List<String> names = null;
-		names =  lincsService.getLincsServiceHttpSoap11Endpoint().getCompound1NamesFromComputational(tissueTypes, cellLines);
+		names =  lincsService.getLincsServiceHttpSoap11Endpoint().getCompound1NamesFromComputational(convertToString(tissueTypes),convertToString(cellLines));
 			 
 		if (names != null && names.size() > 0)
 			   names.add(0, "All");
@@ -115,7 +136,7 @@ public class Lincs {
 
 		List<String> names = null;
 		names = lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getCompound2NamesFromComputational(tyssueTypes, cellLines, drug1Names) ;
+				.getCompound2NamesFromComputational(convertToString(tyssueTypes), convertToString(cellLines), convertToString(drug1Names)) ;
 		if (names != null && names.size() > 0)
 			   names.add(0, "All");
 		return names;
@@ -128,7 +149,7 @@ public class Lincs {
 
 		 
 		return  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getExperimentalData(tissueTypes, cellLineNames, drug1Names, drug2Names, measurementTypes, assayTypes);
+				.getExperimentalData(convertToString(tissueTypes), convertToString(cellLineNames), convertToString(drug1Names), convertToString(drug2Names), convertToString(measurementTypes), convertToString(assayTypes));
          
 	}
 	
@@ -136,7 +157,7 @@ public class Lincs {
 
 		 
 		return  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getExperimentalDataWithLimit(tissueTypes, cellLineNames, drug1Names, drug2Names, measurmentTypes, assayTypes, onlyTitration, rowLimit);
+				.getExperimentalDataWithLimit(convertToString(tissueTypes), convertToString(cellLineNames), convertToString(drug1Names), convertToString(drug2Names), convertToString(measurmentTypes), convertToString(assayTypes), onlyTitration, rowLimit);
          
 	}
 	
@@ -145,15 +166,34 @@ public class Lincs {
 
 		 
 		return  lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getComputationalData(tissueTypes, cellLineNames, drug1Names, drug2Names, similarityAlgorithms);
+				.getComputationalData(convertToString(tissueTypes), convertToString(cellLineNames), convertToString(drug1Names), convertToString(drug2Names), convertToString(similarityAlgorithms));
          
 	}
 	
 	List<ComputationalData> getComputationalData(List<String>tissueTypes, List<String>cellLineNames, List<String>drug1Names, List<String>drug2Names, List<String>similarityAlgorithms, int rowLimit) throws Exception {
 
 		return lincsService.getLincsServiceHttpSoap11Endpoint()
-				.getComputationalDataWithLimit(tissueTypes, cellLineNames, drug1Names, drug2Names, similarityAlgorithms, rowLimit);
+				.getComputationalDataWithLimit(convertToString(tissueTypes), convertToString(cellLineNames), convertToString(drug1Names), convertToString(drug2Names), convertToString(similarityAlgorithms), rowLimit);
 
+	}
+	
+	private String convertToString(List<String> list)
+	{
+		String s = "";
+	    if (list != null && list.size() > 0)
+	    {
+	    	for(int i=0; i<list.size()-1; i++)
+	    	{
+	    		if (list.get(i) != null && !list.get(i).trim().equals(""))
+	    		{
+	    			s = s + list.get(i).trim() + DEL;
+	    		}
+	    	}
+	    	if (list.get(list.size()-1) != null && !list.get(list.size()-1).trim().equals(""));
+	           s = s + list.get(list.size()-1);
+	    }
+	    
+	    return s;
 	}
 
 }
