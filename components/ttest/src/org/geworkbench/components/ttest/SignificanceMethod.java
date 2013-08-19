7924ca56b9ab2110f4d0b3bf96db854e50ded21a
Zhou Ji <zji@c2b2.columbia.edu>
2013-08-19 21:03:39 +0000

    bug 3497
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@10770 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/ttest/src/org/geworkbench/components/ttest/SignificanceMethod.java
+++ components/ttest/src/org/geworkbench/components/ttest/SignificanceMethod.java
@@ -1,9 +1,9 @@
 package org.geworkbench.components.ttest;
 
 public class SignificanceMethod {
-	public static int JUST_ALPHA 		= 	0;
-	public static int STD_BONFERRONI 	= 	1; 
-	public static int ADJ_BONFERRONI 	= 	2;
-	public static int MAX_T 			= 	3; 
-	public static int MIN_P 			= 	4;
+	public final static int JUST_ALPHA 		= 	0;
+	public final static int STD_BONFERRONI 	= 	1; 
+	public final static int ADJ_BONFERRONI 	= 	2;
+	public final static int MAX_T 			= 	3; 
+	public final static int MIN_P 			= 	4;
 }
