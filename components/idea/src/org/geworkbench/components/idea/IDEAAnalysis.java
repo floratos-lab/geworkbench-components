597774b9f3b27801e3085ca786a174993cdee94a
Zhou Ji <zji@c2b2.columbia.edu>
2013-09-24 21:31:21 +0000

    Disable the local version of IDEA. For detail, see bug 3137.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/branches/geworkbench_2_5_0/geworkbench@10909 8b29bb6e-589a-435a-9a12-3ab907be3462

--- components/idea/src/org/geworkbench/components/idea/IDEAAnalysis.java
+++ components/idea/src/org/geworkbench/components/idea/IDEAAnalysis.java
@@ -2,36 +2,26 @@ package org.geworkbench.components.idea;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.math.MathException;
 import org.geworkbench.analysis.AbstractGridAnalysis;
 import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrixDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
-import org.geworkbench.bison.datastructure.bioobjects.IdeaGLoc;
-import org.geworkbench.bison.datastructure.bioobjects.IdeaModule;
-import org.geworkbench.bison.datastructure.bioobjects.IdeaNode;
 import org.geworkbench.bison.datastructure.bioobjects.IdeaResultDataSet;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
-import org.geworkbench.bison.datastructure.complex.panels.DSItemList;
 import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
 import org.geworkbench.bison.model.analysis.AlgorithmExecutionResults;
 import org.geworkbench.bison.model.analysis.ClusteringAnalysis;
@@ -40,9 +30,7 @@ import org.geworkbench.builtin.projects.DataSetNode;
 import org.geworkbench.builtin.projects.DataSetSubNode;
 import org.geworkbench.builtin.projects.ProjectPanel;
 import org.geworkbench.builtin.projects.ProjectSelection;
-import org.geworkbench.builtin.projects.history.HistoryPanel;
 import org.geworkbench.engine.management.Subscribe;
-import org.geworkbench.util.FishersExactTest;
 import org.geworkbench.util.ProgressBar;
 
 /**
@@ -72,296 +60,14 @@ public class IDEAAnalysis extends AbstractGridAnalysis implements
 		setDefaultPanel(IDEAAnalysisPanel);
 	}
 
-	@SuppressWarnings("unchecked")
-	public AlgorithmExecutionResults execute(Object input) {
-
-		// read input data, dataset view, dataset, etc.
-		if (!(input instanceof DSMicroarraySetView)) {
-			return new AlgorithmExecutionResults(false,
-					"Input dataset for IDEA analysis should be a MicroarraySet.\n"
-							+ "But you selected a "
-							+ input.getClass().getName(), null);
-		}
-
-		double pvalue;
-		try{
-			pvalue=Double.parseDouble(IDEAAnalysisPanel.getPvalue());
-			if((pvalue<=0)||(pvalue>=1))
-				return new AlgorithmExecutionResults(false,
-						"P-value should be between 0 and 1.", null);
-		}
-		catch (Exception e){
-			return new AlgorithmExecutionResults(false,
-					"P-value is invalid.", null);
-		}
-		
-		DSMicroarraySetView<DSGeneMarker, DSMicroarray> datasetView = (DSMicroarraySetView<DSGeneMarker, DSMicroarray>) input;	
-		DSMicroarraySet maSet = datasetView.getMicroarraySet();
-		if(maSet.getAnnotationFileName()==null){
-			return new AlgorithmExecutionResults(false,
-					"IDEA analysis needs annotation file. Please load it first.", null);
-		}		
-		int numGenes = maSet.getMarkers().size();
-		
-		pbIdea = ProgressBar.create(ProgressBar.INDETERMINATE_TYPE);
-		pbIdea.addObserver(this);
-		pbIdea.setTitle("IDEA Analysis");
-		pbIdea.setBounds(new ProgressBar.IncrementModel(0, numGenes, 0,
-				numGenes, 1));
-
-		pbIdea.setMessage("Calculating IDEA, please wait...");
-		pbIdea.start();
-		this.stopAlgorithm = false;
-
-		HashSet<IdeaEdge> edgeIndex = new HashSet<IdeaEdge>();
-
-		List<IdeaNetworkEdge> network = IDEAAnalysisPanel.getNetwork();
-		if (network == null){			
-			pbIdea.dispose();			
-			return new AlgorithmExecutionResults(false,
-					"network file is invalid.", null);
-		}
-
-		Map<Integer, List<DSGeneMarker>> preGeneList = new HashMap<Integer, List<DSGeneMarker>>();
-		for (IdeaNetworkEdge edge : network) {
-			preGeneList.put(edge.getGene1(), new ArrayList<DSGeneMarker>());
-			preGeneList.put(edge.getGene2(), new ArrayList<DSGeneMarker>());
-		}
-		
-		for (DSGeneMarker marker : datasetView.markers()) {
-			int id = marker.getGeneId();
-
-			List<DSGeneMarker> markers = preGeneList.get(id);
-			if (markers != null) {
-				markers.add(marker);
-			}
-		}
-
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}
-
-		for (IdeaNetworkEdge edge : network) {
-
-			List<DSGeneMarker> markers1 = preGeneList.get(edge.getGene1());
-			List<DSGeneMarker> markers2 = preGeneList.get(edge.getGene2());
-
-			if ((markers1 != null) && (markers2 != null)) {
-				for (DSGeneMarker marker1 : markers1) {
-					for (DSGeneMarker marker2 : markers2) {
-
-						IdeaEdge anEdge = new IdeaEdge(marker1, marker2,
-								edge.getInteractionType());
-						edgeIndex.add(anEdge);
-					}
-				}
-			}
-		}
-		
-		if(edgeIndex.size()==0){
-			pbIdea.dispose();
-			return new AlgorithmExecutionResults(false,
-					"No overlap between network edges and data set, analysis aborted!", null);
-		}
-		
-		Phenotype phenotype = IDEAAnalysisPanel.getPhenotype(); // never null
-		Set<Integer> includeSet=IDEAAnalysisPanel.preparePhenoSet(IDEAAnalysisPanel.getIncludeString());
-		phenotype.setIncludeList(includeSet);
-		Set<Integer> excludeSet=IDEAAnalysisPanel.preparePhenoSet(IDEAAnalysisPanel.getExcludeString());
-		phenotype.setExcludeList(excludeSet);
-		if (IDEAAnalysisPanel.getIncludeString().equals("")) {
-			pbIdea.dispose();
-			return new AlgorithmExecutionResults(false,
-					"phenotype data is invalid.", null);
-		}
-		
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}
-
-		try {
-			/* ============= key process =========== */
-			NullDistribution nullDist = new NullDistribution(this);
-			boolean finished = nullDist.calcNullDist(maSet, edgeIndex, pvalue,
-					phenotype);
-			if (!finished || this.stopAlgorithm) {
-				pbIdea.dispose();
-				return null;
-			}
-		} catch (MathException e) {
-			e.printStackTrace();
-			pbIdea.dispose();
-			log.error(e);
-			return new AlgorithmExecutionResults(false,
-					"IDEA calculation failed due to MathException", null);
-		} catch (IOException e) {
-			e.printStackTrace();
-			pbIdea.dispose();
-			log.error(e);
-			return new AlgorithmExecutionResults(false,
-					"IDEA calculation failed due to IOException", null);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			pbIdea.dispose();
-			log.error(e);
-			return new AlgorithmExecutionResults(false,
-					"IDEA calculation failed due to ClassNotFoundException",
-					null);
-		} catch (Exception e) {
-			e.printStackTrace();
-			pbIdea.dispose();
-			log.error(e);
-			return new AlgorithmExecutionResults(false,
-					"IDEA calculation failed due to Exception", null);
-		} 
-		
-		
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}
-
-		List<IdeaEdge> locList = new ArrayList<IdeaEdge>();
-		List<IdeaEdge> gocList = new ArrayList<IdeaEdge>();
-		for (IdeaEdge anEdge : edgeIndex) {
-			if (anEdge.isLoc())
-				locList.add(anEdge);
-			else if (anEdge.isGoc())
-				gocList.add(anEdge);
-		}
-
-		Collections.sort(locList);
-
-		Collections.sort(gocList, Collections.reverseOrder());
-
-		TreeSet<IdeaProbeGene> probes = new TreeSet<IdeaProbeGene>();
-		/*
-		 * process probes, which is a alternative way to evaluate genes other
-		 * than entrez genes which I call gene in this code
-		 */
-		for (IdeaEdge e : edgeIndex) {
-			IdeaProbeGene p1 = new IdeaProbeGene(e.getProbeId1());
-			IdeaProbeGene p2 = new IdeaProbeGene(e.getProbeId2());
-			probes.add(p1);
-			probes.add(p2);
-		}
-
-		for (IdeaProbeGene p : probes) {
-			p.setEdges(edgeIndex);
-		}
-
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}
-		
-		int N = edgeIndex.size();
-		FishersExactTest fe = new FishersExactTest(2 * N);
-
-		int Sl = locList.size();
-		int Sg = gocList.size();
-
-		// calculate LOC p-value using fisher exact test to evaluate entrez
-		try{
-			for (IdeaProbeGene p : probes) {
-				int H = p.getEdgeCount();
-				// default values are 1
-				double cumLoc = 1;
-				double cumGoc = 1;
-				if (p.getLocs() > 0) { // calculate LOC p-value using fisher
-										// exact test to evaluate probe
-					int Dl = p.getLocs();
-					cumLoc = fe.calculateRightSideOneTailedP(Dl, H - Dl, Sl - Dl, N
-							- Sl - H + Dl);
-				}
-				if (p.getGocs() > 0) {
-					int Dg = p.getGocs();
-					cumGoc = fe.calculateRightSideOneTailedP(Dg, H - Dg, Sg - Dg, N
-							- Sg - H + Dg);
-				}
-				p.updateNes(cumLoc, cumGoc);
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			pbIdea.dispose();
-			log.error(e);			
-			return new AlgorithmExecutionResults(false,
-					"IDEA calculation failed due to exception:" +
-					"\nfinding significant genes using Fisher Exact Test.",
-					null);
-		}
-		
-		List<IdeaProbeGene> probeNes = new ArrayList<IdeaProbeGene>();
-		for (IdeaProbeGene p : probes) {
-			probeNes.add(p);
-		}
-		Collections.sort(probeNes, new IdeaProbeGene.NesComparator());
-
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}
-		
-		//prepare locResultList,gocResultList,nodeResultList
-		//locList->locResultList		
-		List<IdeaGLoc> locResultList=new ArrayList<IdeaGLoc>();
-		for(IdeaEdge e:locList){
-			IdeaGLoc anItem=new IdeaGLoc(e.getProbeId1(),e.getMarker1().getGeneName(),
-					e.getProbeId2(), e.getMarker2().getGeneName(),
-					e.getMI(), e.getDeltaCorr(), e.getzDeltaCorr() );
-			locResultList.add(anItem);
-		}
-		//gocList->gocResultList
-		List<IdeaGLoc> gocResultList=new ArrayList<IdeaGLoc>();
-		for(IdeaEdge e:gocList){
-			IdeaGLoc anItem=new IdeaGLoc(e.getProbeId1(),e.getMarker1().getGeneName(),
-					e.getProbeId2(), e.getMarker2().getGeneName(),
-					e.getMI(), e.getDeltaCorr(), e.getzDeltaCorr() );
-			gocResultList.add(anItem);
-		}
-		//probeNes->nodeResultList
-		List<IdeaNode> nodeResultList=new ArrayList<IdeaNode>();
-		DSItemList<DSGeneMarker> markers = maSet.getMarkers();
-		for (IdeaProbeGene pg : probeNes) {
-			if (pg.isSignificant()) // FIXME inconsistent with remote result
-			{
-				nodeResultList.add(pg.getIdeaNode(markers));
-			}
-		}
-		
-		//prepare moduleResultList
-		List<IdeaModule> moduleResultList = new ArrayList<IdeaModule>();
-		for (IdeaProbeGene p : probeNes) {// present significant node with its
-			// edges
-			if (p.isSignificant(pvalue)) {
-				moduleResultList.addAll(p.getModuleList());
-			}
-		}
-		if (this.stopAlgorithm) {
-			pbIdea.dispose();
-			return null;
-		}		
-		IdeaResultDataSet analysisResult = new IdeaResultDataSet(maSet,
-				"IDEA Analysis Result",locResultList, gocResultList, nodeResultList, moduleResultList,pvalue);
-		String stemp = generateHistoryString()+"\n";
-		HistoryPanel.addToHistory(analysisResult, stemp+ this.generateHistoryForMaSetView(datasetView));
-
-		AlgorithmExecutionResults results = new AlgorithmExecutionResults(true,
-				"Idea Analysis", analysisResult);
-		pbIdea.dispose();
-		return results;
+	@Override
+	public AlgorithmExecutionResults execute(Object input) {		// inform the user that only remote service is available
+		return new AlgorithmExecutionResults(
+				false,
+				"The local service for IDEA is not available. Please select the grid version in the Services tab",
+				null);
 	}
 
-	private String generateHistoryString() {
-		StringBuffer histStr = new StringBuffer();
-		histStr.append(IDEAAnalysisPanel.getDataSetHistory());
-		return histStr.toString();
-	}
-
-	
 	public void stop(){
 		if (pbIdea!=null)
 			pbIdea.dispose();
